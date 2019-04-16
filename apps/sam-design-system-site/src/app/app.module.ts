a8ebb00e9c2a934ee8dc92fed7de6955c9c163c4
Colin Alford <colinalford@gmail.com>
2019-04-16 14:45:55 -0400

    Removed styles lib from package.json, removed components from design system site

--- apps/sam-design-system-site/src/app/app.module.ts
+++ apps/sam-design-system-site/src/app/app.module.ts
@@ -1,13 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
-import { NxModule } from '@nrwl/nx';
-import { SamAlertModule } from '@sam-design-system/components';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, NxModule.forRoot(), SamAlertModule],
+  imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
 })
