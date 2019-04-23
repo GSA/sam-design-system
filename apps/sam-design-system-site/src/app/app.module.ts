9e0d3345222535f82c8efd055051be9f1f02f1b1
Carlos Valle <carlos.m.valle@gmail.com>
2019-04-22 22:50:53 -0400

    Adding Icon Component to library

--- apps/sam-design-system-site/src/app/app.module.ts
+++ apps/sam-design-system-site/src/app/app.module.ts
@@ -2,10 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { sdsIcons } from '@sam-design-system/components';
+
+const appIcons = {
+  // App Specific Icons
+ };
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [
+    BrowserModule,
+    FontAwesomeModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule {
+  constructor() {
+    library.add(sdsIcons, appIcons);
+  }
+}
