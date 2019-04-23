2e9e050759ed3301deacecb55dadcd67c61dadd4
Thomas Tighe <ttighe@reisystems.com>
2019-04-23 09:34:20 -0400

    add pagination

---
+++ libs/packages/components/src/lib/pagination/pagination.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'iae-pagination',
+  templateUrl: './pagination.component.html',
+  styleUrls: ['./pagination.component.scss']
+})
+export class PaginationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
