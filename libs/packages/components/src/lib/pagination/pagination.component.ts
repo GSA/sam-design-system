7094a0ea77e21222cfcd5db3d8c75ddbc8b5d062
Thomas Tighe <ttighe@reisystems.com>
2019-04-23 12:06:59 -0400

    pagination

--- libs/packages/components/src/lib/pagination/pagination.component.ts
+++ libs/packages/components/src/lib/pagination/pagination.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter,Output } from '@angular/core';
+
 
 @Component({
   selector: 'iae-pagination',
@@ -8,8 +9,41 @@ import { Component, OnInit } from '@angular/core';
 export class PaginationComponent implements OnInit {
 
   constructor() { }
+  @Output() public pageChange = new EventEmitter<number>();
+
+  totalPages: number;
+  currentPage: number = 1;
+  id: string;
+  options: [{ label: '5', value: 5 },
+    { label: '10', value: 10 },
+    { label: '15', value: 15 },
+    { label: '20', value: 20 },
+    { label: '50', value: 50 },
+    { label: '100', value: 100 }];
+
+
+  previousPage() {
+    if (this.currentPage > 1) {
+      this.currentPage--;
+      //raise event
+    }
+  }
+
+  nextPage() {
+    if (this.currentPage < this.totalPages) {
+      this.currentPage++;
+      //raise event
+    }
+  }
+
+  // pageChange() {
+
+
+  // }
+
 
   ngOnInit() {
+
   }
 
 }
