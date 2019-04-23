631dc97f944563724752b1506b5af8ab809bbfbc
Colin Alford <colinalford@gmail.com>
2019-04-23 11:32:27 -0400

    Cleaned up pack and publish script

--- tools/scripts/pack-and-publish.js
+++ tools/scripts/pack-and-publish.js
@@ -9,7 +9,6 @@ main();
 
 function main () {
   const args = parseArgs(process.argv.slice(2));
-  console.log(args);
   const rootDir = resolve(__dirname, '../../');
   const angularJsonPath = resolve(__dirname, '../../angular.json');
   
@@ -17,13 +16,7 @@ function main () {
   const { error: angularJsonError, contents: angularJson } = loadJson(angularJsonPath);
   handleError(angularJsonError);
 
-  const projectNames = Object.keys(angularJson.projects);
-  const libNames = projectNames.filter(name => {
-    const metadata = angularJson.projects[name];
-    // Returns true if lib is a library and uses the ng-packagr schematic (indicates intent to publish)
-    return metadata.projectType === 'library'
-      && metadata.architect.build.builder === '@angular-devkit/build-ng-packagr:build';
-  });
+  const libNames = findPublishableLibs(angularJson);
   
   libNames.forEach(lib => {
     const distDir = resolve(rootDir, `dist/libs/${lib}/`);
@@ -31,6 +24,7 @@ function main () {
     execSync(`npm pack`, { cwd: distDir, stdio: 'inherit' });
 
     const tarballPath = findTarball(distDir);
+
     if (tarballPath) {
       const publishCmd = `npm publish ${tarballPath} ${args['dry-run'] ? '--dry-run' : ''}`;
       execSync(publishCmd, { cwd: distDir, stdio: 'inherit' });
@@ -39,7 +33,21 @@ function main () {
       tarballError.code = 1;
       handleError(tarballError);
     }
+    
   });
+}
+
+function findPublishableLibs (angularJson) {
+  const projectNames = Object.keys(angularJson.projects);
+
+  const libNames = projectNames.filter(name => {
+    const metadata = angularJson.projects[name];
+    // Returns true if lib is a library and uses the ng-packagr schematic (indicates intent to publish)
+    return metadata.projectType === 'library'
+      && metadata.architect.build.builder === '@angular-devkit/build-ng-packagr:build';
+  });
+
+  return libNames;
 }
 
 function findTarball (directory) {
