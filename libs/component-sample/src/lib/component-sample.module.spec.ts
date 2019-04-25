f5b28ce800bca7c87007f4281750745b9fab8faf
Thomas Tighe <ttighe@reisystems.com>
2019-04-25 09:59:17 -0400

    fix tests

--- libs/component-sample/src/lib/component-sample.module.spec.ts
+++ libs/component-sample/src/lib/component-sample.module.spec.ts
@@ -1,10 +1,11 @@
 import { async, TestBed } from '@angular/core/testing';
 import { ComponentSampleModule } from './component-sample.module';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('ComponentSampleModule', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [ComponentSampleModule]
+      imports: [ComponentSampleModule,RouterTestingModule]
     }).compileComponents();
   }));
 
