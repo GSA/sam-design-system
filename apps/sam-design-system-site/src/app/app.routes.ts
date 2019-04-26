38637ee86f190381125fcc2f1f7fbd5bf8b081cd
Thomas Tighe <ttighe@reisystems.com>
2019-04-26 10:22:58 -0400

    removing bad comments

--- apps/sam-design-system-site/src/app/app.routes.ts
+++ apps/sam-design-system-site/src/app/app.routes.ts
@@ -2,5 +2,5 @@ import { Routes, RouterModule } from '@angular/router';
 
 export const ROUTES: Routes = [
   { path: 'component', loadChildren: '@gsa-sam/component-sample#ComponentSampleModule' } 
- //{path:'garbage', loadChildren:'@gsa-sam/fakenew#FakenewModule'}
+
 ];
