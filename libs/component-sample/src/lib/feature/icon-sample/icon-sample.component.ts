acb1bc8ae439ea33f4366a0d01a06ebec253e5fd
Carlos Valle <carlos.m.valle@gmail.com>
2019-04-29 02:56:36 -0400

    Adding icon library and example page

---
+++ libs/component-sample/src/lib/feature/icon-sample/icon-sample.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'sds-icon-sample',
+  templateUrl: 'icon-sample.component.html',
+  encapsulation: ViewEncapsulation.None
+})
+
+export class IconSampleComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() { }
+}
--- libs/packages/components/src/lib/public-api.ts
+++ libs/packages/components/src/lib/public-api.ts
@@ -1,3 +1,4 @@
 export * from './accordion';
+export * from './icon';
 export * from './page';
 export * from './toolbar';
