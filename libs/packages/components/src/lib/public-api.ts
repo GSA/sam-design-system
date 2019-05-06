74ba1e4544aa4a24acffec73b0f47e4812b0f0bc
Thomas Tighe <ttighe@reisystems.com>
2019-05-06 15:21:56 -0400

    adding to public apis

--- libs/packages/components/src/lib/public-api.ts
+++ libs/packages/components/src/lib/public-api.ts
@@ -2,3 +2,6 @@ export * from './accordion/accordion.module';
 export * from './icon';
 export * from './page/page.module';
 export * from './toolbar/toolbar.module';
+export * from './header/header.module';
+export * from './footer/footer.module';
+export * from './pagination/pagination.module';
