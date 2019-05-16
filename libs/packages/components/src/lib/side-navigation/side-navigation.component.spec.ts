5ddbb08d5209e0530deae0037c3857d3b3a3017c
Thomas Tighe <ttighe@reisystems.com>
2019-05-16 13:57:33 -0400

    sid nav tests

--- libs/packages/components/src/lib/side-navigation/side-navigation.component.spec.ts
+++ libs/packages/components/src/lib/side-navigation/side-navigation.component.spec.ts
@@ -3,6 +3,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { SdsSideNavigationComponent } from './side-navigation.component';
 import { SdsAccordionModule } from '../accordion/accordion.module';
 import { RouterTestingModule } from '@angular/router/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 
@@ -13,7 +14,7 @@ describe('SdsSideNavigationComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [SdsSideNavigationComponent],
-      imports: [RouterTestingModule, SdsAccordionModule]
+      imports: [RouterTestingModule, SdsAccordionModule, BrowserAnimationsModule]
     })
       .compileComponents();
   }));
