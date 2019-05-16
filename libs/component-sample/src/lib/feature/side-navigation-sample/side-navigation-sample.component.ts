c3e674ffcc91dd9a1087c5d398e8d90a1ffc5b09
Thomas Tighe <ttighe@reisystems.com>
2019-05-16 10:49:43 -0400

    adding sample

--- libs/component-sample/src/lib/feature/side-navigation-sample/side-navigation-sample.component.ts
+++ libs/component-sample/src/lib/feature/side-navigation-sample/side-navigation-sample.component.ts
@@ -7,7 +7,31 @@ import { Component, OnInit } from '@angular/core';
 
 export class SideNavigationSampleComponent implements OnInit {
   constructor() { }
-  model = { navigationLinks: [] };
+  model = {
+    navigationLinks: [{
+      text: 'Parent 1', id: 'linkp1', children: [
+        { text: 'Child 1 of Parent 1', route: '/', id: 'linkc1p1' },
+        { text: 'Child 2 of Parent 1', route: '/', id: 'linkc2p1' },
+        { text: 'Child 3 of Parent 1', route: '/', id: 'linkc3p1' },
+        {
+          text: 'Child 4 of Parent 1', route: '/', id: 'linkc4p1'
+          , children: [
+            { text: 'Grandchild 1 of Child 4 of Parent 1', route: '/', id: 'linkgc1c4p1' },
+            { text: 'Grandchild 2 of Child 4 of Parent 1', route: '/', id: 'linkgc2c4p1' }
+          ]
+        }
+      ]
+    },
+    {
+      text: 'Parent 2', id: 'linkp2', children: [
+        { text: 'Child 1 of Parent 2', route: '/', id: 'linkc1p2' },
+        { text: 'Child 2 of Parent 2', route: '/', id: 'linkc2p2' },
+        { text: 'Child 3 of Parent 2', route: '/', id: 'linkc3p2' },
+        { text: 'Child 4 of Parent 2', route: '/', id: 'linkc4p2' }
+      ]
+    }
+    ]
+  };
 
   ngOnInit() { }
 }
--- libs/component-sample/src/lib/feature/side-navigation-sample/side-navigation-sample.module.ts
+++ libs/component-sample/src/lib/feature/side-navigation-sample/side-navigation-sample.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
-
 import {
+  SdsAccordionModule,  SdsToolbarModule,  SdsPageModule,
   SdsSideNavigationModule
 } from '@gsa-sam/components';
 
 import { SideNavigationSampleComponent } from './side-navigation-sample.component';
 
 @NgModule({
-  imports: [SdsSideNavigationModule],
+  imports: [SdsSideNavigationModule, SdsAccordionModule,
+    SdsToolbarModule, SdsPageModule],
   exports: [SideNavigationSampleComponent],
   declarations: [SideNavigationSampleComponent],
   providers: []
