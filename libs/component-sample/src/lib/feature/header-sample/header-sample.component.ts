a52b42ea9e467ae133480de55d318c35a7b5d099
Thomas Tighe <ttighe@reisystems.com>
2019-06-12 14:41:40 -0400

    setting all links to internal

--- libs/component-sample/src/lib/feature/header-sample/header-sample.component.ts
+++ libs/component-sample/src/lib/feature/header-sample/header-sample.component.ts
@@ -27,10 +27,10 @@ export class HeaderSampleComponent implements OnInit {
 
   modelHeader: HeaderModel = {
     secondaryLinks: [{
-      imageClassPrefix: 'fas', imageClass: 'comment', text: 'Messages', route: 'http://www.google.com', id: 'messages', mode: NavigationMode.EXTERNAL
+      imageClassPrefix: 'fas', imageClass: 'comment', text: 'Messages', route: '/', id: 'messages', mode: NavigationMode.INTERNAL
     },
     {
-      imageClassPrefix: 'fas', imageClass: 'edit', text: 'Requests', route: '/', id: 'request', hasCounter: true, mode: NavigationMode.EVENT
+      imageClassPrefix: 'fas', imageClass: 'edit', text: 'Requests', route: '/', id: 'request', hasCounter: true, mode: NavigationMode.INTERNAL
     },
     {
       imageClassPrefix: 'fas', imageClass: 'th', text: 'Workspace', route: '/', id: 'workspace', mode: NavigationMode.INTERNAL
@@ -39,8 +39,8 @@ export class HeaderSampleComponent implements OnInit {
       imageClassPrefix: 'fas', imageClass: 'sign-out-alt', text: 'Sign Out', route: '/', id: 'signOut', mode: NavigationMode.INTERNAL
     }],
     navigationLinks: [
-      { text: 'Search', route: 'http://www.google.com', id: 'search', mode: NavigationMode.EXTERNAL },
-      { text: 'Databank', route: '/', id: 'databank', mode: NavigationMode.EVENT },
+      { text: 'Search', route: '/', id: 'search', mode: NavigationMode.INTERNAL },
+      { text: 'Databank', route: '/', id: 'databank', mode: NavigationMode.INTERNAL },
       { text: 'Data Services', route: '/', id: 'dataService', mode: NavigationMode.INTERNAL },
       { text: 'Help', route: '/', id: 'help', mode: NavigationMode.INTERNAL },
       {
@@ -53,8 +53,8 @@ export class HeaderSampleComponent implements OnInit {
       },
       {
         text: 'Link 4', id: 'link4', mode: NavigationMode.INTERNAL, route: undefined, children: [
-          { text: 'Link 4 sub 1', route: 'http://www.google.com', id: 'link4sub1', mode: NavigationMode.EXTERNAL },
-          { text: 'Link 4 sub 2', route: '/', id: 'link4sub2', mode: NavigationMode.EVENT },
+          { text: 'Link 4 sub 1', route: '/', id: 'link4sub1', mode: NavigationMode.INTERNAL },
+          { text: 'Link 4 sub 2', route: '/', id: 'link4sub2', mode: NavigationMode.INTERNAL },
           { text: 'Link 4 sub 3', route: '/', id: 'link4sub3', mode: NavigationMode.INTERNAL },
           { text: 'Link 4 sub 4', route: '/', id: 'link4sub4', mode: NavigationMode.INTERNAL }
         ]
