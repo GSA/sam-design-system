86318c784baf40aaf89253c3ea4f2dfe5ffb3795
Yerram_Shilpa <yerram_shilpa@Shilpas-MacBook-Pro.local>
2019-06-24 16:04:09 -0400

    comments added

--- libs/packages/sam-formly/src/lib/formly-filters/sds-filters.component.ts
+++ libs/packages/sam-formly/src/lib/formly-filters/sds-filters.component.ts
@@ -1,15 +1,15 @@
 import {
-    Component,
-    Input,
-    ChangeDetectionStrategy,
-    OnChanges
-  } from '@angular/core';
-  import { FormGroup } from '@angular/forms';
-  import { FormlyFieldConfig } from '@ngx-formly/core';
- 
-  @Component({
-    selector: 'sds-filters',
-    template: `
+  Component,
+  Input,
+  ChangeDetectionStrategy,
+  OnChanges
+} from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { FormlyFieldConfig } from '@ngx-formly/core';
+
+@Component({
+  selector: 'sds-filters',
+  template: `
     <sds-accordion multi="true">
         <sds-accordion-item>
             <sds-accordion-item-header> {{accordionLabel}} </sds-accordion-item-header>
@@ -17,13 +17,27 @@ import {
         </sds-accordion-item>
     </sds-accordion>
     `,
-    changeDetection: ChangeDetectionStrategy.OnPush
-  })
-  export class SdsFiltersComponent {
-    @Input() public form: FormGroup;
-    @Input() public fields: FormlyFieldConfig[];
-    @Input() public model: any;
-    @Input() accordionLabel: string = 'Filters';
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class SdsFiltersComponent {
+  /**
+  * Pass in a Form Group for ReactiveForms Support
+  */
+  @Input() public form: FormGroup;
 
-  }
+  /**
+  *  Fields are used to configure the UI components 
+  */
+  @Input() public fields: FormlyFieldConfig[];
   
+  /**
+  *  Model used to display the filter values.
+  */
+  @Input() public model: any;
+
+  /**
+  *  Accordion Label used to display the Accordion header text.
+  */
+  @Input() accordionLabel: string = 'Filters';
+
+}
