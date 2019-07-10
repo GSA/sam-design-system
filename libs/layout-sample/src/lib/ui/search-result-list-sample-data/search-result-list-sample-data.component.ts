62ef9f6d2d1ecb38c0f8bf90aca17ede11ba0ccf
samanthabaker <samantha.lynneb@gmail.com>
2019-07-10 11:56:15 -0400

    entity results list styling

--- libs/layout-sample/src/lib/ui/search-result-list-sample-data/search-result-list-sample-data.component.ts
+++ libs/layout-sample/src/lib/ui/search-result-list-sample-data/search-result-list-sample-data.component.ts
@@ -11,19 +11,4 @@ export class SearchResultListSampleDataComponent {
 
   @Input() model;
 
-}
-export class SubheaderSampleComponent {
-
-  subheader = {
-    buttons: [
-      { id: 'FirstButton', text: 'Button', class: 'usa-button--secondary' },
-      { id: 'SecondButton', text: 'Button', class: 'usa-button--primary' }
-    ],
-    actions: [
-      { id: 'DownloadBtn', icon: 'bars', text: 'Download' },
-      { id: 'FollowBtn', icon: 'bars', text: 'Follow' },
-      { id: 'ShareBtn', icon: 'bars', text: 'Share' }
-    ]
-  };
-
-}
+};
