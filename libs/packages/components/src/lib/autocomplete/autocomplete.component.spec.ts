7588bb123ae09a13c51e44173fe101a211650541
Thomas Tighe <ttighe@reisystems.com>
2019-07-12 10:10:32 -0400

    adding tests to autocomplete wrapper

--- libs/packages/components/src/lib/autocomplete/autocomplete.component.spec.ts
+++ libs/packages/components/src/lib/autocomplete/autocomplete.component.spec.ts
@@ -1,7 +1,9 @@
 /* tslint:disable */
 import { async, ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
 import { SDSAutocompleteComponent } from './autocomplete.component';
-
+import { AutoCompleteSampleDataService } from '../autocomplete-search/autocomplete-seach-test-service.spec';
+import { SDSAutocompletelConfiguration } from './models/SDSAutocompletelConfiguration.model';
+import { TreeMode, SDSSelectedItemModel } from '../selected-result/models/sds-selectedItem.model';
 import { FormsModule } from '@angular/forms';
 
 import { By } from '@angular/platform-browser';
@@ -24,7 +26,16 @@ describe('SDSAutocompleteComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(SDSAutocompleteComponent);
     component = fixture.componentInstance;
-
+    component.service = new AutoCompleteSampleDataService();
+    component.model = new SDSSelectedItemModel();
+    component.configuration = new SDSAutocompletelConfiguration();
+    component.configuration.id = 'autoId';
+    component.configuration.primaryKeyField = 'id';
+    component.model.treeMode = TreeMode.SINGLE;
+    component.configuration.primaryTextField = 'name';
+    component.configuration.secondaryTextField = 'subtext';
+    component.configuration.debounceTime = 0;
+    component.configuration.autocompletePlaceHolderText = '';
     fixture.detectChanges();
   });
 
