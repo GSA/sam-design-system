048339d39e2270cba6418f5cf031014b7c47cbfa
Thomas Tighe <ttighe@reisystems.com>
2019-07-12 13:39:50 -0400

    test page

--- libs/component-sample/src/lib/feature/autocomplete/autocomplete-sample.module.ts
+++ libs/component-sample/src/lib/feature/autocomplete/autocomplete-sample.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AutocompleteSampleComponent } from './autocomplete-sample.component';
 import { SdsAutocompleteModule } from '@gsa-sam/components';
+import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [AutocompleteSampleComponent],
   imports: [
-    CommonModule, SdsAutocompleteModule
+    CommonModule, SdsAutocompleteModule, FormsModule
   ], exports: [AutocompleteSampleComponent]
 })
 export class AutocompleteSampleModule { }
