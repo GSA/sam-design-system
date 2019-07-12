3b7561f62c4d9a07ede238523c3fd709871fa6ab
Thomas Tighe <ttighe@reisystems.com>
2019-07-12 12:39:39 -0400

    sample service

--- libs/component-sample/src/lib/feature/autocomplete/service/autocomplete-sample.service.ts
+++ libs/component-sample/src/lib/feature/autocomplete/service/autocomplete-sample.service.ts
@@ -1,10 +1,10 @@
 /* tslint:disable */
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
-import {SDSAutocomplteServiceInterface} from '@gsa-sam/components';
+import { SDSAutocomplteServiceInterface, SDSHiercarchicalServiceResult } from '@gsa-sam/components';
+import { map } from 'rxjs/operators';
 
-
-import { SampleAutocompleteData, SSDSSelectedItemModelel } from './autocomplete-sample.data';
+import { SampleAutocompleteData } from './autocomplete-sample.data';
 
 
 @Injectable()
@@ -22,18 +22,20 @@ export class AutocompleteSampleDataService implements SDSAutocomplteServiceInter
         this.loadedData = data;
     }
 
-    getDataByText(currentItems: number, searchValue?: string): Observable<SDSSelectedItemModel> {
+    getDataByText(currentItems: number, searchValue?: string): Observable<SDSHiercarchicalServiceResult> {
         let itemIncrease = 25;
         let data = of(this.loadedData);
         let itemsOb: Observable<Object[]>;
+
         if (searchValue) {
-            itemsOb = data.map(items => items.filter(itm =>
+            itemsOb = data.pipe(map(items => items.filter(itm =>
                 (itm.name.indexOf(searchValue) !== -1 ||
                     itm.subtext.indexOf(searchValue) !== -1
-                )));
+                ))));
         } else {
             itemsOb = data;
         }
+
         let items: object[];
         itemsOb.subscribe(
             (result) => {
@@ -53,12 +55,13 @@ export class AutocompleteSampleDataService implements SDSAutocomplteServiceInter
             totalItems: totalItemCount
         };
         return of(returnItem);
+
     }
 
 
 
 
-   
+
 
 
 
