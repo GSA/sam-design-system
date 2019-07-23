0d6c6fd8020f31578bbadcf2e216811c026abda8
samanthabaker <samantha.lynneb@gmail.com>
2019-07-23 15:45:09 -0400

    svg placholder

--- libs/layout-sample/src/lib/ui/search-result-list-sample-data/search-list-event-sample.module.ts
+++ libs/layout-sample/src/lib/ui/search-result-list-sample-data/search-list-event-sample.module.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { SearchResultListSampleDataComponent } from './search-result-list-sample-data.component';
 import { SdsSubheaderModule } from '@sam-design-system/layouts';
-
+import { IconSampleComponent } from '/libs/component-sample/src/lib/feature/icon-sample/icon-sample.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 
 @NgModule({
   declarations: [SearchResultListSampleDataComponent],
   imports: [
-    CommonModule, FormsModule, SdsSubheaderModule,
+    CommonModule, FormsModule, SdsSubheaderModule, FontAwesomeModule,
 
   ], exports: [SearchResultListSampleDataComponent]
 })
