12ff023b665b649c897b4f08bd926a0d85610976
Carlos Valle <carlosmvalle@gmail.com>
2019-08-02 15:30:27 -0400

    Fix close icon in autocomplete results

--- libs/packages/components/src/lib/autocomplete-search/autocomplete-search.module.ts
+++ libs/packages/components/src/lib/autocomplete-search/autocomplete-search.module.ts
@@ -4,13 +4,20 @@ import { FormsModule } from '@angular/forms';
 import { SDSAutocompleteSearchComponent } from './autocomplete-search.component';
 import { RouterModule } from '@angular/router';
 import { SDSClickOutsideModule } from '../click-outside/click-outside.module';
-import { SdsTabOutsideModule } from '../tab-outside/taboutside.module'; 
+import { SdsTabOutsideModule } from '../tab-outside/taboutside.module';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
 @NgModule({
   imports: [
-    CommonModule, FormsModule, RouterModule,
-    SDSClickOutsideModule,SdsTabOutsideModule
+    CommonModule,
+    FormsModule,
+    RouterModule,
+    SDSClickOutsideModule,
+    SdsTabOutsideModule,
+    FontAwesomeModule
   ],
   declarations: [SDSAutocompleteSearchComponent],
   exports: [SDSAutocompleteSearchComponent]
 })
-export class SdsAutocompleteSearchModule { }
+
+export class SdsAutocompleteSearchModule {}
--- libs/packages/components/src/lib/selected-result/selected-result.module.ts
+++ libs/packages/components/src/lib/selected-result/selected-result.module.ts
@@ -3,12 +3,11 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { SDSSelectedResultComponent } from './selected-result.component';
 import { RouterModule } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
-  imports: [
-    CommonModule, FormsModule, RouterModule
-  ],
+  imports: [CommonModule, FormsModule, RouterModule, FontAwesomeModule],
   declarations: [SDSSelectedResultComponent],
   exports: [SDSSelectedResultComponent]
 })
-export class SdsSelectedResultsModule { }
+export class SdsSelectedResultsModule {}
