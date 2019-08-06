ace29dd11ee7e536b96c49b555f613a2d75a81ee
Thomas Tighe <ttighe@reisystems.com>
2019-08-06 10:04:46 -0400

    remove non existant import

--- libs/layout-sample/src/lib/ui/search-result-list-sample-data/search-list-event-sample.module.ts
+++ libs/layout-sample/src/lib/ui/search-result-list-sample-data/search-list-event-sample.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { SearchResultListSampleDataComponent } from './search-result-list-sample-data.component';
 import { SdsSubheaderModule } from '@sam-design-system/layouts';
-import { IconSampleComponent } from '/libs/component-sample/src/lib/feature/icon-sample/icon-sample.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 
