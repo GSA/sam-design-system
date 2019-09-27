8fc7e186da8f58f22a41ad2abb68ad192e2f6a29
Carlos Valle <carlos.m.valle@gmail.com>
2019-09-27 18:22:20 -0400

    Update header component to match new styles

--- libs/component-sample/src/lib/feature/header-sample/header-sample.component.ts
+++ libs/component-sample/src/lib/feature/header-sample/header-sample.component.ts
@@ -1,69 +1,94 @@
 import { Component, OnInit } from '@angular/core';
-import { BehaviorSubject } from "rxjs";
-import { HeaderModel, NavigationMode } from '@gsa-sam/components'
+import { BehaviorSubject } from 'rxjs';
+import { HeaderModel, NavigationMode } from '@gsa-sam/components';
 @Component({
   selector: 'gsa-sam-header-sample',
   templateUrl: './header-sample.component.html',
   styleUrls: ['./header-sample.component.scss']
 })
 export class HeaderSampleComponent implements OnInit {
-
-  constructor() { }
-
+  constructor() {}
 
   public linkEvent = new BehaviorSubject<object>(null);
 
-
-
-  ngOnInit() {
-
-    this.linkEvent.subscribe(
-      value => {
-        console.log('Link Event Clicked Change');
-        console.log(value);
-      }
-    );
-  }
-
   modelHeader: HeaderModel = {
-    secondaryLinks: [{
-      imageClassPrefix: 'fas', imageClass: 'comment', text: 'Messages', route: '/', id: 'messages', mode: NavigationMode.INTERNAL
-    },
-    {
-      imageClassPrefix: 'fas', imageClass: 'edit', text: 'Requests', route: '/', id: 'request', hasCounter: true, mode: NavigationMode.INTERNAL
-    },
-    {
-      imageClassPrefix: 'fas', imageClass: 'th', text: 'Workspace', route: '/', id: 'workspace', mode: NavigationMode.INTERNAL
-    },
-    {
-      imageClassPrefix: 'fas', imageClass: 'sign-out-alt', text: 'Sign Out', route: '/', id: 'signOut', mode: NavigationMode.INTERNAL
-    }],
-    navigationLinks: [
-      { text: 'Search', route: '/', id: 'search', mode: NavigationMode.INTERNAL },
-      { text: 'Databank', route: '/', id: 'databank', mode: NavigationMode.INTERNAL },
-      { text: 'Data Services', route: '/', id: 'dataService', mode: NavigationMode.INTERNAL },
-      { text: 'Help', route: '/', id: 'help', mode: NavigationMode.INTERNAL },
+    secondaryLinks: [
       {
-        text: 'Link 3', id: 'link3', mode: NavigationMode.INTERNAL, route: undefined, children: [
-          { text: 'Link 3 sub 1', route: '/', id: 'link3sub1', mode: NavigationMode.INTERNAL },
-          { text: 'Link 3 sub 2', route: '/', id: 'link3sub2', mode: NavigationMode.INTERNAL },
-          { text: 'Link 3 sub 3', route: '/', id: 'link3sub3', mode: NavigationMode.INTERNAL },
-          { text: 'Link 3 sub 4', route: '/', id: 'link3sub4', mode: NavigationMode.INTERNAL }
-        ]
+        imageClassPrefix: 'sds',
+        imageClass: 'request',
+        text: 'Requests',
+        route: '/',
+        id: 'request',
+        hasCounter: true,
+        selected: true,
+        mode: NavigationMode.INTERNAL
       },
       {
-        text: 'Link 4', id: 'link4', mode: NavigationMode.INTERNAL, route: undefined, children: [
-          { text: 'Link 4 sub 1', route: '/', id: 'link4sub1', mode: NavigationMode.INTERNAL },
-          { text: 'Link 4 sub 2', route: '/', id: 'link4sub2', mode: NavigationMode.INTERNAL },
-          { text: 'Link 4 sub 3', route: '/', id: 'link4sub3', mode: NavigationMode.INTERNAL },
-          { text: 'Link 4 sub 4', route: '/', id: 'link4sub4', mode: NavigationMode.INTERNAL }
-        ]
+        imageClassPrefix: 'sds',
+        imageClass: 'messages',
+        text: 'Messages',
+        route: '/',
+        id: 'messages',
+        mode: NavigationMode.INTERNAL
+      },
+      {
+        imageClassPrefix: 'sds',
+        imageClass: 'workspace',
+        text: 'Workspace',
+        route: '/',
+        id: 'workspace',
+        mode: NavigationMode.INTERNAL
+      },
+      {
+        imageClassPrefix: 'sds',
+        imageClass: 'log-out',
+        text: 'Sign Out',
+        route: '/',
+        id: 'signOut',
+        mode: NavigationMode.INTERNAL
       }
     ],
+    navigationLinks: [
+      {
+        text: 'Home',
+        selected: true,
+        route: '/',
+        id: 'home',
+        mode: NavigationMode.INTERNAL
+      },
+      {
+        text: 'Search',
+        route: '/',
+        id: 'search',
+        mode: NavigationMode.INTERNAL
+      },
+      {
+        text: 'Databank',
+        route: '/',
+        id: 'databank',
+        mode: NavigationMode.INTERNAL
+      },
+      {
+        text: 'Data Services',
+        route: '/',
+        id: 'dataService',
+        mode: NavigationMode.INTERNAL
+      },
+      { text: 'Help', route: '/', id: 'help', mode: NavigationMode.INTERNAL }
+    ],
     home: {
-      text: 'Home',
+      text: 'Logo',
+      logo: '/assets/img/logo-sam.svg',
       route: '/',
-      id: 'home', mode: NavigationMode.INTERNAL
+      id: 'home',
+      mode: NavigationMode.INTERNAL
     }
   };
+
+  ngOnInit() {
+    this.linkEvent.subscribe(value => {
+      console.log('Link Event Clicked Change');
+      console.log(value);
+    });
+  }
 }
