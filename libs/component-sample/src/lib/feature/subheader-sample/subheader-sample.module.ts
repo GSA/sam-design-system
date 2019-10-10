566c4dddb26798e9dfbb796796dfae75f0090ff2
Carlos Valle <carlos.m.valle@gmail.com>
2019-10-10 17:14:01 -0400

    refactor(subheader): update subheader sample page

--- libs/component-sample/src/lib/feature/subheader-sample/subheader-sample.module.ts
+++ libs/component-sample/src/lib/feature/subheader-sample/subheader-sample.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { SdsSubheaderModule } from '@sam-design-system/layouts';
 import { SubheaderSampleComponent } from './subheader-sample.component';
+import { SdsSearchModule } from '@gsa-sam/components';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
-  imports: [SdsSubheaderModule],
+  imports: [FontAwesomeModule, SdsSubheaderModule, SdsSearchModule],
   exports: [],
   declarations: [SubheaderSampleComponent]
 })
