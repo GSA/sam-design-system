2ccbfddf511e6de30395496cb30e89ebec385d97
Thomas Tighe <ttighe@reisystems.com>
2019-10-29 13:22:55 -0400

    move error message

--- libs/packages/sam-formly/src/lib/formly/wrappers/form-field.wrapper.ts
+++ libs/packages/sam-formly/src/lib/formly/wrappers/form-field.wrapper.ts
@@ -4,20 +4,19 @@ import { FieldWrapper } from '@ngx-formly/core';
 @Component({
   selector: 'sds-formly-wrapper-form-field',
   template: `
-  <div class="usa-form-group" [class.usa-form-group--error]="showError">
+<div class="usa-form-group" [class.usa-form-group--error]="showError">
   <label class="usa-label" *ngIf="to.label && to.hideLabel !== true" [attr.for]="id">
-
   <span *ngIf="to.tagText" class="usa-tag"
   [ngClass]="to.tagClass ? to.tagClass : 'sds-tag--info-white'">
   {{to.tagText}}</span> 
   <span>{{ to.label }}</span>
     <span *ngIf="to.required && to.hideRequiredMarker !== true">*</span>
-  </label>
-      <ng-template #fieldComponent></ng-template>
-      <div *ngIf="showError" class="usa-error-message" [style.display]="'block'">
+  </label>  
+  <div *ngIf="showError" class="usa-error-message" [style.display]="'block'">
     <formly-validation-message [field]="field"></formly-validation-message>
   </div>
   <small *ngIf="to.description" class="form-text text-muted">{{ to.description }}</small>
+  <ng-template #fieldComponent></ng-template>
 </div>
   `,
 })
