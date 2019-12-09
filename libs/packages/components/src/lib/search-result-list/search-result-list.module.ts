016638c554cfc6c8f45e8eabe7232e66cbea1164
Samantha Baker <38532587+samanthabaker@users.noreply.github.com>
2019-12-09 13:01:07 -0500

    add sds icon lib

--- libs/packages/components/src/lib/search-result-list/search-result-list.module.ts
+++ libs/packages/components/src/lib/search-result-list/search-result-list.module.ts
@@ -2,7 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { SdsSearchResultListComponent } from './search-result-list.component';
+
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { sds } from '@gsa-sam/sam-styles/src/icons/';
+import { fas } from '@fortawesome/free-solid-svg-icons';
 
 @NgModule({
   imports: [
@@ -11,4 +15,8 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
   declarations: [SdsSearchResultListComponent],
   exports: [SdsSearchResultListComponent]
 })
-export class SdsSearchResultListModule { }
+export class SdsSearchResultListModule {
+  constructor() {
+    library.add(fas, sds);
+  }
+ }
