05c291966768c36bef25bd965a6171a297532fe9
Alejandro Colina <58086564+AlejandroColinaGSA@users.noreply.github.com>
2019-12-17 09:23:04 -0500

    implemented feature passtrough

--- libs/component-sample/src/lib/feature/text-editor-sample/text-editor-sample.component.ts
+++ libs/component-sample/src/lib/feature/text-editor-sample/text-editor-sample.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'sds-text-editor-sample',
   templateUrl: './text-editor-sample.component.html'
 })
 export class TextEditorSampleComponent {
-
+  public sdsTextEditor: string;
+  public modelData: string;
   constructor() { }
+ 
+  OnInit() {
+      this.sdsTextEditor = 'Hello Text Editor!';
+
+  }
+ 
+  TxtEdtOnBlur(event: any) {
+    // console.log(event);
+  }
+ 
+  TxtEdtOnFocus(event: any){
+    // console.log(event);
+  }
+ 
+  TxtEdtEventData(event: any){
+    // console.log(event);
+  }
+ 
+  TxtEdtDataChange(event: any) {
+ 
+  }
 
 
 
--- libs/packages/components/src/lib/text-editor/text-editor.component.ts
+++ libs/packages/components/src/lib/text-editor/text-editor.component.ts
@@ -1,30 +1,52 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
 import { CKEditor4 } from 'ckeditor4-angular';
 
 @Component({
   selector: 'sds-text-editor',
   templateUrl: './text-editor.component.html',
-  inputs:['typeOfEditor']
+  
 })
 export class SdsTextEditorComponent implements OnInit {
 
-  @Input('editorData') ckEditorData: any;
+  
+  TextData: string;
+  readonly: boolean;
+  editorData: string;
+ 
+  // tslint:disable-next-line: no-input-rename
+  @Input("Textdata") TextEditordata: string;
+ 
+  // tslint:disable-next-line: no-input-rename
+  @Input("ngModelData") ngModelData: string;
+ 
+  @Output() TextEditerOnChange: EventEmitter<any> = new EventEmitter();
+  @Output() TextEditorDataChange: EventEmitter<any> = new EventEmitter();
+  @Output() TextEditorOnFocus: EventEmitter<any> = new EventEmitter();
+  @Output() TextEditorOnBlur: EventEmitter<any> = new EventEmitter();
+
+  constructor() {}
+ 
+  ngOnInit() {}
 
 
-  constructor() { 
 
+  // public onChange(event: CKEditor4.EventInfo) {
+  //   console.log(event.editor.getData());
+  // }
+
+  onBlurEvent(event: CKEditor4.EventInfo) {
+    this.TextEditorOnBlur.emit(event.editor.getData());
   }
  
-  public editorData = '<p>Hello, world!</p>';
-  public model = {
-    editorData: '<p>Hello, world!</p>'
-  };
-
-  ngOnInit() {
+  onFocusChange(event: CKEditor4.EventInfo) {
+    this.TextEditorOnFocus.emit(event.editor.getData());
   }
-
-  public onChange(event: CKEditor4.EventInfo) {
-    console.log(event.editor.getData());
+    public onChange( event: CKEditor4.EventInfo ) {
+      this.TextEditerOnChange.emit(event.editor.getData());
   }
-
+ 
+  onDataChange(event: CKEditor4.EventInfo) {
+    this.TextEditorDataChange.emit(event.editor.getData());
+  }
+ 
 }
