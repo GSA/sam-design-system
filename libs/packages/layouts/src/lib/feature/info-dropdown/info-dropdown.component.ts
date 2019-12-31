55c422d8c4c9852e1045dcbddcc5f14eb54b3847
Alejandro Colina <58086564+AlejandroColinaGSA@users.noreply.github.com>
2019-12-31 09:05:06 -0500

    init

---
+++ libs/packages/layouts/src/lib/feature/info-dropdown/info-dropdown.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'sds-info-dropdown',
+  templateUrl: './info-dropdown.component.html',
+  styleUrls: ['./info-dropdown.component.scss']
+})
+export class SdsInfoDropdownComponent  {
+
+  constructor() { }
+
+  
+
+}
