6217f6a3dd39bccf94972af5cb92df16b8b002c3
Hemal1975 <hemal6512@gmail.com>
2020-01-09 12:54:34 -0500

    commit changes for VP

--- libs/component-sample/src/lib/feature/video-player-sample/video-player-sample.component.ts
+++ libs/component-sample/src/lib/feature/video-player-sample/video-player-sample.component.ts
@@ -7,10 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VideoPlayerSampleComponent implements OnInit {
   source: string;
+  height: string;
+  width: string;
+	caption: string;
+  poster: string;
+	controls: string;
+  id: string;
+	fallback: string;
+	seekInterval: number;
+	debug: string;
+	preload: string;
+
   constructor() { }
 
   ngOnInit() {
     this.source = "http://www.html5videoplayer.net/videos/toystory.mp4";
+    this.height = "360";
+    this.width = "640";
+    this.id = "100";
+    this.caption ="";
+    this.poster = "";
   }
 
 }
--- libs/packages/components/src/lib/video-player/video-player.component.ts
+++ libs/packages/components/src/lib/video-player/video-player.component.ts
@@ -7,13 +7,30 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class SdsVideoPlayerComponent implements OnInit {
 
+  InitPxVideo: Object;
 
-  @Input('videoSource') vidioSource: string;
 
-  constructor() { }
+  @Input('videoSource') videoSource: string;
+  @Input('videoHeight') videoHeight: string;
+  @Input('videoWidth') videoWidth: string;
+  @Input('myvid') myvid: string;
+  @Input('videoCaption') videoCaption: string;
+  @Input('videoPoster') videoPoster: string;
+
+  constructor() {
+
+   }
 
   ngOnInit() {
-
+    this.InitPxVideo = new Object();
+    this.InitPxVideo = {
+      "videoId": "myvid",
+      "captionsOnDefault": true,
+      "seekInterval": 20,
+      "videoTitle": "clips of stand-up comedy",
+      "debug": true
+    }
+    console.log(this.InitPxVideo);
   }
 
 
