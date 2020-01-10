0318179bc0637cabedd3898808554dc1f1545669
Hemal1975 <hemal6512@gmail.com>
2020-01-10 11:32:19 -0500

    Commit for initial

--- libs/packages/components/src/lib/video-player/video-player.component.ts
+++ libs/packages/components/src/lib/video-player/video-player.component.ts
@@ -18,7 +18,15 @@ export class SdsVideoPlayerComponent implements OnInit, AfterViewInit {
   @Input('videoPoster') videoPoster: string;
 
   constructor(private window: Window) {
-
+      // this.InitPxVideo = new Object();
+      this.InitPxVideo = ({
+        "videoId": "myvid",
+    "captionsOnDefault": true,
+    "seekInterval": 20,
+    "videoTitle": "clips of stand-up comedy",
+    "debug": true
+      });
+      console.log(this.InitPxVideo);
    }
 
 
