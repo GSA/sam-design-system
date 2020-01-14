dee06d5d7f59084f7e0af3f98fa2b1b12faf434f
Alejandro Colina <58086564+AlejandroColinaGSA@users.noreply.github.com>
2020-01-14 14:06:39 -0500

    fixes to popup alignment

--- libs/component-sample/src/lib/feature/search-result-list-sample/popup-sample.component.ts
+++ libs/component-sample/src/lib/feature/search-result-list-sample/popup-sample.component.ts
@@ -6,8 +6,11 @@ import { Component} from '@angular/core';
     <div class="sds-popup">
     <fa-icon [fixedWidth]="true" [icon]="['sds', 'information-circle']" size="2x"></fa-icon>
     <div class="sds-popup__content">
-      <fa-icon [fixedWidth]="true" [icon]="['sds', 'information-circle']" size="2x"
-        class="float-right popup__inner-icon"></fa-icon>
+      <div class="display-flex flex-column flex-align-end margin-right-05">
+        <div>
+          <fa-icon [fixedWidth]="true" [icon]="['sds', 'information-circle']" size="2x" class="popup__inner-icon"></fa-icon>
+        </div>
+      </div>
       <ul>
         <li>Showing a newer version of this entity</li>
         <li>Fields with dotted lines indicate updated data</li>
