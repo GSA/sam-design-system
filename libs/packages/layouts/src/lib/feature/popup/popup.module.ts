5f0d27a4816d079c86861fb54411242df76c22df
Alejandro Colina <58086564+AlejandroColinaGSA@users.noreply.github.com>
2020-01-14 14:34:38 -0500

    initial commit

---
+++ libs/packages/layouts/src/lib/feature/popup/popup.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SdsPopupComponent } from './popup.component';
+
+@NgModule({
+  imports: [CommonModule],
+  exports: [SdsPopupComponent],
+  declarations: [SdsPopupComponent]
+})
+export class SdsPopupModule { }
