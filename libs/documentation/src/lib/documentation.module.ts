746741e2dc5e48fb326c626650fe285d25a684f0
davereed <dave@contagious.ly>
2020-01-15 00:12:41 -0500

    Add overview and intro pages

--- libs/documentation/src/lib/documentation.module.ts
+++ libs/documentation/src/lib/documentation.module.ts
@@ -1,3 +1,6 @@
+import { IntroductionModule } from './pages/introduction/introduction.module';
+import { IntroductionComponent } from './pages/introduction/introduction.component';
+import { OverviewModule } from './pages/overview/overview.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
@@ -6,15 +9,18 @@ import { sds } from '@gsa-sam/sam-styles/src/icons/';
 import { fas } from '@fortawesome/free-solid-svg-icons';
 import { ROUTES as HEADER_ROUTES, HeaderModule } from './components/header/header.module';
 import { DocumentationSharedModule } from './shared';
+import { OverviewComponent } from './pages/overview/overview.component';
 
 export const ROUTES: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'components/header' },
+  { path: 'overview', component: OverviewComponent },
+  { path: 'introduction', component: IntroductionComponent },
   { path: 'components', pathMatch: 'full', redirectTo: 'components/alert' },
   { path: 'components/header', children: HEADER_ROUTES },
 ];
 
 @NgModule({
-  imports: [CommonModule, DocumentationSharedModule, RouterModule.forChild(ROUTES), HeaderModule]
+  imports: [CommonModule, DocumentationSharedModule, RouterModule.forChild(ROUTES), HeaderModule, OverviewModule, IntroductionModule]
 })
 export class DocumentationModule {
   constructor() {
---
+++ libs/documentation/src/lib/pages/introduction/introduction.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  templateUrl: './introduction.component.html'
+})
+
+export class IntroductionComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() { }
+}
