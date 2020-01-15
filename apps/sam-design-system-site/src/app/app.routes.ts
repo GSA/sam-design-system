5562c3ad63921ad8a41161f155beb3b80b8cb133
davereed <dave@contagious.ly>
2020-01-15 00:21:54 -0500

    Update default route

--- apps/sam-design-system-site/src/app/app.routes.ts
+++ apps/sam-design-system-site/src/app/app.routes.ts
@@ -4,6 +4,7 @@ export const ROUTES: Routes = [
   { path: 'component', loadChildren: '@gsa-sam/component-sample#ComponentSampleModule' } ,
   { path: 'layout', loadChildren: '@gsa-sam/layout-sample#LayoutSampleModule' } ,
   { path: 'formly', loadChildren: '@gsa-sam/sam-formly-sample#SamFormlySampleModule' },
-  { path: 'documentation', loadChildren: '@gsa-sam/documentation#DocumentationModule' }
+  { path: 'documentation', loadChildren: '@gsa-sam/documentation#DocumentationModule' },
+  { path: '**', redirectTo: 'documentation/overview' }
 
 ];
