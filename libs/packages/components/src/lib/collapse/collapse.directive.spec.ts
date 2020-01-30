d9b12673fdc72661c703d121a57198204741c969
Hemal1975 <hemal6512@gmail.com>
2020-01-30 17:45:43 -0500

    unit test

--- libs/packages/components/src/lib/collapse/collapse.directive.spec.ts
+++ libs/packages/components/src/lib/collapse/collapse.directive.spec.ts
@@ -1,13 +1,37 @@
 import { CollapseDirective } from "./collapse.directive";
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { CollapseSampleComponent } from '../../../../../component-sample/src/lib/feature/collapse-sample/collapse-sample.component';
+import { By } from '@angular/platform-browser';
 
-describe('CollapseDirective', ()=>{
-  let directive: CollapseDirective;
 
-  beforeEach(() => {
-    directive = new CollapseDirective();
+
+describe('CollapseSampleComponent', () => {
+  let component: CollapseSampleComponent;
+
+  beforeEach(async(() => {
+
+    component = new CollapseSampleComponent();
+
+    TestBed.configureTestingModule({
+      declarations: [ CollapseDirective, CollapseSampleComponent]
+    })
+    .compileComponents();
+  }));
+
+   it('should be able to test directive', async(() => {
+    TestBed.overrideComponent(CollapseSampleComponent, {
+      set: {
+        template: '<div sdsCollapse></div>'
+      }
+    });
+  }));
+
+  it('check directive value', () => {
+      const fixture = TestBed.createComponent(CollapseSampleComponent);
+      const directiveEl = fixture.debugElement.query(By.directive(CollapseDirective));
+      expect(directiveEl).not.toBeNull();
+      const directiveInstance = directiveEl.injector.get(CollapseDirective);
+      expect(directiveInstance.collapsed).toBeTruthy();
   });
 
-  it('check for input value', () => {
-    expect(directive.collapsed).toEqual(true);
-  });
 });
