190e8b861c0f57ca3beb2512db52c64743afa877
AJAYPAL SINGH BAL <ajaypal.singh.bal@accenturefederal.com>
2020-01-31 09:48:01 -0500

    update button.spec.ts

--- libs/packages/sam-formly/src/lib/formly/types/button.spec.ts
+++ libs/packages/sam-formly/src/lib/formly/types/button.spec.ts
@@ -1,86 +1,46 @@
+
 import { FormlyModule, FormlyForm } from '@ngx-formly/core';
 import { FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormlyBootstrapModule } from '@ngx-formly/bootstrap';
 import { FormlyFieldButtonComponent } from './button';
 import { Component, ViewChild } from '@angular/core';
-import {
-  TestBed,
-  ComponentFixture
-} from '@angular/core/testing';
+import { TestBed, ComponentFixture } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { FormlySelectModule } from '@ngx-formly/core/select';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
+
 const createTestComponent = (html: string) =>
-  createGenericTestComponent(html, TestComponent) as ComponentFixture<
-    TestComponent
-  >;
+    createGenericTestComponent(html, TestComponent) as ComponentFixture<TestComponent>;
 
-export function createGenericTestComponent<T>(
-  html: string,
-  type: { new (...args: any[]): T }
-): ComponentFixture<T> {
-  TestBed.overrideComponent(type, { set: { template: html } });
-  const fixture = TestBed.createComponent(type);
-  fixture.detectChanges();
-  return fixture as ComponentFixture<T>;
-}
+export function createGenericTestComponent<T>(html: string, type: { new(...args: any[]): T }): ComponentFixture<T> {
+        TestBed.overrideComponent(type, { set: { template: html } });
+        const fixture = TestBed.createComponent(type);
+        fixture.detectChanges();
+        return fixture as ComponentFixture<T>;
+    }
 
-let testComponentButtons;
+    let testComponentButtons;
 
-describe('Formly Field button Component', () => {
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [TestComponent, FormlyFieldButtonComponent],
-      imports: [
-        NoopAnimationsModule,
-        ReactiveFormsModule,
-        FormlySelectModule,
-        FormlyModule.forRoot({
-          types: [
-            {
-              name: 'button',
-              component: FormlyFieldButtonComponent
-            }
-          ]
-        })
-      ]
-    });
-  });
-
-  describe('button', () => {
-    beforeEach(() => {
-      testComponentButtons = {
-        form: new FormGroup({}),
-        options: {},
-        model: {}
-      };
-    });
-
-    it('should test click', () => {
-      testComponentButtons.fields = [
-        {
-          key: 'button-test',
-          type: 'button',
-          templateOptions: {
-            text: 'Show Extension',
-            btnType: 'info',
-            onClick: $event => {
-              this.model.showExtension = 'show';
-            }
-          }
-        }
-      ];
-
-      const fixture = createTestComponent(
-        '<formly-form [form]="form" [fields]="fields" [model]="model"></formly-form>'
-      );
-      const buttonField: any = fixture.debugElement.query(
-        By.css('.usa-button--unstyled')
-      );
-      const spy = spyOn(buttonField.nativeElement, 'click');
-
-      buttonField.nativeElement.click();
-      fixture.detectChanges();
+    describe('Formly Field button Component', () => {
+        beforeEach(() => {
+            TestBed.configureTestingModule({
+                declarations: [TestComponent, FormlyFieldButtonComponent],
+                imports: [
+                    NoopAnimationsModule,
+                    ReactiveFormsModule,
+                    FormlySelectModule,
+                    FormlyModule.forRoot({
+                        types: [
+                            {
+                                name: 'button',
+                                component: FormlyFieldButtonComponent,
+                            },
+                        ],
+                    }),
+                ],
+            });
+        });
 
         describe('button', () => {
             beforeEach(() => {
@@ -142,21 +102,14 @@ describe('Formly Field button Component', () => {
             
         });
 
-      expect(spy).toHaveBeenCalled();
-    });
-  });
 });
 
-@Component({
-  selector: 'formly-form-button',
-  template: '',
-  entryComponents: []
-})
+@Component({ selector: 'formly-form-button', template: '', entryComponents: [] })
 class TestComponent {
-  @ViewChild(FormlyForm) formlyForm: FormlyForm;
+    @ViewChild(FormlyForm) formlyForm: FormlyForm;
 
-  fields = testComponentButtons.fields;
-  form: FormGroup = testComponentButtons.form;
-  model = testComponentButtons.model || {};
-  options = testComponentButtons.options;
+    fields = testComponentButtons.fields;
+    form: FormGroup = testComponentButtons.form;
+    model = testComponentButtons.model || {};
+    options = testComponentButtons.options;
 }
