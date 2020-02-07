86088341b669b5fd2b76ec377d8c0f41df375127
Hemal1975 <hemal6512@gmail.com>
2020-02-07 11:40:12 -0500

    commit code

--- libs/component-sample/src/lib/feature/video-player-sample/video-player-sample.module.ts
+++ libs/component-sample/src/lib/feature/video-player-sample/video-player-sample.module.ts
@@ -1,8 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { VideoPlayerSampleComponent } from './video-player-sample.component';
-import { SdsVideoPlayerModule } from '../../../../../packages/components/src/lib/video-player/video-player.module';
-
+import { SdsVideoPlayerModule } from '@gsa-sam/components';
 
 @NgModule({
   declarations: [VideoPlayerSampleComponent],
