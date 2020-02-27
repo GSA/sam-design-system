6d60a77fce55eeb81eee8de51d40145734ac1874
Hemal1975 <hemal6512@gmail.com>
2020-02-26 20:29:55 -0500

    commit changes

--- libs/packages/components/src/lib/popup/popup.directive.ts
+++ libs/packages/components/src/lib/popup/popup.directive.ts
@@ -5,7 +5,7 @@ import { Directive, Input, ElementRef, Renderer2, HostListener, OnInit, OnChange
   selector: '[sdsPopup]',
   exportAs: 'sdsPopup'
 })
-export class SdsPopupDirective {
+export class SdsPopupDirective implements OnInit, AfterViewInit{
    position:string;
    sdsPopupContent: string;
    place: string;
@@ -13,54 +13,54 @@ export class SdsPopupDirective {
     sdsPopupDire: HTMLElement;
 
 
-//   constructor(private el: ElementRef, private renderer: Renderer2) {
-//     this.renderer.addClass(this.el.nativeElement, 'sds-popup');
-//   }
+  constructor(private el: ElementRef, private renderer: Renderer2) {
+    this.renderer.addClass(this.el.nativeElement, 'sds-popup');
+  }
 
-//   ngOnInit(){
+  ngOnInit(){
 
-//   }
+  }
 
-//   ngAfterViewInit() {
-//     this.mainContentDiv = this.el.nativeElement.querySelector('.sds-popup__content');
-//     this.sdsPopupContent = this.mainContentDiv.getAttribute('sdsPopupContent');
-//     this.position = this.mainContentDiv.getAttribute('position');
-//     this.place = this.mainContentDiv.getAttribute('place');
-//  }
+  ngAfterViewInit() {
+    this.mainContentDiv = this.el.nativeElement.querySelector('.sds-popup__content');
+    this.sdsPopupContent = this.mainContentDiv.getAttribute('sdsPopupContent');
+    this.position = this.mainContentDiv.getAttribute('position');
+    this.place = this.mainContentDiv.getAttribute('place');
+ }
 
-//   @HostListener('mouseenter') onMouseEnter() {
-//     this.show();
-//     // if (!this.sdsPopupDire) { this.show(); }
-//   }
+  @HostListener('mouseenter') onMouseEnter() {
+    this.show();
+    // if (!this.sdsPopupDire) { this.show(); }
+  }
 
-//   @HostListener('mouseleave') onMouseLeave() {
-//     if (this.sdsPopupDire) { this.hide(); }
-//   }
+  @HostListener('mouseleave') onMouseLeave() {
+    if (this.sdsPopupDire) { this.hide(); }
+  }
 
 
-//   // show() {
+  // show() {
 
-//   //   // this.renderer.appendChild(this.el.nativeElement, this.sdsPopupDire);
-//   // }
+  //   // this.renderer.appendChild(this.el.nativeElement, this.sdsPopupDire);
+  // }
 
-//   hide() {
-//     // this.renderer.removeClass(this.sdsPopupDire, 'sds-tooltip-show');
-//     this.renderer.removeChild(document.body, this.sdsPopupDire);
-//     window.setTimeout(() => {
-//       // this.renderer.removeChild(document.body, this.sdsPopupDire);
-//       // this.sdsPopupDire = null;
-//     });
-//   }
+  hide() {
+    // this.renderer.removeClass(this.sdsPopupDire, 'sds-tooltip-show');
+    this.renderer.removeChild(document.body, this.sdsPopupDire);
+    window.setTimeout(() => {
+      // this.renderer.removeChild(document.body, this.sdsPopupDire);
+      // this.sdsPopupDire = null;
+    });
+  }
 
-//   show() {
-//     this.renderer.addClass(this.mainContentDiv, this.place);
-//     this.renderer.addClass(this.mainContentDiv, this.position);
-//     this.sdsPopupDire = this.renderer.createElement('div');
-//     this.renderer.addClass(this.sdsPopupDire, 'tooltip');
-//     this.renderer.addClass(this.sdsPopupDire, 'tooltip-left');
-//     this.renderer.appendChild(
-//       this.sdsPopupDire,
-//       this.renderer.createText(this.sdsPopupContent)); // textNode
-//       this.renderer.appendChild(this.mainContentDiv, this.sdsPopupDire);
-//   }
+  show() {
+    this.renderer.addClass(this.mainContentDiv, this.place);
+    this.renderer.addClass(this.mainContentDiv, this.position);
+    this.sdsPopupDire = this.renderer.createElement('div');
+    this.renderer.addClass(this.sdsPopupDire, 'tooltip');
+    this.renderer.addClass(this.sdsPopupDire, 'tooltip-left');
+    this.renderer.appendChild(
+      this.sdsPopupDire,
+      this.renderer.createText(this.sdsPopupContent)); // textNode
+      this.renderer.appendChild(this.mainContentDiv, this.sdsPopupDire);
+  }
 }
