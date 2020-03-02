a15f4045c7718377545ec06fbfa75ec55b0e4b36
Samantha Baker <38532587+samanthabaker@users.noreply.github.com>
2020-03-02 11:39:44 -0500

    add feedback to footer

--- libs/component-sample/src/lib/feature/footer-sample/footer-sample.module.ts
+++ libs/component-sample/src/lib/feature/footer-sample/footer-sample.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FooterSampleComponent } from './footer-sample.component';
 import { SdsFooterModule } from '../../../../../packages/components/src/lib/footer/footer.module';
+import { CollapseSampleComponent } from '../../collapse-sample.component';
+import { SdsCollapseModule } from '../../../../../../libs/packages/components/src/lib/collapse/collapse.module';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
 @NgModule({
   declarations: [FooterSampleComponent],
   imports: [
-    CommonModule, SdsFooterModule
+    CommonModule, SdsFooterModule, SdsCollapseModule, FontAwesomeModule
   ], exports: [FooterSampleComponent]
 })
 export class FooterSampleModule { }
