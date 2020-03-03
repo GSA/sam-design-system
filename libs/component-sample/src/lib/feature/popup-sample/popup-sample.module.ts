1233bab309e69938f146666bdbf349c0ed05dd6d
Hemal1975 <hemal6512@gmail.com>
2020-03-03 17:41:47 -0500

    Update popup-sample.module.ts

--- libs/component-sample/src/lib/feature/popup-sample/popup-sample.module.ts
+++ libs/component-sample/src/lib/feature/popup-sample/popup-sample.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { PopupSampleComponent } from './popup-sample.component';
 import { SdsPopupModule } from '@gsa-sam/components';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { IconSampleModule } from '../icon-sample/icon-sample.module';
+// import { IconSampleModule } from '../icon-sample/icon-sample.module';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { sds } from '@gsa-sam/sam-styles/src/icons/';
 import { fas } from '@fortawesome/free-solid-svg-icons';
@@ -11,8 +11,7 @@ import { fas } from '@fortawesome/free-solid-svg-icons';
 @NgModule({
   declarations: [PopupSampleComponent],
   imports: [
-    CommonModule, SdsPopupModule, FontAwesomeModule,
-    IconSampleModule
+    CommonModule, SdsPopupModule, FontAwesomeModule
   ],exports: [PopupSampleComponent]
 })
 export class PopupSampleModule {
