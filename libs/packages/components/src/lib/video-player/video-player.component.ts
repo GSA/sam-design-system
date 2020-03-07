aa7e4ded10071eaa330e5444e7d9be2400261d0c
Hemal1975 <hemal6512@gmail.com>
2020-03-07 17:45:56 -0500

    commit changes

--- libs/packages/components/src/lib/video-player/video-player.component.ts
+++ libs/packages/components/src/lib/video-player/video-player.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'sds-video-player',
@@ -6,6 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./video-player.component.scss']
 })
 export class VideoPlayerComponent implements OnInit {
+
+  @Input() videoSourceWebmEl: string;
+  @Input() videoSourceMp4El: string;
+  @Input() videoHeightEl: string;
+  @Input() videoWidthEl: string;
+  @Input() videoPlayerId: string;
+  @Input() videoCaptionEl: string;
+  @Input() videoPosterEl: string;
+  @Input() videoSeekInterval: number;
+  @Input() videoDebug: boolean;
+  @Input() videoPreloadEl: string;
 
   constructor() { }
 
