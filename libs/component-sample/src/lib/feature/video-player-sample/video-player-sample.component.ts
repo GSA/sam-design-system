f310ae850810d6d55dd5e27dff8b823940806d58
Hemal1975 <hemal6512@gmail.com>
2020-03-08 08:12:45 -0400

    commit working changes

--- libs/component-sample/src/lib/feature/video-player-sample/video-player-sample.component.ts
+++ libs/component-sample/src/lib/feature/video-player-sample/video-player-sample.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   templateUrl: './video-player-sample.component.html',
   styleUrls: ['./video-player-sample.component.scss']
@@ -8,14 +9,14 @@ import { Component, OnInit } from '@angular/core';
 export class VideoPlayerSampleComponent implements OnInit {
   sourceWebm: string;
   sourceMp4: string;
-  height: string;
+  height= 345;
   width: string;
 	caption: string;
   poster: string;
 	controls: string;
   id: string;
-	seekInterval = 'dfsdfs';
-	debug = 'fghgfhh';
+	seekInterval = 20;
+	debug = true;
   preload: string;
 
   constructor() {
@@ -24,7 +25,6 @@ export class VideoPlayerSampleComponent implements OnInit {
 
   ngOnInit() {
     this.sourceMp4 = "https://media.w3.org/2010/05/sintel/trailer.mp4";
-    this.height = '360';
     this.width = "550";
     this.id = "sampleVid1";
     this.caption = '';
--- libs/packages/components/src/lib/video-player/video-player.component.ts
+++ libs/packages/components/src/lib/video-player/video-player.component.ts
@@ -1,33 +1,33 @@
 import { Component, OnInit, Input, AfterViewInit, ViewEncapsulation } from '@angular/core';
 import { InitPxVideo } from './js/px-video';
-
-export interface VPInterface{
-  videoHeightEl: string;
-  videoSeekInterval: number;
-  videoDebug: boolean;
-}
-
+import { VPInterface } from './video-player';
 
 @Component({
   selector: 'sds-video-player',
   templateUrl: './video-player.component.html',
   styleUrls: ['./css/px-video.css'],
-  encapsulation: ViewEncapsulation.None,
+  encapsulation: ViewEncapsulation.None
 })
 export class SdsVideoPlayerComponent implements OnInit, AfterViewInit {
+
+
   @Input() videoSourceWebmEl: string;
   @Input() videoSourceMp4El: string;
-  @Input() videoHeightEl: VPInterface;
+  @Input() videoHeightEl: {type : number};
   @Input() videoWidthEl: string;
   @Input() videoPlayerId: string;
   @Input() videoCaptionEl: string;
   @Input() videoPosterEl: string;
-  @Input() videoSeekInterval: VPInterface;
-  @Input() videoDebug: VPInterface;
-  @Input() videoPreloadEl: string;
+  @Input() videoSeekInterval: {type: number};
+  @Input() videoDebug: {type: boolean};
+  @Input() videoPreloadEl: {type: string};
 
 
-  ngOnInit(){}
+  ngOnInit(){
+    // console.log(typeof this.videoSeekInterval);
+    // console.log(typeof this.videoDebug);
+    console.log(typeof this.videoHeightEl);
+  }
 
   ngAfterViewInit() {
     InitPxVideo({
