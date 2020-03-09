40f00727c2d74c99fd2c4f6de24b05e531eb35cc
davereed <dave@contagious.ly>
2020-03-08 22:04:49 -0400

    Make the dependencies pull from library package json

--- libs/documentation/src/lib/pages/introduction/introduction.component.ts
+++ libs/documentation/src/lib/pages/introduction/introduction.component.ts
@@ -5,7 +5,13 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 })
 
 export class IntroductionComponent implements OnInit {
-  constructor() { }
+  packages: any = {};
+
+  constructor() {
+    this.packages.components = require('./../../../../../packages/components/package.json');
+    this.packages.formly = require('./../../../../../packages/sam-formly/package.json');
+    this.packages.layouts = require('./../../../../../packages/layouts/package.json');
+  }
 
   ngOnInit() { }
 }
--- libs/documentation/src/lib/pages/introduction/introduction.module.ts
+++ libs/documentation/src/lib/pages/introduction/introduction.module.ts
@@ -1,10 +1,11 @@
+import { CommonModule } from "@angular/common";
 import { NgModule } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { IntroductionComponent } from './introduction.component';
 import { PrismModule } from '@ngx-prism/core';
 
 @NgModule({
-  imports: [FontAwesomeModule, PrismModule],
+  imports: [CommonModule, FontAwesomeModule, PrismModule],
   exports: [IntroductionComponent],
   declarations: [IntroductionComponent],
   providers: []
