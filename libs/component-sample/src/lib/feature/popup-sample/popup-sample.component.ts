3767c00da904b37fed1aed55a2dff81ea3dabb26
Hemal1975 <hemal6512@gmail.com>
2020-03-10 21:01:13 -0400

    Update popup-sample.component.ts

--- libs/component-sample/src/lib/feature/popup-sample/popup-sample.component.ts
+++ libs/component-sample/src/lib/feature/popup-sample/popup-sample.component.ts
@@ -5,25 +5,7 @@ import { Component, Input, ElementRef, Renderer2, AfterViewInit } from '@angular
   templateUrl: './popup-sample.component.html',
   styleUrls: ['./popup-sample.component.scss']
 })
-export class PopupSampleComponent implements AfterViewInit{
+export class PopupSampleComponent{
 
-  // @Input() Popup: HTMLElement;
-  // @Input() position: string;
-  // @Input() placement: string;
-  // sdsPopupDiv: HTMLElement;
-
-  constructor(private el: ElementRef, private renderer: Renderer2) {
-    // this.renderer.addClass(this.el.nativeElement, 'sds-popup');
-    // this.sdsPopupDiv = document.createElement('div');
-    // this.renderer.addClass(this.sdsPopupDiv, 'sds-popup__content');
-  }
-
-  ngAfterViewInit() {
-    // this.renderer.appendChild(this.el.nativeElement, this.Popup);
-    // this.renderer.addClass(this.sdsPopupDiv, this.placement);
-    // this.renderer.addClass(this.sdsPopupDiv, this.position);
-    // this.renderer.appendChild(this.sdsPopupDiv, this.el.nativeElement.children[0]);
-    // this.renderer.appendChild(this.el.nativeElement, this.sdsPopupDiv);
-  }
 
 }
