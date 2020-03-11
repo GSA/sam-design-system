d99e2a1c71c3b1f5508f2eb8f27bb07d1bd2fa46
Hemal1975 <hemal6512@gmail.com>
2020-03-11 13:26:12 -0400

    We are not testing sample component for this directive so removed dependece of the component

--- libs/component-sample/src/lib/feature/popup-sample/popup-sample.component.spec.ts
+++ libs/component-sample/src/lib/feature/popup-sample/popup-sample.component.spec.ts
@@ -1,27 +1,19 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import {  PopupSampleComponent } from './popup-sample.component';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-
 describe('  PopupSampleComponent', () => {
-  let component:  PopupSampleComponent;
-  let fixture: ComponentFixture<  PopupSampleComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [   PopupSampleComponent ],
-      imports: [FontAwesomeModule]
+      declarations: [  ],
+      imports: []
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(  PopupSampleComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
-    expect(component).toBeTruthy();
+
   });
 });
