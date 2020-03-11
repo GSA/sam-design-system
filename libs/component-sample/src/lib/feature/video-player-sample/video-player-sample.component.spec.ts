edf03271dfcd0627cd91891347217f643b511b45
Hemal1975 <hemal6512@gmail.com>
2020-03-11 11:32:30 -0400

    We are not testing sample spec file so I removed from sample.spec.ts

--- libs/component-sample/src/lib/feature/video-player-sample/video-player-sample.component.spec.ts
+++ libs/component-sample/src/lib/feature/video-player-sample/video-player-sample.component.spec.ts
@@ -1,27 +1,21 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { VideoPlayerSampleComponent } from './video-player-sample.component';
-import { SdsVideoPlayerModule } from 'libs/packages/components/src/lib/video-player/video-player.module';
 
 describe('VideoPlayerSampleComponent', () => {
-  let component: VideoPlayerSampleComponent;
-  let fixture: ComponentFixture<VideoPlayerSampleComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ VideoPlayerSampleComponent ],
-      imports: [SdsVideoPlayerModule]
+      declarations: [  ],
+      imports: []
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(VideoPlayerSampleComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+
   });
 
   it('should create', () => {
-    expect(component).toBeTruthy();
+
   });
 });
