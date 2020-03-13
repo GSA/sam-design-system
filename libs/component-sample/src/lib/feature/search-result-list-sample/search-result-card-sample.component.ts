fc07806a9e2be3c5f13df15a7094d7eb6238274d
Hemal1975 <hemal6512@gmail.com>
2020-03-13 12:17:39 -0400

    Updated result list sample data object
    Updated result list sample data object with Icon library and icon name

--- libs/component-sample/src/lib/feature/search-result-list-sample/search-result-card-sample.component.ts
+++ libs/component-sample/src/lib/feature/search-result-list-sample/search-result-card-sample.component.ts
@@ -9,11 +9,11 @@ export class SearchResultCardSampleComponent  {
   constructor(private change: ChangeDetectorRef) { }
 
   items = [
-    { text: 'First', id: 1 },
-    { text: 'Second', id: 2 },
-    { text: 'Third', id: 3 },
-    { text: 'Fourth', id: 4 },
-    { text: 'Fifth', id: 5 , hasNewerData:true},
+    { iconlib : 'sds', icon: "bell", text: 'First', id: 1 },
+    { iconlib : 'sds', icon: 'book', text: 'Second', id: 2 },
+    { iconlib : 'sds', icon: 'download', text: 'Third', id: 3 },
+    { iconlib : 'fas', icon: 'comment', text: 'Fourth', id: 4 },
+    { iconlib : 'sds', icon: 'arrow-left', text: 'Fifth', id: 5 , hasNewerData:true},
 
   ];
 
