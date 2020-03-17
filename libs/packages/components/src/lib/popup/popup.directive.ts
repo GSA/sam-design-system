2c1fd03a5245837637c641e68dc76104847dadfb
Hemal1975 <hemal6512@gmail.com>
2020-03-17 14:58:18 -0400

    Update popup for description
    Add directive in html and update functionality in ts file . Added tooltip class for popup

--- libs/packages/components/src/lib/popup/popup.directive.ts
+++ libs/packages/components/src/lib/popup/popup.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, Input, ElementRef, Renderer2, AfterViewInit, OnInit } from '@angular/core';
+import { Directive, Input, ElementRef, Renderer2, AfterViewInit } from '@angular/core';
 
 
 @Directive({
@@ -10,6 +10,7 @@ export class SdsPopupDirective implements AfterViewInit {
   @Input() position: string;
   @Input() placement: string;
   sdsPopupDiv: HTMLElement;
+  descDiv: HTMLElement;
 
   constructor(private el: ElementRef, private renderer: Renderer2) {
     this.renderer.addClass(this.el.nativeElement, 'sds-popup');
@@ -17,12 +18,20 @@ export class SdsPopupDirective implements AfterViewInit {
     this.renderer.addClass(this.sdsPopupDiv, 'sds-popup__content');
   }
 
-
   ngAfterViewInit() {
-    this.renderer.appendChild(this.el.nativeElement, this.sdsPopup);
     this.renderer.addClass(this.sdsPopupDiv, this.placement);
     this.renderer.addClass(this.sdsPopupDiv, this.position);
+    if(typeof(this.sdsPopup) !== 'string'){
+    this.renderer.appendChild(this.el.nativeElement, this.sdsPopup);
     this.renderer.appendChild(this.sdsPopupDiv, this.el.nativeElement.children[0]);
     this.renderer.appendChild(this.el.nativeElement, this.sdsPopupDiv);
+    } else {
+      this.descDiv = document.createElement('div');
+      this.renderer.addClass(this.descDiv, 'tooltip');
+      this.renderer.addClass(this.descDiv, 'tooltip-left');
+      this.descDiv.innerText = this.sdsPopup;
+      this.renderer.appendChild(this.sdsPopupDiv, this.descDiv);
+      this.renderer.appendChild(this.el.nativeElement, this.sdsPopupDiv);
+    }
   }
 }
