5e8ad4ed662fb7e8bd68c845a578f537e36bc0f9
yerramshilpa <yerramshilpa@gmail.com>
2020-03-24 09:08:55 -0400

    updated filters interaction

--- libs/packages/sam-formly/src/lib/formly-filters/sds-filters.component.ts
+++ libs/packages/sam-formly/src/lib/formly-filters/sds-filters.component.ts
@@ -8,14 +8,13 @@ import { FormlyFieldConfig } from '@ngx-formly/core';
 import { Subject } from 'rxjs';
 import { SDSFormlyUpdateComunicationService } from './service/sds-filters-comunication.service';
 import { pairwise } from 'rxjs/operators';
-import { Router, ActivatedRoute, UrlSerializer, UrlTree, DefaultUrlSerializer } from '@angular/router';
-
+import { Router, ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'sds-filters',
   template: `
       <formly-form [form]="form" (modelChange)="modelChange.next($event)" [fields]="fields" [model]="model"></formly-form>
     `,
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SdsFiltersComponent implements OnInit {
 
@@ -40,30 +39,45 @@ export class SdsFiltersComponent implements OnInit {
   @Input() public model: any;
 
   /**
-   *  Accordion Label used to display the Accordion header text.
-   */
-  @Input() accordionLabel: string = 'Filters';
-
-  /**
    *  Emit results when model updated
    */
   @Output() filterChange = new EventEmitter<object[]>();
 
-  private queryParams = '';
+
+  /**
+   * debounce time for current page input
+   */
+  @Input() debounceTime = 0;
+  private queryParams = {};
+  label = [];
 
   constructor(@Optional() private formlyUpdateComunicationService: SDSFormlyUpdateComunicationService,
     private router: Router, private route: ActivatedRoute) { }
 
-
   ngOnInit(): void {
+
+    this.route.queryParams.subscribe(params => {
+      for (const key in params) {
+        if (key) {
+          const arr = key.replace(']', '').split('[');
+          this.model[arr[0]] = { [arr[1]]: JSON.parse(params[key]) };;
+        }
+      }
+      setTimeout(() => {
+        this.form.patchValue({
+          ...this.model
+        })
+      });
+    });
     this.form.valueChanges
       .pipe(pairwise())
       .subscribe(([prev, next]: [any, any]) => {
-        // this.queryParams = this.getKeyValueFilters(next);
-
-        this.queryParams = this.convertToParam(next)
-        window.location.href = `${this.queryParams}`;
-       ;
+        this.queryParams = this.convertToParam(next);
+        this.router.navigate([], {
+          relativeTo: this.route,
+          queryParams: this.queryParams,
+          queryParamsHandling: 'merge',
+        });
         this.filterChange.emit(next);
         if (this.formlyUpdateComunicationService) {
           this.formlyUpdateComunicationService.updateFilter(next);
@@ -81,43 +95,14 @@ export class SdsFiltersComponent implements OnInit {
               filters[key][subKey] = null;
             }
             if (filters[key][subKey]) {
-              const value = `${key}[${subKey}]=${JSON.stringify(filters[key][subKey])}`;
-              result.push(value);
-              // result[key + "[" + subKey + "]"] = filters[key][subKey];
+              // const value = `${key}[${subKey}]=${JSON.stringify(filters[key][subKey])}`;
+              // result.push(value);
+              result[key + "[" + subKey + "]"] = JSON.stringify(filters[key][subKey]);
             }
           }
         }
       }
     }
-    console.log(result.join('&'), 'url')
-    return '?'+result.join('&');
-  }
-
-  //   var queryStringToJSON = function (url) {
-  //     if (url === '')
-  //        return '';
-  //     var pairs = (url || location.search).slice(1).split('&');
-  //     var result = {};
-  //     for (var idx in pairs) {
-  //     var pair = pairs[idx].split('=');
-  //     if (!!pair[0])
-  //         result[pair[0].toLowerCase()] = decodeURIComponent(pair[1] || '');
-  //     }
-  //    return result;
-  // }
-}
-
-
-export class CustomUrlSerializer implements UrlSerializer {
-  parse(url: any): UrlTree {
-      let dus = new DefaultUrlSerializer();
-      return dus.parse(url);
-  }
-
-  serialize(tree: UrlTree): any {
-      let dus = new DefaultUrlSerializer(),
-          path = dus.serialize(tree);
-      // use your regex to replace as per your requirement.
-      return path.replace(/%2/g,',');
+    return result;
   }
 }
