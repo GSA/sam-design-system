7d9116c2b465d9d3ef2517a5d1cfdcb9e2a47d72
yerramshilpa <yerramshilpa@gmail.com>
2020-03-27 07:47:25 -0400

    converted back to obj

--- libs/packages/sam-formly/src/lib/formly-filters/sds-filters.component.ts
+++ libs/packages/sam-formly/src/lib/formly-filters/sds-filters.component.ts
@@ -1,6 +1,6 @@
 import {
   Component, Input, Output,
-  ChangeDetectionStrategy,
+  ChangeDetectionStrategy, ChangeDetectorRef,
   EventEmitter, Optional, OnInit
 } from '@angular/core';
 import { FormGroup } from '@angular/forms';
@@ -56,29 +56,45 @@ export class SdsFiltersComponent implements OnInit {
   private queryParams = {};
   label = [];
   convertedmodel: any ={};
-  constructor(@Optional() private formlyUpdateComunicationService: SDSFormlyUpdateComunicationService,
+  constructor(@Optional() private formlyUpdateComunicationService: SDSFormlyUpdateComunicationService,private cdr: ChangeDetectorRef,
     private router: Router, private route: ActivatedRoute, private location: Location) { }
 
   ngOnInit(): void {
-    this.route.queryParamMap.subscribe(params => {
-      const query = params.get("query");
-      this.model = this.convertToModel(query);
+    // this.route.queryParamMap.subscribe(params => {
+    //   const query = params.get("query");
+    //   this.model = this.convertToModel(query);
+    //   setTimeout(() => {
+    //     this.form.patchValue({
+    //       ...this.model
+    //     })
+    //   })
+    // });
+
+    this.route.queryParams.subscribe(params => {
+      console.log(params);
+      // tslint:disable-next-line:forin
+      for (const key in params) {
+        // if (key) {
+          const arr = key.replace(']', '').split('[');
+          //console.log(arr, 'array');
+          this.model[arr[0]] = { [arr[1]]: JSON.parse(params[key]) };
+        // } 
+    }
       setTimeout(() => {
         this.form.patchValue({
           ...this.model
         })
-      })
+      });
     });
-
+  
     this.form.valueChanges
       .pipe(pairwise())
       .subscribe(([prev, next]: [any, any]) => {
         const params = this.convertToParam(next);
-       
        // this.queryParams = this.convertToParam(next);
         this.router.navigate([], {
           relativeTo: this.route,
-          queryParams:  { query:params },
+          queryParams:  params,
           queryParamsHandling: 'merge',
         });
         this.filterChange.emit(next);
@@ -88,12 +104,28 @@ export class SdsFiltersComponent implements OnInit {
       });
   }
   convertToParam(filters) {
+   
     const encodedValues = qs.stringify(
       filters,
       { skipNulls: true ,  encode: false}
     );
-    const parse =qs.parse(encodedValues);
-    return encodedValues;
+
+    const target = {};
+    encodedValues.split('&').forEach((pair) => {
+  if(pair !== '') {
+    const splitpair = pair.split('=');
+    const key = splitpair[0].charAt(0).toLowerCase() + splitpair[0].slice(1).split(' ').join('');
+    target[key] = splitpair[1];
+  }
+});
+
+console.dir(target);
+    
+  
+
+
+   // const parse =qs.parse(encodedValues);
+    return target;
   }
   convertToModel(str){
     let obj = {};
