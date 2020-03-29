b70561e92686e473f18efbfe8e47df0544cfcc43
yerramshilpa <yerramshilpa@gmail.com>
2020-03-29 14:33:27 -0400

    url tracking changes

--- libs/packages/sam-formly/src/lib/formly-filters/sds-filters.component.ts
+++ libs/packages/sam-formly/src/lib/formly-filters/sds-filters.component.ts
@@ -1,27 +1,39 @@
 import {
-  Component, Input, Output,
-  ChangeDetectionStrategy, ChangeDetectorRef,
-  EventEmitter, Optional, OnInit
+  Component,
+  Input,
+  Output,
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  EventEmitter,
+  Optional,
+  OnInit
 } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { FormlyFieldConfig } from '@ngx-formly/core';
 import { Subject } from 'rxjs';
 import { SDSFormlyUpdateComunicationService } from './service/sds-filters-comunication.service';
 import { pairwise } from 'rxjs/operators';
-import { Router, ActivatedRoute, NavigationEnd, NavigationStart } from '@angular/router';
+import {
+  Router,
+  ActivatedRoute,
+  NavigationEnd,
+  NavigationStart
+} from '@angular/router';
 import * as qs from 'qs';
-import * as lodash from 'lodash';
 import { Location } from '@angular/common';
-import 'rxjs/add/operator/pairwise';
-@Component({
+import 'rxjs/add/operator/pairwise';@Component({
   selector: 'sds-filters',
   template: `
-      <formly-form [form]="form" [fields]="fields" [model]="model"></formly-form>
-    `,
-  changeDetection: ChangeDetectionStrategy.OnPush,
+    <formly-form
+      [form]="form"
+      (modelChange)="modelChange.next($event)"
+      [fields]="fields"
+      [model]="model"
+    ></formly-form>
+  `,
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class SdsFiltersComponent implements OnInit {
-
   /**
    * Modeal update
    */
@@ -33,76 +45,75 @@ export class SdsFiltersComponent implements OnInit {
   @Input() public form: FormGroup;
 
   /**
-   *  Fields are used to configure the UI components 
+   *  Fields are used to configure the UI components
    */
   @Input() public fields: FormlyFieldConfig[];
 
   /**
    *  Model used to display the filter values.
    */
-  @Input() public model: any = {};
+  @Input() public model: any;
 
   /**
    *  Emit results when model updated
    */
   @Output() filterChange = new EventEmitter<object[]>();
+
   /**
    * debounce time for current page input
    */
   @Input() debounceTime = 0;
- 
-  convertedmodel: any = {};
-  constructor(@Optional() private formlyUpdateComunicationService: SDSFormlyUpdateComunicationService, private cdr: ChangeDetectorRef,
-    private router: Router, private route: ActivatedRoute, private location: Location) { }
+  private queryParams = {};
+  label = [];
+  constructor(
+    @Optional()
+    private formlyUpdateComunicationService: SDSFormlyUpdateComunicationService,
+    private cdr: ChangeDetectorRef,
+    private router: Router,
+    private route: ActivatedRoute,
+    private location: Location
+  ) {}
 
   ngOnInit(): void {
+    const _isObj = (obj: any): boolean => typeof obj === 'object' && obj !== null;
+    const _isEmpty = (obj: any): boolean => Object.keys(obj).length === 0;
+    const overwrite = (baseObj: any, newObj: any) => {
+      let result = {};
+      for (let key in baseObj) {
+        if (_isObj(baseObj[key])) {
+          result[key] = overwrite(baseObj[key], newObj[key] || {});
+        } else {
+          result[key] = newObj[key] || null;
+        }
+      }
+      return result;
+    };
+
     this.route.queryParams.subscribe(params => {
-      this.convertedmodel = qs.parse(qs.stringify(params, { skipNulls: true, encode: false }));
-      const arraysEqual = lodash.isEqual(this.convertedmodel, this.model);
-      if (!arraysEqual) {
-        const obj = lodash.concat(this.convertedmodel, this.model);
-        this.model = { ...obj[1] };
+      if (_isEmpty(this.form.getRawValue())) {
+        this.model = qs.parse(params);
+        this.form.patchValue({
+          ...this.model
+        });
+      } else {
+        
+        const updatedFormValue = overwrite(
+          this.form.getRawValue(),
+          qs.parse(params)
+        );
+        console.log('updatedFormValue ===>', updatedFormValue);
+        this.form.setValue(updatedFormValue);
       }
     });
 
-    // this.route.queryParams.subscribe(params => {
-    //   let convertedmodel = {};
-    //   const getAllKeys = Object.keys(params);
-    //   // tslint:disable-next-line:forin
-    //   for (const key in this.model) {
-    //         getAllKeys.forEach(function(keyName) {
-    //           if (keyName.indexOf(key) !== -1) {
-    //             const str = `${[keyName]}:${params[keyName]}`;
-    //             const updatedParam = qs.parse(str);
-    //             console.log(updatedParam[key]);
-    //             convertedmodel = {
-    //               ...convertedmodel,
-    //               [key] : updatedParam[key]
-    //             };
-    //           } else {
-    //             convertedmodel = {
-    //               ...convertedmodel,
-    //               [key] : {}
-    //             };
-    //           }
-    //         })
-    //       }
-    //       console.log(convertedmodel);
-    //   setTimeout(() => {
-    //     this.form.patchValue({
-    //       ...convertedmodel
-    //     });
-    //   });
-    // });
     this.form.valueChanges
       .pipe(pairwise())
       .subscribe(([prev, next]: [any, any]) => {
-      
         const params = this.convertToParam(next);
         this.router.navigate([], {
           relativeTo: this.route,
           queryParams: params,
-          queryParamsHandling: 'merge',
+          queryParamsHandling: 'merge'
         });
         this.filterChange.emit(next);
         if (this.formlyUpdateComunicationService) {
@@ -110,20 +121,30 @@ export class SdsFiltersComponent implements OnInit {
         }
       });
   }
+
   convertToParam(filters) {
-    const encodedValues = qs.stringify(
-      filters,
-      { skipNulls: true, encode: false }
-    );
-    const obj = {};
-    encodedValues.split('&').forEach((pair) => {
+    const encodedValues = qs.stringify(filters, {
+      skipNulls: true,
+      encode: false
+    });
+    const target = {};
+    encodedValues.split('&').forEach(pair => {
       if (pair !== '') {
         const splitpair = pair.split('=');
-        obj[splitpair[0]] = splitpair[1];
+        const key =
+          splitpair[0].charAt(0).toLowerCase() +
+          splitpair[0]
+            .slice(1)
+            .split(' ')
+            .join('');
+        target[key] = splitpair[1];
       }
     });
-    return obj;
+    // console.dir(target);
+    // const parse =qs.parse(encodedValues);
+    return target;
   }
+
   convertToModel(str) {
     let obj = {};
     obj = qs.parse(str);
