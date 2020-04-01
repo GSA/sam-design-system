c8b71a31ffe63e3cab6cd989b413ce39b03314b1
Hemal1975 <hemal6512@gmail.com>
2020-04-01 13:10:07 -0400

    Updated data object with icon and iconlib

--- libs/component-sample/src/lib/feature/search-result-list-sample/result-list-info-message.data.ts
+++ libs/component-sample/src/lib/feature/search-result-list-sample/result-list-info-message.data.ts
@@ -2,5 +2,6 @@ export let SearchMessageInfoSampleData =
   {
    'title': 'No matches found',
    'message': 'We could not find a match for your search criteri Please try another search or go back to previous results.',
-   'icon': ''
+   'iconlin': 'sds',
+   'icon': 'bell'
 }
