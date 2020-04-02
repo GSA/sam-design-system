477b29ff5dfb17fb2aa38d05c92f9ad31381a62b
Hemal1975 <hemal6512@gmail.com>
2020-04-02 15:10:26 -0400

    Updated code for ICON

--- libs/component-sample/src/lib/feature/search-result-list-sample/result-list-info-message.data.ts
+++ libs/component-sample/src/lib/feature/search-result-list-sample/result-list-info-message.data.ts
@@ -3,5 +3,6 @@ export let SearchMessageInfoSampleData =
    'title': 'No matches found',
    'message': 'We could not find a match for your search criteri Please try another search or go back to previous results.',
    'iconlin': 'sds',
-   'icon': 'bell'
+   'icon': 'book',
+   'iconsize': '5x'
 }
