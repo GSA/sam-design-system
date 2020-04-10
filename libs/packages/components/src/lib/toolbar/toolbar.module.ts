a76c4dd040401e789a74958ee5061c34683e063c
Alejandro Colina <jose.colina@gsa.gov>
2020-04-09 20:57:16 -0400

    modification to toolbar

--- libs/packages/components/src/lib/toolbar/toolbar.module.ts
+++ libs/packages/components/src/lib/toolbar/toolbar.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { SdsToolbarComponent } from './toolbar.component';
 import { SdsToolbarHeaderComponent } from './toolbar-header.component';
 import { SdsToolbarExpandDirective } from './toolbar-expand.directive';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule,FontAwesomeModule],
   exports: [SdsToolbarComponent, SdsToolbarExpandDirective],
   declarations: [
     SdsToolbarComponent,
