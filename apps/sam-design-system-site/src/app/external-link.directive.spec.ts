ee2370aeb067ec36e10ae859b2cd97ca940c2229
Tiger Li <22798884+tigerpway@users.noreply.github.com>
2020-04-13 14:30:06 -0400

    took out comments

--- apps/sam-design-system-site/src/app/external-link.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-// import { ExternalLinkDirective } from './external-link.directive';
-
-// describe('ExternalLinkDirective', () => {
-//   it('should create an instance', () => {
-//     const directive = new ExternalLinkDirective();
-//     expect(directive).toBeTruthy();
-//   });
-// });
