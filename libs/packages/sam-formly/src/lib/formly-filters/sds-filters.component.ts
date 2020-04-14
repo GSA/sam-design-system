1cb0f3b574386790867de50a12c4c1c7c823340f
yerramshilpa <yerramshilpa@gmail.com>
2020-04-14 09:03:54 -0400

    revert history changes

--- libs/packages/sam-formly/src/lib/formly-filters/sds-filters.component.ts
+++ libs/packages/sam-formly/src/lib/formly-filters/sds-filters.component.ts
@@ -63,73 +63,14 @@ export class SdsFiltersComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    const _isObj = (obj: any): boolean => typeof obj === 'object' && obj !== null;
-    const _isEmpty = (obj: any): boolean => Object.keys(obj).length === 0;
-    const overwrite = (baseObj: any, newObj: any) => {
-      const result = {};
-      for (const key in baseObj) {
-        if (_isObj(baseObj[key])) {
-          result[key] = overwrite(baseObj[key], newObj[key] || {});
-        } else {
-          result[key] = newObj[key] || null;
-        }
-      }
-      return result;
-    };
-    this.route.queryParams.subscribe(params => {
-      if (_isEmpty(this.form.getRawValue())) {
-        const paramModel = this.convertToModel(params);
-        this.form.patchValue({
-          ...this.model , ...paramModel
-        });
-      
-      } else {
-        const updatedFormValue = overwrite(
-          this.form.getRawValue(),
-          this.convertToModel(params)
-        );
-        this.form.setValue(updatedFormValue);
-      }
-    });
-
     this.form.valueChanges
       .pipe(pairwise())
       .subscribe(([prev, next]: [any, any]) => {
-        const params = this.convertToParam(next);
-        this.router.navigate([], {
-          relativeTo: this.route,
-          queryParams: params,
-          queryParamsHandling: 'merge'
-        });
-        this.filterChange.emit(next);
+         this.filterChange.emit(next);
         if (this.formlyUpdateComunicationService) {
           this.formlyUpdateComunicationService.updateFilter(next);
         }
       });
   }
 
-  convertToParam(filters) {
-    const encodedValues = qs.stringify(filters, {
-      skipNulls: true,
-      encode: false
-    });
-    const target = {};
-    encodedValues.split('&').forEach(pair => {
-      if (pair !== '') {
-        const splitpair = pair.split('=');
-        target[ splitpair[0]] = splitpair[1];
-      }
-    });
-    return target;
-  }
-
-  convertToModel(filters) {
-    let obj = {};
-    const encodedValues = qs.stringify(filters, {
-      skipNulls: true,
-      encode: false
-    });
-    obj = qs.parse(encodedValues);
-    return obj;
-  }
 }
