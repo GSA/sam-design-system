c808a3d6c93763bc6f56cb82a53972b985880945
yerramshilpa <yerramshilpa@gmail.com>
2020-04-25 00:56:37 -0400

    history tracking changes

--- libs/packages/sam-formly/src/lib/formly-filters/sds-filters.component.ts
+++ libs/packages/sam-formly/src/lib/formly-filters/sds-filters.component.ts
@@ -1,23 +1,35 @@
 import {
-  Component, Input, Output,
-  ChangeDetectionStrategy, ChangeDetectorRef,
-  EventEmitter, Optional, OnInit
+  Component,
+  Input,
+  Output,
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  EventEmitter,
+  Optional,
+  OnInit
 } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { FormlyFieldConfig, FormlyFormOptions } from '@ngx-formly/core';
 import { Subject } from 'rxjs';
 import { SDSFormlyUpdateComunicationService } from './service/sds-filters-comunication.service';
-import { pairwise } from 'rxjs/operators';
-import { Router, ActivatedRoute, } from '@angular/router';
+import { pairwise, filter } from 'rxjs/operators';
+import { Router, ActivatedRoute, NavigationStart } from '@angular/router';
 import * as qs from 'qs';
+import { Md5 } from 'ts-md5/dist/md5';
+import { HostListener } from '@angular/core';
 
 @Component({
   selector: 'sds-filters',
   template: `
-      <formly-form [form]="form" [fields]="fields" [options]="options" [model]="model"></formly-form>`,
-  changeDetection: ChangeDetectionStrategy.OnPush,
+    <formly-form
+      [form]="form"
+      [fields]="fields"
+      [options]="options"
+      [model]="model"
+    ></formly-form>
+  `,
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-
 export class SdsFiltersComponent implements OnInit {
   /**
    * Modeal update
@@ -53,24 +65,83 @@ export class SdsFiltersComponent implements OnInit {
    * debounce time for current page input
    */
   @Input() debounceTime = 0;
- 
+
+  // routeTrigger = '';
+  _isObj = (obj: any): boolean => typeof obj === 'object' && obj !== null;
+  _isEmpty = (obj: any): boolean => Object.keys(obj).length === 0;
+  nullify = (obj: any) => {
+    for (let key in obj) {
+      if (this._isObj(obj[key])) {
+        obj[key] = this.nullify(obj[key]);
+      } else {
+        obj[key] = null;
+      }
+    }
+    return obj;
+  };
+
   constructor(
     @Optional()
     private formlyUpdateComunicationService: SDSFormlyUpdateComunicationService,
     private cdr: ChangeDetectorRef,
     private router: Router,
-    private route: ActivatedRoute,
+    private route: ActivatedRoute
   ) {}
 
+  @HostListener('window:popstate', ['$event'])
+  onpopstate(event) {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const ref = urlParams.get('ref');
+    const updatedFormValue =
+      ref == null
+        ? this.options.resetModel()//this.nullify(this.form.value)
+        : JSON.parse(localStorage.getItem(ref));
+    console.log(updatedFormValue);
+    if (window.location.pathname.includes('formlyInput')) {
+      this.form.patchValue(updatedFormValue, { emitEvent: false });
+    } else {
+      this.form.setValue(updatedFormValue, { emitEvent: false });
+    }
+    this.filterChange.emit(updatedFormValue);
+    if (this.formlyUpdateComunicationService) {
+      this.formlyUpdateComunicationService.updateFilter(updatedFormValue);
+    }
+  }
+
   ngOnInit(): void {
+    if (this._isEmpty(this.form.getRawValue())) {
+      const queryString = window.location.search;
+      const urlParams = new URLSearchParams(queryString);
+      const initialRef = urlParams.get('ref');
+      initialRef == null
+        ? localStorage.clear()
+        : ((this.model = JSON.parse(localStorage.getItem(initialRef))),
+          setTimeout(() => {
+            this.form.patchValue(
+              {
+                ...this.model
+              },
+              { emitEvent: false }
+            );
+          }));
+    }
+
     this.form.valueChanges
       .pipe(pairwise())
       .subscribe(([prev, next]: [any, any]) => {
-         this.filterChange.emit(next);
+        const md5 = new Md5();
+        const hashCode = md5.appendStr(qs.stringify(next)).end();         
+        this.router.navigate([], {
+          relativeTo: this.route,
+          queryParams: { ref: hashCode },
+          queryParamsHandling: 'merge'
+        });
+        localStorage.setItem(hashCode.toString(), JSON.stringify(next));
+        this.filterChange.emit(next);
         if (this.formlyUpdateComunicationService) {
           this.formlyUpdateComunicationService.updateFilter(next);
         }
       });
   }
-
 }
