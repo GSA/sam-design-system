ca88a47a6f801b7e082a0ff0b87f19fbde73dbe4
yerramshilpa <yerramshilpa@gmail.com>
2020-05-02 10:04:31 -0400

    updated  test

--- libs/packages/components/src/lib/search/search.spec.ts
+++ libs/packages/components/src/lib/search/search.spec.ts
@@ -15,34 +15,64 @@ class TestComponent {
 fdescribe('SearchComponent', () => {
   let component: SdsSearchComponent;
   let fixture: ComponentFixture<SdsSearchComponent>;
- 
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SdsSearchComponent ],
+      declarations: [SdsSearchComponent],
       providers: [FocusMonitor, ViewportRuler]
-    })
-    .compileComponents();
+    }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(SdsSearchComponent);
     component = fixture.componentInstance;
     component.searchSettings.size = 'large';
+    // component.model.searchText ='test';
+    // component.model.searchCategory ='30';
     fixture.detectChanges();
   });
 
-  it('Should update the model value on click event', ()=>{
-    const e = jasmine.createSpyObj('e', [ 'preventDefault' ]);
+  it('should be dropdown', () => {
+    //  const e = jasmine.createSpyObj('e', [ 'preventDefault' ]);
+
+    component.inputEl.nativeElement.value = 'test';
+    // component.selectEl.nativeElement.value = '30';
+    component.searchSettings.dropdown.options = [
+      { label: '30 Days', value: '30' },
+      { label: '60 Days', value: '60' },
+      { label: '90 Days', value: '90' }
+    ];
+
+    expect(component.inputEl.nativeElement.value).toBe('test');
+  });
+
+  it('should implement controlvalueaccessor with default values', () => {
+    component.registerOnChange(_ => undefined);
+    component.registerOnTouched(() => undefined);
+    component.writeValue({ searchText: 'aaa', searchCategory: '30' });
+    expect(component.model.searchText).toBe('aaa');
+  });
+
+  it('should implement controlvalueaccessor with empty', () => {
+    component.model = {};
+    component.registerOnChange(_ => undefined);
+    component.registerOnTouched(() => undefined);
+    component.writeValue(component.model);
+    expect(component.model.searchText).toBe(undefined);
+  });
+
+  it('Should update the model value on click event', () => {
+    const e = jasmine.createSpyObj('e', ['preventDefault']);
     component.inputEl.nativeElement.value = 'test';
     component.handleClick(e);
     expect(e.preventDefault).toHaveBeenCalled();
-    expect(component.model.searchText).toBe('test')
-
+    expect(component.model.searchText).toBe('test');
   });
 
   it('Should check the Initial State Visible', () => {
-    expect(component.inputState.visible).toBe(component.inputState.initial.visible);
+    expect(component.inputState.visible).toBe(
+      component.inputState.initial.visible
+    );
   });
 
   it('should accept inputs and assign properties', () => {
@@ -59,5 +89,26 @@ fdescribe('SearchComponent', () => {
     expect(component.inputEl.nativeElement.style.left).toBe('');
     expect(component.inputEl.nativeElement.style.width).toBe('');
     expect(component.inputState.visible).toBeFalsy();
+  });
+
+  it('Should trigger focus event', () => {
+    const e = null;
+    component.inputState.initial.visible = true;
+    component.focusChange(e);
+    fixture.detectChanges();
+    expect(component.inputEl.nativeElement.style.display).toBe('');
+    expect(component.inputEl.nativeElement.style.position).toBe('');
+    expect(component.inputEl.nativeElement.style.left).toBe('');
+    expect(component.inputEl.nativeElement.style.width).toBe('');
+    expect(component.inputState.visible).toBeTruthy();
+  });
+  it('Should visible false event', () => {
+    const e = jasmine.createSpyObj('e', ['preventDefault']);
+    component.inputState.visible = false;
+    component.handleClick(e);
+    fixture.detectChanges();
+    expect(component.inputEl.nativeElement.style.display).toBe('block');
+    expect(component.inputEl.nativeElement.style.position).toBe('absolute');
+    expect(component.inputState.visible).toBeTruthy();
   });
 });
