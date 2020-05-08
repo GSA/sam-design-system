6caa76b909ab18da2eac4e80fccfbbe03e38032e
= <alexandra.screven@gsa.gov>
2020-05-08 07:15:54 -0700

    [IAE-32619] started advanced filters component tests

--- libs/packages/sam-formly/src/lib/formly-filters/advanced-filters/advanced-filters.component.spec.ts
+++ libs/packages/sam-formly/src/lib/formly-filters/advanced-filters/advanced-filters.component.spec.ts
@@ -2,28 +2,49 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AdvancedFiltersComponent } from './advanced-filters.component';
 import { SdsDialogService } from '@gsa-sam/components';
+import { SdsAdvancedFiltersService } from './sds-advanced-filters.service';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
 
 describe('AdvancedFiltersComponent', () => {
   let component: AdvancedFiltersComponent;
   let fixture: ComponentFixture<AdvancedFiltersComponent>;
   let dialogServiceStub: Partial<SdsDialogService>;
+  let advancedFiltersServiceStub: Partial<SdsAdvancedFiltersService>;
+  let advancedFiltersService: SdsAdvancedFiltersService;
+  let dialogDe: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ AdvancedFiltersComponent ],
-      providers: [ { provide: SdsDialogService, useValue: dialogServiceStub } ],
+      providers: [ { provide: SdsDialogService, useValue: dialogServiceStub },
+      {
+        provide: SdsAdvancedFiltersService, useValue: advancedFiltersServiceStub
+      } ],
 
     })
-    .compileComponents();
+    .compileComponents().then(() => {
+      fixture = TestBed.createComponent(AdvancedFiltersComponent);
+      component = fixture.componentInstance;
+      advancedFiltersService = TestBed.get(SdsAdvancedFiltersService);
+      dialogDe = fixture.debugElement.query(By.css('.usa-button--unstyled'));
+      fixture.detectChanges();
+    })
   }));
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(AdvancedFiltersComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('stub object and injected SdsDialogService should not be the same', () => {
+    expect(dialogServiceStub === SdsDialogService).toBe(false);
+  });
+
+  it('stub object and injected SdsAdvancedFiltersService should not be the same', () => {
+    expect(advancedFiltersServiceStub === SdsAdvancedFiltersService).toBe(false);
+  });
+
+  it('#openDialog() should open sdsFormlyDialogComponent', () => {
+    component.openDialog()
+  })
 });
