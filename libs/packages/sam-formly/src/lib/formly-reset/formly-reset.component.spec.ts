b02ce73749f36431938f31cd8c3ab1d9c024a023
= <alexandra.screven@gsa.gov>
2020-05-12 08:00:35 -0700

    [iae-32618] working on nested component testing to make sure reset really clears model

--- libs/packages/sam-formly/src/lib/formly-reset/formly-reset.component.spec.ts
+++ libs/packages/sam-formly/src/lib/formly-reset/formly-reset.component.spec.ts
@@ -2,27 +2,29 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { SdsFormlyResetComponent } from './formly-reset.component';
-import { By } from '@angular/platform-browser';
-import { DebugElement } from '@angular/core';
+import { DebugElement, Component } from '@angular/core';
+import { FormlyFormOptions } from '@ngx-formly/core';
 
 describe('SdsFormlyResetComponent', () => {
   let component: SdsFormlyResetComponent;
-  let fixture: ComponentFixture<SdsFormlyResetComponent>;
   let buttonDe: DebugElement;
   let buttonElement: HTMLElement;
   let options: { "_initialModel": { "searchEntity": { "uniqueEntityIdSam": 20 } }, "formState": {}, "_hiddenFieldsForCheck": [] }
 
+  let testHost: TestHostComponent;
+  let fixture: ComponentFixture<TestHostComponent>;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SdsFormlyResetComponent ],
+      declarations: [ SdsFormlyResetComponent, TestHostComponent ],
       imports: [FontAwesomeModule]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(SdsFormlyResetComponent);
-    component = fixture.componentInstance;
+    fixture = TestBed.createComponent(TestHostComponent);
+    testHost = fixture.componentInstance;
     buttonDe = fixture.debugElement;
     buttonElement = buttonDe.nativeElement;
     fixture.detectChanges();
@@ -38,7 +40,16 @@ describe('SdsFormlyResetComponent', () => {
 
   it('should reset to original options when clicked', () => {
     buttonDe.triggerEventHandler('click', null);
-    // click(buttonElement);
+    fixture.detectChanges();
     expect(component.options).toBe(options);
   })
 });
+
+
+@Component({
+  template: `
+      <sds-formly-reset [options]="options"></sds-formly-reset>`
+})
+class TestHostComponent {
+  options: FormlyFormOptions = {};
+}
