f4791885ec154aaf983979178ce3953c05f943b5
joshnezianya <joshua.nezianya@gsa.gov>
2020-05-13 07:59:45 -0400

    Refactored Template for Search Results List
    Added method to SDS result list component to check for conditions of model.

--- libs/packages/components/src/lib/search-result-list/search-result-list.component.ts
+++ libs/packages/components/src/lib/search-result-list/search-result-list.component.ts
@@ -1,11 +1,12 @@
-import { Component, Input, ContentChild, TemplateRef } from '@angular/core';
+import { Component, Input, ContentChild, TemplateRef, ViewChild, ElementRef, Renderer2, OnInit, OnChanges, AfterViewInit } from '@angular/core';
 @Component({
   selector: 'sds-search-result-list',
   templateUrl: './search-result-list.component.html',
   styleUrls: ['./search-result-list.component.scss']
 })
-export class SdsSearchResultListComponent {
+export class SdsSearchResultListComponent implements OnInit, OnChanges {
 
+constructor() {}
   /**
    * List of items
    */
@@ -20,4 +21,38 @@ export class SdsSearchResultListComponent {
    * Child Template to be used to display the data for each item in the list of items
    */
   @ContentChild('resultContent') resultContentTemplate: TemplateRef<any>;
+
+
+  data = {
+      title: '',
+      message: ''
+  }
+
+  ngOnInit(): void {
+    console.log(this.model)
+    // this.displayTemplate();
+  }
+
+  ngOnChanges(){
+      this.displayTemplate()
+  }
+
+  displayTemplate(){
+    if(this.model.error){
+      this.data.title = this.model.error.title;
+      this.data.message = this.model.error.description;
+    } else if (!this.model.error){
+      this.data.title = this.model.emptySearch.title;
+      this.data.message = this.model.emptySearch.description;
+    } else {
+      this.data.title ='';
+      this.data.message ='';
+    }
+    return;
+  }
+
+
+
+
+
 }
