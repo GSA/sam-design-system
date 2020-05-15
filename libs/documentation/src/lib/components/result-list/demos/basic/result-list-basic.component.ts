e0abe704c75638eeb99696ae20852e7129240c25
joshnezianya <joshua.nezianya@gsa.gov>
2020-05-15 16:03:55 -0400

    Fixed unsaved file
    Saved unsaved file

--- libs/documentation/src/lib/components/result-list/demos/basic/result-list-basic.component.ts
+++ libs/documentation/src/lib/components/result-list/demos/basic/result-list-basic.component.ts
@@ -1,26 +1,24 @@
-import { Component, ChangeDetectorRef} from '@angular/core';
+import { Component, ChangeDetectorRef } from '@angular/core';
 
 @Component({
   templateUrl: './result-list-basic.component.html'
 })
-
 export class ResultListBasic {
   constructor(private change: ChangeDetectorRef) {}
 
   itemsDefault: object = {
-    results:[] = [
+    results: [] = [
       { title: 'First', id: 1 },
       { title: 'Second', id: 2 },
       { title: 'Third', id: 3 },
       { title: 'Fourth', id: 4 },
       { title: 'Fifth', id: 5, hasNewerData: true }
     ]
-
   };
 
   noEntry: object = {
     emptySearch: {
-      title: "No Search",
+      title: 'No Search',
       description: `No Search criteria. Please try again
                     If you continue to have this problem, please contact the <a href='https://www.fsd.gov/'>Federal Service Desk.</a>`,
       icon: {
@@ -29,11 +27,11 @@ export class ResultListBasic {
         size: '6x'
       }
     }
-  }
+  };
 
   errorMessages: object = {
     error: {
-      title: "Bad Request",
+      title: 'Bad Request',
       description: `There was an issue with the search request. If you continue to
                     experience this issue, please contact the <a href='https://www.fsd.gov/'>Federal Service Desk.</a>`,
       icon: {
@@ -44,19 +42,19 @@ export class ResultListBasic {
     }
   };
 
-  emptyItems:object = {
+  emptyItems: object = {
     results: [] = [],
     noItems: {
-      title: "No Matches Found",
+      title: 'No Matches Found',
       description: `We couldn't find a match for your search criteria.
       We couldn't find a match for your search criteria.`,
       icon: {
         name: 'fa-alert-error',
         library: 'sds',
         size: '6x'
+      }
     }
-  }
-}
+  };
 
   messages = this.errorMessages;
   items = this.itemsDefault;
