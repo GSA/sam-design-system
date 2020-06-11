98f9092af44360d37e3da9ea5c78f4021756530a
yerramshilpa <yerramshilpa@gmail.com>
2020-06-11 00:23:00 -0400

    updated video package

--- tools/scripts/pack-and-publish.js
+++ tools/scripts/pack-and-publish.js
@@ -7,51 +7,62 @@ const parseArgs = require('minimist');
 
 main();
 
-function main () {
+function main() {
   const args = parseArgs(process.argv.slice(2));
   const rootDir = resolve(__dirname, '../../');
   const angularJsonPath = resolve(__dirname, '../../angular.json');
-  
+
   console.log('Loading angular.json...');
-  const { error: angularJsonError, contents: angularJson } = loadJson(angularJsonPath);
+  const { error: angularJsonError, contents: angularJson } = loadJson(
+    angularJsonPath
+  );
   handleError(angularJsonError);
 
   const libNames = findPublishableLibs(angularJson);
-  
+
   libNames.forEach(lib => {
     const distDir = resolve(rootDir, `dist/libs/${lib}/`);
     execSync(`ng build ${lib}`, { stdio: 'inherit' });
+    if (lib === 'components') {
+      execSync(
+        'cp -r ./libs/packages/components/src/assets/ ./dist/libs/components/assets'
+      );
+    }
     execSync(`npm pack`, { cwd: distDir, stdio: 'inherit' });
 
     const tarballPath = findTarball(distDir);
 
     if (tarballPath) {
-      const publishCmd = `npm publish ${tarballPath} ${args['dry-run'] ? '--dry-run' : ''}`;
+      const publishCmd = `npm publish ${tarballPath} ${
+        args['dry-run'] ? '--dry-run' : ''
+      }`;
       execSync(publishCmd, { cwd: distDir, stdio: 'inherit' });
     } else {
       const tarballError = new Error('No tarball found');
       tarballError.code = 1;
       handleError(tarballError);
     }
-    
   });
 }
 
-function findPublishableLibs (angularJson) {
+function findPublishableLibs(angularJson) {
   const projectNames = Object.keys(angularJson.projects);
 
   const libNames = projectNames.filter(name => {
     const metadata = angularJson.projects[name];
     // Returns true if lib is a library and uses the ng-packagr schematic (indicates intent to publish)
-    return metadata.projectType === 'library' 
-      && metadata.architect.build 
-      && metadata.architect.build.builder === '@angular-devkit/build-ng-packagr:build';
+    return (
+      metadata.projectType === 'library' &&
+      metadata.architect.build &&
+      metadata.architect.build.builder ===
+        '@angular-devkit/build-ng-packagr:build'
+    );
   });
 
   return libNames;
 }
 
-function findTarball (directory) {
+function findTarball(directory) {
   const { error, files } = loadDir(directory);
   handleError(error);
 
@@ -65,19 +76,19 @@ function findTarball (directory) {
   return tarball;
 }
 
-function loadDir (path) {
+function loadDir(path) {
   let error, files;
 
   try {
     files = readDirSync(path, 'utf-8');
   } catch (e) {
-    error = e
-  } 
+    error = e;
+  }
 
-  return { error, files }
+  return { error, files };
 }
 
-function loadJson (path) {
+function loadJson(path) {
   let error, contents;
 
   try {
@@ -91,7 +102,7 @@ function loadJson (path) {
   return { error, contents };
 }
 
-function handleError (error) {
+function handleError(error) {
   if (error) {
     console.error(error.message);
     process.exit(error.code);
