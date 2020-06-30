bbbf8995eee8d4f9c6c2fdb3d6b9a709406a18d9
yerramshilpa <yerramshilpa@gmail.com>
2020-06-29 22:27:48 -0400

    updated button css

--- libs/packages/components/src/lib/search-result-list/search-result-list.component.ts
+++ libs/packages/components/src/lib/search-result-list/search-result-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, ContentChild, TemplateRef, EventEmitter } from '@angular/core';
+import { Component, Input, ContentChild, TemplateRef, EventEmitter, OnInit } from '@angular/core';
 import { SearchModel } from './model/search-results.model';
 
 @Component({
@@ -7,12 +7,12 @@ import { SearchModel } from './model/search-results.model';
   styleUrls: ['./search-result-list.component.scss']
 })
 
-export class SdsSearchResultListComponent {
+export class SdsSearchResultListComponent implements OnInit {
 
    /**
    * Model for search results
    */
-  @Input() model:SearchModel;
+  @Input() model = new SearchModel() || [];
 
   /**
    * Show divider between results
@@ -23,5 +23,11 @@ export class SdsSearchResultListComponent {
    * Child Template to be used to display the data for each item in the list of items
    */
   @ContentChild('resultContent') resultContentTemplate: TemplateRef<any>;
-
+  ngOnInit() {
+    if(Array.isArray(this.model)) {
+      const items = this.model;
+      this.model = new SearchModel();
+      this.model.results = items;
+    }
+  }
 }
