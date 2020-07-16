00be2dff841ed9ffe954802ed6e5eab33d88bd4d
yerramshilpa <44271677+yerramshilpa@users.noreply.github.com>
2020-07-16 14:14:40 -0400

    updated css for tri state

--- libs/packages/sam-formly/src/lib/formly/types/multicheckbox.ts
+++ libs/packages/sam-formly/src/lib/formly/types/multicheckbox.ts
@@ -3,48 +3,70 @@ import { FieldType, FormlyTemplateOptions } from '@ngx-formly/core';
 
 @Component({
   selector: 'sds-formly-field-multicheckbox',
-  templateUrl: './multicheckbox.html'
+  templateUrl: './multicheckbox.html',
+  styleUrls: ['./multicheckbox.scss']
 })
-export class FormlyFieldMultiCheckboxComponent extends FieldType implements OnInit {
+export class FormlyFieldMultiCheckboxComponent extends FieldType
+  implements OnInit {
   defaultOptions = {
     templateOptions: {
-      options: [],
-    },
+      options: []
+    }
   };
   isCollapsedContent = false;
-  selectAllChecked: boolean;
-
+  allComplete: boolean;
+  ariaChecked = '';
   ngOnInit() {
-    this.isSelectAllChecked();
+    this.someComplete();
   }
   onChange(value: any, checked: boolean) {
     if (this.to.type === 'array') {
-      this.formControl.patchValue(checked
-        ? [...(this.formControl.value || []), value]
-        : [...(this.formControl.value || [])].filter(o => o !== value),
+      this.formControl.patchValue(
+        checked
+          ? [...(this.formControl.value || []), value]
+          : [...(this.formControl.value || [])].filter(o => o !== value)
       );
     } else {
-      this.formControl.patchValue({ ...this.formControl.value, [value]: checked });
+      this.formControl.patchValue({
+        ...this.formControl.value,
+        [value]: checked
+      });
     }
-    this.isSelectAllChecked();
+    this.someComplete();
     this.formControl.markAsTouched();
   }
 
-  isSelectAllChecked() {
+  someComplete() {
+    let value;
     if (this.formControl && this.formControl.value) {
-      if (!(Array.isArray(this.formControl.value))) {
-        this.selectAllChecked = Object.values(this.formControl.value).filter(x => x === true).length > 0 ? true : false;
+      if (!Array.isArray(this.formControl.value)) {
+        value = Object.values(this.formControl.value).filter(x => x === true)
+          .length;
       } else {
-        this.selectAllChecked = this.formControl.value.length > 0 ? true : false;
+        value = this.formControl.value.length;
       }
+    }
+    this._getAriaChecked(value);
+  }
+
+  setAll(ev) {
+    this.ariaChecked = 'true';
+    this.allComplete = ev.target.checked;
+    if (Array.isArray(this.field.templateOptions.options)) {
+      this.field.templateOptions.options.map(option => {
+        this.onChange(option.key, ev.target.checked);
+      });
     }
   }
 
-  onSelectAllChange(ev) {
-    if (Array.isArray(this.field.templateOptions.options)) {
-      this.field.templateOptions.options.map(option => {
-        this.onChange(option.key, ev.target.checked)
-      })
+  _getAriaChecked(value) {
+    this.allComplete =
+      value === this.field.templateOptions.options.length ? true : false;
+
+    if (value === 0) {
+      this.ariaChecked = 'true';
+    } else {
+      this.ariaChecked = value > 0 && !this.allComplete ? 'mixed' : 'false';
     }
   }
 }
