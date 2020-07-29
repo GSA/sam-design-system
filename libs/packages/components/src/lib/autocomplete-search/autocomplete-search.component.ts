cdd0b1823f5f19d707c53e21d0473661835019d0
yerramshilpa <44271677+yerramshilpa@users.noreply.github.com>
2020-07-29 16:33:53 -0400

    keydown working

--- libs/packages/components/src/lib/autocomplete-search/autocomplete-search.component.ts
+++ libs/packages/components/src/lib/autocomplete-search/autocomplete-search.component.ts
@@ -362,51 +362,73 @@ export class SDSAutocompleteSearchComponent implements ControlValueAccessor {
 
   private onArrowGroupDown(): void {
     if (this.results && this.results.length > 0) {
-      console.log(
-        this.results[this.highlightedIndex][this.configuration.groupByChild]
-          .length
-      );
       if (this.highlightedIndex < this.results.length - 1) {
-        if (
-          this.highlightedChildIndex ==
-          this.results[this.highlightedIndex][this.configuration.groupByChild]
-            .length
-        ) {
-          this.highlightedIndex++;
-          this.highlightedChildIndex = 0;
+        if (this.highlightedChildIndex != null) {
+          if (
+            this.highlightedChildIndex <
+            this.results[this.highlightedIndex][this.configuration.groupByChild]
+              .length -
+              1
+          ) {
+            this.setHighlightedItem(
+              this.results[this.highlightedIndex][
+                this.configuration.groupByChild
+              ][this.highlightedChildIndex]
+            );
+            this.highlightedChildIndex++;
+          } else if (
+            this.highlightedChildIndex ===
+            this.results[this.highlightedIndex][this.configuration.groupByChild]
+              .length -
+              1
+          ) {
+            this.setHighlightedItem(
+              this.results[this.highlightedIndex][
+                this.configuration.groupByChild
+              ][this.highlightedChildIndex]
+            );
+            this.highlightedIndex++;
+            this.highlightedChildIndex = null;
+          }
         } else {
-          this.highlightedChildIndex++;
+          this.setHighlightedItem(this.results[this.highlightedIndex]);
+          this.highlightedChildIndex = 0;
         }
-        this.setHighlightedItem(
-          this.results[this.highlightedIndex][this.configuration.groupByChild][
-            this.highlightedChildIndex
-          ]
-        );
-        this.scrollSelectedItemIntoView();
       }
+      this.scrollSelectedItemIntoView();
     }
   }
 
   private onArrowGroupUp(): void {
     if (this.results && this.results.length > 0) {
       if (this.highlightedIndex >= 0) {
-        if (this.highlightedChildIndex == 0) {
-          this.highlightedIndex == 0
-            ? this.highlightedIndex
-            : this.highlightedIndex--;
-          this.highlightedChildIndex = this.results[this.highlightedIndex][
-            this.configuration.groupByChild
-          ].length;
+        if (this.highlightedChildIndex != null) {
+          if (this.highlightedChildIndex > 0) {
+            this.setHighlightedItem(
+              this.results[this.highlightedIndex][
+                this.configuration.groupByChild
+              ][this.highlightedChildIndex]
+            );
+            this.highlightedChildIndex--;
+          } else if (this.highlightedChildIndex === 0) {
+            this.setHighlightedItem(
+              this.results[this.highlightedIndex][
+                this.configuration.groupByChild
+              ][this.highlightedChildIndex]
+            );
+            this.highlightedIndex != 0
+              ? this.highlightedIndex--
+              : this.highlightedIndex;
+            this.highlightedChildIndex = null;
+          }
         } else {
-          this.highlightedChildIndex--;
+          this.setHighlightedItem(this.results[this.highlightedIndex]);
+          this.highlightedChildIndex =
+            this.results[this.highlightedIndex][this.configuration.groupByChild]
+              .length - 1;
         }
-        this.setHighlightedItem(
-          this.results[this.highlightedIndex][this.configuration.groupByChild][
-            this.highlightedChildIndex
-          ]
-        );
-        this.scrollSelectedItemIntoView();
       }
+      this.scrollSelectedItemIntoView();
     }
   }
 
