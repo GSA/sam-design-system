332f9a2da974c0dc092dd7aeb7dc24af858aea64
joshnezianya <joshua.nezianya@gsa.gov>
2020-08-03 11:26:29 -0400

    Field updates.
    Removed template for formly config. Updated formly config. Added mock radio buttons.

--- libs/packages/layouts/src/lib/feature/download-modal/download-modal.component.ts
+++ libs/packages/layouts/src/lib/feature/download-modal/download-modal.component.ts
@@ -12,50 +12,87 @@ export class SdsDownloadModalComponent implements OnInit {
   fields: FormlyFieldConfig[];
   model: any;
   form: FormGroup;
-  options: FormlyFormOptions
-  constructor(private dialogService: SdsDialogService) { }
+  options: FormlyFormOptions;
+  constructor(private dialogService: SdsDialogService) {}
 
   ngOnInit() {
     this.form = new FormGroup({
       name: new FormControl('', Validators.required),
-      dateOfBirth: new FormControl('', Validators.required),
-    })
+      dateOfBirth: new FormControl('', Validators.required)
+    });
 
     this.model = {
       id: 'file',
-      name: 'Bazooka Joe',
-      dateOfBirth: '2001-01-01T05:00:00.000Z',
+      name: 'FileType.csv',
       options: 'multicheckbox'
     };
 
     this.fields = [
       {
-        key: 'file'
+        key: 'terms',
+        type: 'checkbox',
+        templateOptions: {
+          label: 'Download full record.',
+          required: true
+        }
       },
+      {
+        key: 'terms',
+        type: 'checkbox',
+        templateOptions: {
+          label: 'Download summary information only.',
+          required: true
+        }
+      },
+      {
+        key: 'terms',
+        type: 'checkbox',
+        templateOptions: {
+          label: 'Download FDPS case records.',
+          required: true
+        }
+      },{
+        key: 'terms',
+        type: 'checkbox',
+        templateOptions: {
+          label: 'Download current record and all of its modification.',
+          required: true
+        }
+      },
+      {
+        key: 'gender',
+        type: 'radio',
+        templateOptions: {
+          label: 'Select File Type',
+          // placeholder: 'Placeholder',
+          // description: 'Select File Type',
+          // options: [
+          //   { value: 1, label: 'Search Results' },
+          //   { value: 2, label: 'Search Results' },
+          //   { value: 3, label: 'WD Decision' },
+          // ],
+        },
+      },
+
       {
         key: 'name',
         type: 'input',
         templateOptions: {
           required: true,
-          label: 'Name',
+          label: 'Name'
         }
       },
       {
-        key: 'dateOfBirth',
-        type: 'datepicker',
+        key: 'terms',
+        type: 'checkbox',
         templateOptions: {
-          required: true,
-          label: 'Date Of Birth',
+          label: 'Add to my Saved Searches.',
+          required: false
         }
       },
-      {
-
-      }
+      {}
     ];
 
-    this.options = {
-    }
-
+    this.options = {};
   }
-
 }
