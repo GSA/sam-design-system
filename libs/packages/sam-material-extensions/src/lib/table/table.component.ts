30c008765b8c31db5c2dc80d159de55ecd396207
= <alexandra.screven@gsa.gov>
2020-08-12 16:35:27 -0400

    feature(table): additional expansion table styling

--- libs/packages/sam-material-extensions/src/lib/table/table.component.ts
+++ libs/packages/sam-material-extensions/src/lib/table/table.component.ts
@@ -1,18 +1,10 @@
 import { Component, Input } from '@angular/core';
 import * as _ from 'lodash';
-import {animate, state, style, transition, trigger} from '@angular/animations';
 
 @Component({
   selector: 'sds-table',
   templateUrl: './table.component.html',
-  styleUrls: ['./table.component.scss'],
-  animations: [
-    trigger('detailExpand', [
-      state('collapsed', style({height: '0px', minHeight: '0', display: 'none'})),
-      state('expanded', style({height: '*'})),
-      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
-    ]),
-  ],
+  styleUrls: ['./table.component.scss']
 })
 
 export class SdsTableComponent {
