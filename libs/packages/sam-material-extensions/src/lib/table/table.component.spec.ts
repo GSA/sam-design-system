458665a2c3b82e0023dda64f13d43d5547231fe8
= <alexandra.screven@gsa.gov>
2020-08-20 13:36:47 -0400

    feature(mat-table): unit test for sort being set to matSort instance and expandable row templateRef coming through

--- libs/packages/sam-material-extensions/src/lib/table/table.component.spec.ts
+++ libs/packages/sam-material-extensions/src/lib/table/table.component.spec.ts
@@ -1,104 +1,35 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SdsTableComponent } from './table.component';
-import { MatTableDataSource, MatTableModule, MatSortModule, MatSort } from '@angular/material';
+import { MatTableDataSource, MatTableModule, MatSortModule } from '@angular/material';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { Component, ViewChild } from '@angular/core';
+
+let data: any[] = [{
+  id: 1,
+  firstName: "Abagail",
+  lastName: "Podd"
+}, {
+  id: 2,
+  firstName: "Ave"
+}, {
+  id: 3,
+  firstName: "Tymothy"
+}];
+
+let columns = [
+  {
+    primaryKey: 'id'
+  },
+  {
+    primaryKey: 'firstName'
+  },
+]
 
 describe('SdsTableComponent', () => {
-  let component: SdsTableComponent;
+  let comp: SdsTableComponent;
   let fixture: ComponentFixture<SdsTableComponent>;
-  let data: any[] = [{
-    id: 1,
-    firstName: "Abagail",
-    lastName: "Podd",
-    email: "apodd0@uol.com.br",
-    gender: "Female",
-    ipAddress: "118.2.75.197"
-  }, {
-    id: 2,
-    firstName: "Ave",
-    lastName: "Stileman",
-    email: "astileman1@linkedin.com",
-    gender: "Male",
-    ipAddress: "160.87.118.244"
-  }, {
-    id: 3,
-    firstName: "Tymothy",
-    lastName: "Huff",
-    email: "thuff2@yelp.com",
-    gender: "Male",
-    ipAddress: "59.14.252.45"
-  }, {
-    id: 4,
-    firstName: "Mahala",
-    lastName: "Scroggie",
-    email: "mscroggie3@artisteer.com",
-    gender: "Female",
-    ipAddress: "86.218.233.126"
-  }, {
-    id: 5,
-    firstName: "Kermie",
-    lastName: "Favell",
-    email: "kfavell4@bigcartel.com",
-    gender: "Male",
-    ipAddress: "46.212.152.92"
-  }, {
-    id: 6,
-    firstName: "Jaymie",
-    lastName: "Runnicles",
-    email: "jrunnicles5@forbes.com",
-    gender: "Male",
-    ipAddress: "103.9.203.130"
-  }, {
-    id: 7,
-    firstName: "Jessamine",
-    lastName: "Mordey",
-    email: "jmordey6@sitemeter.com",
-    gender: "Female",
-    ipAddress: "170.185.216.142"
-  }, {
-    id: 8,
-    firstName: "Wayland",
-    lastName: "Canto",
-    email: "wcanto7@weibo.com",
-    gender: "Male",
-    ipAddress: "87.230.185.148"
-  }, {
-    id: 9,
-    firstName: "Irvin",
-    lastName: "Jocelyn",
-    email: "ijocelyn8@nytimes.com",
-    gender: "Male",
-    ipAddress: "165.166.209.58"
-  }, {
-    id: 10,
-    firstName: "Viviene",
-    lastName: "Smiley",
-    email: "vsmiley9@arizona.edu",
-    gender: "Female",
-    ipAddress: "255.136.64.110"
-  }];
 
-  let columns = [
-    {
-      primaryKey: 'id'
-    },
-    {
-      primaryKey: 'firstName'
-    },
-    {
-      primaryKey: 'lastName'
-    },
-    {
-      primaryKey: 'email'
-    },
-    {
-      primaryKey: 'gender'
-    },
-    {
-      primaryKey: 'ipAddress'
-    },
-  ]
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -107,33 +38,87 @@ describe('SdsTableComponent', () => {
         MatTableModule,
         FontAwesomeModule,
         MatSortModule
-      ]
+      ],
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(SdsTableComponent);
-    component = fixture.componentInstance;
-    component.columns = columns;
-    component.data = data;
+    comp = fixture.componentInstance;
+    comp.columns = columns;
+    comp.data = data;
     fixture.detectChanges();
   });
 
   it('should create', () => {
-    expect(component).toBeTruthy();
+    expect(comp).toBeTruthy();
   });
 
   it('should populate columnIds onInit', () => {
-    expect(component.columnIds.length).toEqual(columns.length)
+    expect(comp.columnIds.length).toEqual(columns.length)
   });
 
   it('should populate dataSource with MatTableDataSource objects onInit', () => {
-    expect(component.dataSource).toBeDefined();
-    expect(component.dataSource instanceof MatTableDataSource).toBeTruthy();
+    expect(comp.dataSource).toBeDefined();
+    expect(comp.dataSource instanceof MatTableDataSource).toBeTruthy();
   });
 
   it('should disable sort if settings are undefined or settingss.sort is false', () => {
-    expect(component.sort.disabled).toEqual(true);
+    expect(comp.sort.disabled).toEqual(true);
+  });
+
+  it('should set dataSource.sort to instance of child MatSort', () => {
+    comp.settings = {
+      sort: true
+    }
+    comp.ngOnInit();
+    expect(comp.dataSource.sort).toEqual(comp.sort);
+  });
+
+});
+
+
+@Component({
+  template: `
+    <ng-template #detailRow>
+      <span>Test Expanded Component</span>
+    </ng-template>
+    <sds-table [data]="data" [columns]="columns" [detailRow]="detailRow"></sds-table>`
+})
+class WrapperComponent {
+  @ViewChild(SdsTableComponent) tableComponentRef: SdsTableComponent;
+  columns = columns;
+  data = data;
+}
+
+describe('SdsTableComponent with expandable rows', () => {
+  let comp: SdsTableComponent;
+  let fixture: ComponentFixture<WrapperComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ SdsTableComponent, WrapperComponent ],
+      imports: [
+        MatTableModule,
+        FontAwesomeModule,
+        MatSortModule
+      ],
+    })
+    .compileComponents();
+  }));
+  beforeEach(() => {
+    fixture = TestBed.createComponent(WrapperComponent);
+
+    const wrapperComponent = fixture.debugElement.componentInstance;
+    comp = wrapperComponent.tableComponentRef; // SdsTableComponent
+    fixture.detectChanges();
+  });
+  it('should create the app', () => {
+    expect(comp).toBeDefined();
+  });
+
+  it('should add an additional column sdsExpandableRow if detailRow is set', () => {
+    expect(comp.columnIds.length).toEqual(columns.length + 1)
   });
 });
