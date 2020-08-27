330337d3d7d7c62724651d5d3ec2b38b99e5b5bc
yerramshilpa <44271677+yerramshilpa@users.noreply.github.com>
2020-08-26 23:44:22 -0400

    downloadmodel

--- libs/documentation/src/lib/components/subheader/demos/basic/subheader-basic.component.ts
+++ libs/documentation/src/lib/components/subheader/demos/basic/subheader-basic.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { FormlyFieldConfig, FormlyFormOptions } from '@ngx-formly/core';
+import { FormGroup } from '@angular/forms';
+import {
+  SdsDialogService,
+  SdsDialogRef,
+  SDS_DIALOG_DATA
+} from '@gsa-sam/components';
+import { SdsDownloadComponent } from 'libs/packages/layouts/src/lib/feature/download/download.component';
+import { SdsFormlyDialogComponent } from '@sam-design-system/sam-formly';
 
 @Component({
   templateUrl: './subheader-basic.component.html',
   styleUrls: ['./subheader-basic.component.scss']
 })
 export class SubHeaderBasic {
-
   subheader = {
     buttons: [
       { id: 'FirstButton', text: 'Button', class: 'usa-button--secondary' },
@@ -17,8 +25,102 @@ export class SubHeaderBasic {
       { id: 'ShareBtn', icon: 'bars', text: 'Share' }
     ]
   };
-
   log(value) {
     console.log(`%cLog: ${value}`, 'color: blue; font-weight: bold');
+  }
+  // Code begin for download
+  fields: FormlyFieldConfig[] = [
+    {
+      key: 'options',
+      type: 'radio',
+      templateOptions: {
+        label: 'Choose options',
+        options: [
+          {
+            value: 'Download Full record',
+            key: 'dfr'
+          },
+          {
+            value: 'Download summary information only',
+            key: 'dsi'
+          },
+          {
+            value: 'Download FDPS case record',
+            key: 'dfc'
+          },
+          {
+            value: 'Download current record and all of its modifications',
+            key: 'dcr'
+          }
+        ]
+      }
+    },
+    {
+      key: 'fileType',
+      type: 'fileinfo',
+      templateOptions: {
+        label: 'Select file type',
+        options: [
+          { value: 'Search Results', key: 'CSV' },
+          { value: 'Search Results', key: 'PDF' },
+          { value: 'WD Decision', key: 'ZIP' }
+        ]
+      }
+    },
+    {
+      key: 'fileName',
+      type: 'input',
+      templateOptions: {
+        label: 'Name'
+      }
+    },
+    {
+      key: 'additionalOptions',
+      type: 'multicheckbox',
+      templateOptions: {
+        options: [
+          {
+            value: 'Add to my saved search',
+            key: 'saved'
+          }
+        ]
+      }
+    }
+  ];
+  model: any = { fileType: 'CSV' };
+  form: FormGroup;
+  configurations = {
+    title: 'Download Dialog',
+    buttonText: 'Download',
+    submitButtonText: 'Download',
+    infoMessage: ``
+  };
+  options: FormlyFormOptions;
+  downloadResponse = {};
+
+  constructor(public dialog: SdsDialogService) {}
+  onActionMenuItem(btnId) {
+    if (btnId == 'DownloadBtn') {
+      const data: any = {
+        fields: this.fields,
+        originalFields: this.fields,
+        originalModel: this.model,
+        submit: 'Download',
+        title: 'Download'
+      };
+
+      const dialogRef = this.dialog.open(SdsFormlyDialogComponent, {
+        width: 'medium',
+        data: data
+      });
+
+      dialogRef.afterClosed().subscribe(result => {
+        if (result) {
+          this.downloadResponse = result;
+        }
+      });
+    }
+
+    console.log('down', btnId);
   }
 }
--- libs/documentation/src/lib/components/subheader/demos/basic/subheader-basic.module.ts
+++ libs/documentation/src/lib/components/subheader/demos/basic/subheader-basic.module.ts
@@ -1,12 +1,33 @@
 import { NgModule } from '@angular/core';
 import { SubHeaderBasic } from './subheader-basic.component';
-import { SdsSubheaderModule } from '@sam-design-system/layouts';
+import {
+  SdsSubheaderModule,
+  SdsDownloadModule
+} from '@sam-design-system/layouts';
 import { CommonModule } from '@angular/common';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { SdsSearchModule } from '@gsa-sam/components';
+import { SdsSearchModule, SdsDialogModule } from '@gsa-sam/components';
+import {
+  SdsFormlyDialogModule,
+  SdsFormlyModule
+} from '@sam-design-system/sam-formly';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormlyModule } from '@ngx-formly/core';
 
 @NgModule({
-  imports: [CommonModule, SdsSubheaderModule, FontAwesomeModule, SdsSearchModule],
+  imports: [
+    CommonModule,
+    SdsSubheaderModule,
+    FontAwesomeModule,
+    SdsSearchModule,
+    SdsDownloadModule,
+    SdsFormlyDialogModule,
+    SdsDialogModule,
+    ReactiveFormsModule,
+    SdsFormlyModule,
+    FormlyModule,
+    FormlyModule
+  ],
   declarations: [SubHeaderBasic],
   exports: [SubHeaderBasic],
   bootstrap: [SubHeaderBasic]
