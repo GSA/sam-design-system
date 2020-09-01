c0666ada3c08f10995462d3d0f138c735545455e
= <alexandra.screven@gsa.gov>
2020-09-01 16:44:18 -0400

    gren-ignore: removed component-sample lib

--- apps/sam-design-system-site/src/app/app.routes.ts
+++ apps/sam-design-system-site/src/app/app.routes.ts
@@ -1,7 +1,6 @@
 import { Routes } from '@angular/router';
 
 export const ROUTES: Routes = [
-  { path: 'component', loadChildren: '@gsa-sam/component-sample#ComponentSampleModule' } ,
   { path: 'documentation', loadChildren: '@gsa-sam/documentation#DocumentationModule' },
   { path: '**', redirectTo: 'documentation/overview' }
 ];
