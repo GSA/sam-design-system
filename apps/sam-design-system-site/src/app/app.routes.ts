70d13759e3e55ea7796287924712163f65db5bbe
= <alexandra.screven@gsa.gov>
2020-09-01 15:40:52 -0400

    gren-ignore: remove sam-formly-sample

--- apps/sam-design-system-site/src/app/app.routes.ts
+++ apps/sam-design-system-site/src/app/app.routes.ts
@@ -2,8 +2,6 @@ import { Routes } from '@angular/router';
 
 export const ROUTES: Routes = [
   { path: 'component', loadChildren: '@gsa-sam/component-sample#ComponentSampleModule' } ,
-  { path: 'formly', loadChildren: '@gsa-sam/sam-formly-sample#SamFormlySampleModule' },
   { path: 'documentation', loadChildren: '@gsa-sam/documentation#DocumentationModule' },
   { path: '**', redirectTo: 'documentation/overview' }
-
 ];
