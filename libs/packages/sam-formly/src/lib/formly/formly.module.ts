a70c8f074ecccce6957066774345a9b7ab724ba3
= <alexandra.screven@gsa.gov>
2020-09-11 13:53:48 -0400

    updated experimental tsconfig.lib.json and removed formly moduleWithProvider

--- libs/packages/sam-formly/src/lib/formly/formly.module.ts
+++ libs/packages/sam-formly/src/lib/formly/formly.module.ts
@@ -1,4 +1,4 @@
-import { NgModule, ModuleWithProviders } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { FormlyModule, FormlyFieldConfig, ConfigOption } from '@ngx-formly/core';
