e0047ffbb45c7735c0ab57247c59db016aba08a6
Alexandra Screven <alexandra.screven@gsa.gov>
2020-09-21 14:54:53 -0400

    added empty sam-design-system-site app

---
+++ apps/sam-design-system-site/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sds-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'sam-design-system-site';
+}
