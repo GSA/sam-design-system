41afea01e3b65885e52bb1118ef7f13f3128fad4
Alexandra Screven <alexandra.screven@gsa.gov>
2020-09-22 11:18:57 -0400

    rebuilt to change cypress to protractor

--- apps/sam-design-system-site/src/app/app.component.spec.ts
+++ apps/sam-design-system-site/src/app/app.component.spec.ts
@@ -1,9 +1,11 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
       declarations: [AppComponent],
     }).compileComponents();
   }));
--- apps/sam-design-system-site/src/app/app.module.ts
+++ apps/sam-design-system-site/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot([], { initialNavigation: 'enabled' }),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
