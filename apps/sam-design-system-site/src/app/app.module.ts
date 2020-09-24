078f93b65e1bc13a137b10208538779f9fbd2d7c
Alexandra Screven <alexandra.screven@gsa.gov>
2020-09-24 16:38:09 -0400

    updated routing

--- apps/sam-design-system-site/src/app/app.module.ts
+++ apps/sam-design-system-site/src/app/app.module.ts
@@ -6,18 +6,13 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DocumentationModule } from '@gsa-sam/documentation';
 
-const ROUTES: Routes = [
-  { path: '',   redirectTo: '/documentation', pathMatch: 'full' },
-  { path: '**', redirectTo: '/documentation/overview' }
-];
-
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     DocumentationModule,
-    RouterModule.forRoot(ROUTES, { initialNavigation: 'enabled' }),
+    RouterModule.forRoot([], { initialNavigation: 'enabled' }),
   ],
   providers: [],
   bootstrap: [AppComponent],
--- libs/documentation/src/lib/documentation.module.ts
+++ libs/documentation/src/lib/documentation.module.ts
@@ -2,16 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { OverviewComponent } from './overview/overview.component';
+import { DocumentationComponent } from './documentation.component';
 
 @NgModule({
   imports: [
     CommonModule,
 
     RouterModule.forChild([
-      { path: 'overview', component: OverviewComponent },
-      { path: '', pathMatch: 'full', redirectTo: 'overview' } 
+      { path: 'documentation', children: [
+        { path: 'overview', component: OverviewComponent },
+        { path: '', pathMatch: 'full', redirectTo: 'overview' }
+      ]},
+      { path: '', pathMatch: 'full', redirectTo: 'documentation' }
     ]),
   ],
-  declarations: [OverviewComponent],
+  declarations: [OverviewComponent, DocumentationComponent],
+  exports: [DocumentationComponent],
 })
 export class DocumentationModule {}
