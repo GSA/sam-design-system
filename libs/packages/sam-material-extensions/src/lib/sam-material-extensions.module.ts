f6efed50388ee9ead41139af02ddd69177fb3918
Alexandra Screven <alexandra.screven@gsa.gov>
2020-09-24 16:02:03 -0400

    added blank table component

--- libs/packages/sam-material-extensions/src/lib/sam-material-extensions.module.ts
+++ libs/packages/sam-material-extensions/src/lib/sam-material-extensions.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TableComponent } from './table/table.component';
 
 @NgModule({
   imports: [CommonModule],
+  declarations: [TableComponent],
+  exports: [TableComponent],
 })
 export class SamMaterialExtensionsModule {}
