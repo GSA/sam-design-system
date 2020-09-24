34a1bf9b22f32f04f7f6b64186544349436b9752
Alexandra Screven <alexandra.screven@gsa.gov>
2020-09-24 16:54:12 -0400

    added documentation component

---
+++ libs/documentation/src/lib/documentation.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'docs-documentation',
+  templateUrl: './documentation.component.html',
+  styleUrls: ['./documentation.component.scss']
+})
+export class DocumentationComponent {
+
+  constructor() { }
+}
