0678408a9473aa1255e0e2f5e0d26fc96b2a32a1
Alexandra Screven <alexandra.screven@gsa.gov>
2020-09-24 17:06:46 -0400

    added compodoc build scripts for material library

---
+++ libs/documentation/src/lib/apidoc/material/material.ts
@@ -0,0 +1,12 @@
+const MATERIAL = {
+    "pipes": [],
+    "interfaces": [],
+    "injectables": [],
+    "classes": [],
+    "directives": [],
+    "components": [],
+    "modules": [],
+    "miscellaneous": []
+};
+
+export default MATERIAL;
