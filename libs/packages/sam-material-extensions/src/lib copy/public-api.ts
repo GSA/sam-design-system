94a46463b04046242699f31cc249f00cb5cd3ee5
yerramshilpa <44271677+yerramshilpa@users.noreply.github.com>
2020-09-29 01:35:26 -0400

    updated pack

--- libs/packages/sam-material-extensions/src/lib copy/public-api.ts
+++
@@ -1,2 +0,0 @@
-export * from './packages-sam-material-extensions.module';
-export * from './table/table.module';
--- libs/packages/sam-material-extensions/src/lib/public-api.ts
+++ libs/packages/sam-material-extensions/src/lib/public-api.ts
@@ -1,2 +1,4 @@
-export * from './packages-sam-material-extensions.module';
+export * from './accordion/accordion.module';
 export * from './table/table.module';
+export * from './table/models/table-column-settings.model';
+export * from './table/models/table-settings.model';
