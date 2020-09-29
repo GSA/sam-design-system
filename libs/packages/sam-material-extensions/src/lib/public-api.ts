f2551f922b5e8c70617208be898c64389e493e3e
yerramshilpa <44271677+yerramshilpa@users.noreply.github.com>
2020-09-29 17:45:12 -0400

    federalist fix

--- libs/packages/sam-material-extensions/src/lib/public-api.ts
+++ libs/packages/sam-material-extensions/src/lib/public-api.ts
@@ -2,3 +2,5 @@ export * from './accordion/accordion.module';
 export * from './table/table.module';
 export * from './table/models/table-column-settings.model';
 export * from './table/models/table-settings.model';
+export * from './table/table.component';
+export * from './accordion/accordion.component';
