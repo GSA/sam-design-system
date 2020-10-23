4a7bdf7fc232e318849e7a573f1e66c1041b66f8
yerramshilpa <44271677+yerramshilpa@users.noreply.github.com>
2020-10-23 12:44:50 -0400

    updated dependency versions

--- libs/packages/layouts/src/lib/feature/header/header.component.ts
+++ libs/packages/layouts/src/lib/feature/header/header.component.ts
@@ -11,8 +11,8 @@ import { NavigationHelper } from '@gsa-sam/components';
 })
 export class SdsHeaderComponent {
 
-  @ViewChild('usaNavOpen', { static: false }) openNavBtn: ElementRef;
-  @ViewChild('usaNavClose', { static: false }) closeNavBtn: ElementRef;
+  @ViewChild('usaNavOpen') openNavBtn: ElementRef;
+  @ViewChild('usaNavClose') closeNavBtn: ElementRef;
   mobileNavActive = false;
   /**
   * Navigation helper
