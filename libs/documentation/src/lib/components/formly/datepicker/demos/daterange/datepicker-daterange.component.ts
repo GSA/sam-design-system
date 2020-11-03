7dc8c0a417bf543c1c972aa53c1ae434cc9abe9f
Bibek-Regmi <bibek.regmi@gsa.gov>
2020-11-03 01:46:20 -0500

    Update demo to include both date range pickers

--- libs/documentation/src/lib/components/formly/datepicker/demos/daterange/datepicker-daterange.component.ts
+++ libs/documentation/src/lib/components/formly/datepicker/demos/daterange/datepicker-daterange.component.ts
@@ -14,9 +14,26 @@ export class FormlyDatepickerDateRange {
   fields: FormlyFieldConfig[] = [
     {
       key: 'expirationDateRangeEx',
-      type: 'daterangepickerv2',
+      type: 'daterangepicker',
       templateOptions: {
         label: 'Expiration Date Range',
+        minDate: new Date(2019, 9, 5),
+        maxDate: new Date(2020, 11, 15),
+      }
+    }
+  ];
+
+  results2: any = {};
+  form2 = new FormGroup({});
+  model2: any = {};
+  options2: FormlyFormOptions = {};
+
+  fields2: FormlyFieldConfig[] = [
+    {
+      key: 'expirationDateRangeEx',
+      type: 'daterangepickerv2',
+      templateOptions: {
+        label: 'Expiration Date Range - Angular 10+',
         minDate: new Date(2019, 9, 5),
         maxDate: new Date(2020, 11, 15),
       }
