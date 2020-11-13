268b6f7df56461d8ecb365883fea4f4c4b0f43c2
yerramshilpa <44271677+yerramshilpa@users.noreply.github.com>
2020-11-12 19:28:47 -0500

    button module exported

--- libs/packages/sam-material-extensions/src/lib/public-api.ts
+++ libs/packages/sam-material-extensions/src/lib/public-api.ts
@@ -4,3 +4,5 @@ export * from './table/models/table-column-settings.model';
 export * from './table/models/table-settings.model';
 export * from './table/table.component';
 export * from './accordion/accordion.component';
+export * from './button-group/button-group.module';
+export * from './button-group/button-group.component';
