1768ddf14808109e7f1adefcba8168288b3ecb55
yerramshilpa <44271677+yerramshilpa@users.noreply.github.com>
2020-12-09 21:13:45 -0500

    updated

--- libs/packages/layouts/src/lib/feature/search-list-layout/search-list-layout.component.ts
+++ libs/packages/layouts/src/lib/feature/search-list-layout/search-list-layout.component.ts
@@ -1,21 +1,40 @@
-import { Component, Input, ContentChild, TemplateRef, Optional, OnChanges, SimpleChanges, OnInit } from '@angular/core';
-import { BehaviorSubject } from "rxjs";
-import { SearchListInterface, SearchListConfiguration } from './model/search-list-layout.model';
+import {
+  Component,
+  Input,
+  ContentChild,
+  TemplateRef,
+  Optional,
+  OnChanges,
+  SimpleChanges,
+  OnInit,
+  ChangeDetectorRef,
+} from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import {
+  SearchListInterface,
+  SearchListConfiguration,
+} from './model/search-list-layout.model';
 import { SDSFormlyUpdateComunicationService } from '@gsa-sam/sam-formly';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'search-list-layout',
   templateUrl: './search-list-layout.component.html',
-  styleUrls: ['./search-list-layout.component.scss']
+  styleUrls: ['./search-list-layout.component.scss'],
 })
 export class SearchListLayoutComponent implements OnChanges, OnInit {
-
   /**
-  * Child Template to be used to display the data for each item in the list of items
-  */
+   * Child Template to be used to display the data for each item in the list of items
+   */
   @ContentChild('resultContent') resultContentTemplate: TemplateRef<any>;
 
-  constructor(@Optional() private formlyUpdateComunicationService: SDSFormlyUpdateComunicationService) { }
+  constructor(
+    private cdr: ChangeDetectorRef,
+    private router: Router,
+    private route: ActivatedRoute,
+    @Optional()
+    private formlyUpdateComunicationService: SDSFormlyUpdateComunicationService
+  ) {}
 
   /**
    * Input service to be called when items change
@@ -49,20 +68,15 @@ export class SearchListLayoutComponent implements OnChanges, OnInit {
   ngOnInit() {
     this.page.pageSize = this.configuration.pageSize;
     this.sortField = this.configuration.defaultSortValue;
-    this.paginationChange.subscribe(
-      () => {
-        this.updateContent();
-      }
-    );
+    this.paginationChange.subscribe(() => {
+      this.updateContent();
+    });
     if (this.formlyUpdateComunicationService) {
-      this.formlyUpdateComunicationService.filterUpdate.subscribe(
-        (filter) => {
-          this.updateFilter(filter);
-        }
-      )
+      this.formlyUpdateComunicationService.filterUpdate.subscribe((filter) => {
+        this.updateFilter(filter);
+      });
     }
   }
-
 
   /**
    * Default Page setttings
@@ -70,8 +84,8 @@ export class SearchListLayoutComponent implements OnChanges, OnInit {
   page = {
     pageNumber: 1,
     pageSize: 25,
-    totalPages: 0
-  }
+    totalPages: 0,
+  };
 
   /**
    * updates the filter and set the page number to 1 and calls imported service
@@ -81,6 +95,38 @@ export class SearchListLayoutComponent implements OnChanges, OnInit {
     this.filterData = filter;
     this.page.pageNumber = 1;
     this.updateContent();
+  }
+
+  updateNavigation() {
+    const queryString = window.location.search.substring(1);
+    let queryObj = qs.parse(queryString, { allowPrototypes: true });
+    if (queryObj.hasOwnProperty('sfm')) {
+      queryObj = {};
+    }
+    queryObj['sfm'] = this.filterData;
+    queryObj['pageNumber'] = this.page.pageNumber.toString();
+    queryObj['sorting'] = this.sortField.toString();
+
+    const params = this.convertToParam(queryObj);
+    this.router.navigate(['.'], {
+      relativeTo: this.route,
+      queryParams: params,
+      // TODO: Need this for future use case
+      // queryParamsHandling: 'merge'
+    });
+  }
+
+  convertToParam(filters) {
+    const encodedValues = qs.stringify(filters, {
+      skipNulls: true,
+      encode: false,
+      filter: this.shortFormatDate,
+    });
+    if (encodedValues) {
+      return this.getUrlParams(encodedValues);
+    } else {
+      return '';
+    }
   }
 
   /**
@@ -122,13 +168,19 @@ export class SearchListLayoutComponent implements OnChanges, OnInit {
   private updateContent() {
     if (this.filterData) {
       setTimeout(() => {
-        this.service.getData({ 'page': this.page, sortField: this.sortField, filter: this.filterData }).subscribe(
-          (result) => {
+        this.service
+          .getData({
+            page: this.page,
+            sortField: this.sortField,
+            filter: this.filterData,
+          })
+          .subscribe((result) => {
             this.items = result.items;
-            this.page.totalPages = Math.ceil(result.totalItems / this.page.pageSize);
+            this.page.totalPages = Math.ceil(
+              result.totalItems / this.page.pageSize
+            );
             this.totalItems = result.totalItems;
-          }
-        );
+          });
       });
     }
   }
