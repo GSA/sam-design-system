2feb1b72e4431b339fb580aeef803dacfd51108d
Bibek Regmi <bibekregmi@Bibek-Regmi-MBP.fios-router.home>
2021-01-06 12:40:53 -0500

    Update style and bootstrap versions, use bs icons on feedback component

--- libs/packages/layouts/src/lib/feature/sds-feedback/sds-feedback.module.ts
+++ libs/packages/layouts/src/lib/feature/sds-feedback/sds-feedback.module.ts
@@ -1,17 +1,16 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { ReactiveFormsModule } from "@angular/forms";
-import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
-import { SdsCollapseModule } from "@gsa-sam/components";
+import { SdsCollapseModule, SdsIconModule } from "@gsa-sam/components";
 import { SdsFeedbackComponent } from "./sds-feedback.component";
 
 
 @NgModule({
   imports: [
     CommonModule,
-    FontAwesomeModule,
     ReactiveFormsModule,
     SdsCollapseModule, 
+    SdsIconModule,
   ],
   declarations: [
     SdsFeedbackComponent
