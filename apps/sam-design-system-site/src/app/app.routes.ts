596cd051286706d4af1954c5cc93c9b11483a82b
Bibek Regmi <bibekregmi@Bibek-Regmi-MBP.fios-router.home>
2021-01-07 23:05:44 -0500

    Downgrade raw loader, update route to use dynamic import syntax

--- apps/sam-design-system-site/src/app/app.routes.ts
+++ apps/sam-design-system-site/src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
 
 export const ROUTES: Routes = [
-  { path: 'documentation', loadChildren: '@gsa-sam/documentation#DocumentationModule' },
+  { path: 'documentation', loadChildren: () => import('@gsa-sam/documentation').then(m => m.DocumentationModule) },
   { path: '**', redirectTo: 'documentation/overview' }
 ];
