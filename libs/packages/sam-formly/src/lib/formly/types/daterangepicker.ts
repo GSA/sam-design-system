42d3c86738b75213c4c18a56d61c04cfe966f3e4
Bibek Regmi <bibekregmi@Bibek-Regmi-MBP.fios-router.home>
2021-01-24 23:20:03 -0500

    update pagination demo and add flex display for datepicker with toggle

--- libs/packages/sam-formly/src/lib/formly/types/daterangepicker.ts
+++ libs/packages/sam-formly/src/lib/formly/types/daterangepicker.ts
@@ -4,25 +4,27 @@ import { FieldType } from '@ngx-formly/core';
 @Component({
   selector: 'sds-formly-field-daterangepicker',
   template: `
-    <mat-date-range-input [formGroup]="formControl" [rangePicker]="picker" [min]="to.minDate" [max]="to.maxDate" [id]="id"
-      class="usa-input display-inline-block margin-top-3"
-      [formlyAttributes]="field"
-      [class.usa-input--error]="showError">
-      <input matStartDate
-        [attr.aria-label]="field.fieldGroup[0]?.templateOptions?.placeholder || 'Start Date'"
-        [formlyAttributes]="field.fieldGroup[0]"
-        [placeholder]="field.fieldGroup[0]?.templateOptions?.placeholder || 'Start Date'"
-        [formControlName]="field.fieldGroup[0].key"
-        />
-        <input matEndDate
-        [attr.aria-label]="field.fieldGroup[1]?.templateOptions?.placeholder || 'End Date'"
-        [formlyAttributes]="field.fieldGroup[1]"
-        [placeholder]="field.fieldGroup[1]?.templateOptions?.placeholder || 'End Date'"
-        [formControlName]="field.fieldGroup[1].key"
-      />  
-    </mat-date-range-input>
-    <mat-datepicker-toggle matSuffix [for]="picker"></mat-datepicker-toggle>
-    <mat-date-range-picker [startAt]="to.startDate" #picker></mat-date-range-picker>
+    <div class="display-flex">
+      <mat-date-range-input [formGroup]="formControl" [rangePicker]="picker" [min]="to.minDate" [max]="to.maxDate" [id]="id"
+        class="usa-input display-inline-block margin-top-3"
+        [formlyAttributes]="field"
+        [class.usa-input--error]="showError">
+        <input matStartDate
+          [attr.aria-label]="field.fieldGroup[0]?.templateOptions?.placeholder || 'Start Date'"
+          [formlyAttributes]="field.fieldGroup[0]"
+          [placeholder]="field.fieldGroup[0]?.templateOptions?.placeholder || 'Start Date'"
+          [formControlName]="field.fieldGroup[0].key"
+          />
+          <input matEndDate
+          [attr.aria-label]="field.fieldGroup[1]?.templateOptions?.placeholder || 'End Date'"
+          [formlyAttributes]="field.fieldGroup[1]"
+          [placeholder]="field.fieldGroup[1]?.templateOptions?.placeholder || 'End Date'"
+          [formControlName]="field.fieldGroup[1].key"
+        />  
+      </mat-date-range-input>
+      <mat-datepicker-toggle class="padding-top-3" matSuffix [for]="picker"></mat-datepicker-toggle>
+      <mat-date-range-picker [startAt]="to.startDate" #picker></mat-date-range-picker>
+    </div>
   `,
   styles: ['.mat-date-range-input-start-wrapper {overflow: unset !important; }', 
             '.mat-date-range-input-end-wrapper {flex-grow: unset !important; }',
