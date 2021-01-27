1302e979c473d552791bced46f18d06dbfd8d26d
yerramshilpa <44271677+yerramshilpa@users.noreply.github.com>
2021-01-27 09:59:17 -0500

    accordion wrapper change with accordion next

--- libs/packages/sam-formly/src/lib/formly/wrappers/form-field.accordian.ts
+++ libs/packages/sam-formly/src/lib/formly/wrappers/form-field.accordian.ts
@@ -4,21 +4,30 @@ import * as qs from 'qs';
 
 /**
  * @param {string} [to.expand] to expand the accordion
- * 
+ *
  */
 
 @Component({
   selector: 'sam-formly-accordian-form-field',
   template: `
-  <sds-accordion-next [(multi)]="multi" #sdsAccordionDemo>
-  <sds-accordion-item  class="sds-accordion__panel" [expanded]="modelHasValue()">
-    <sds-accordion-title>  {{ to.label }}</sds-accordion-title>
-    <sds-accordion-content>
-    <ng-container #fieldComponent></ng-container>
-    </sds-accordion-content>
-  </sds-accordion-item>
-</sds-accordion-next>
-  `
+    <sds-accordion-next
+      [(multi)]="multi"
+      expandedHeight="34px"
+      collapsedHeight="34px"
+      #sdsAccordionDemo
+      class="sds-accordion--filters"
+    >
+      <sds-accordion-item
+        class="sds-accordion__panel"
+        [expanded]="modelHasValue()"
+      >
+        <sds-accordion-title> {{ to.label }}</sds-accordion-title>
+        <sds-accordion-content>
+          <ng-container #fieldComponent></ng-container>
+        </sds-accordion-content>
+      </sds-accordion-item>
+    </sds-accordion-next>
+  `,
 })
 export class FormlyAccordianFormFieldComponent extends FieldWrapper {
   @ViewChild('fieldComponent', { read: ViewContainerRef })
