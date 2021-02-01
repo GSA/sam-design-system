c8ab5e3dde8b0c827c4825ea3b97ee753ac4e405
yerramshilpa <44271677+yerramshilpa@users.noreply.github.com>
2021-02-01 15:11:10 -0500

    removed module

--- libs/packages/sam-formly/src/lib/formly-filters/sds-filters.module.ts
+++ libs/packages/sam-formly/src/lib/formly-filters/sds-filters.module.ts
@@ -3,22 +3,18 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { FormlyModule } from '@ngx-formly/core';
 import { FormlySelectModule } from '@ngx-formly/core/select';
-import { SdsAccordionModule } from '@gsa-sam/sam-material-extensions';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { SdsFiltersComponent } from './sds-filters.component';
 import { SdsFormlyModule } from '../formly/formly.module';
 import { SdsFormlyResetModule } from '../formly-reset/formly-reset.module';
 import { AdvancedFiltersComponent } from './advanced-filters/advanced-filters.component';
 import { SDSFormlyUpdateModelService } from './service/sds-filter-model-update.service';
-import { MatExpansionModule } from '@angular/material/expansion';
 
 @NgModule({
   declarations: [SdsFiltersComponent, AdvancedFiltersComponent],
   imports: [
     CommonModule,
     FormsModule,
-    MatExpansionModule,
-    SdsAccordionModule,
     SdsFormlyModule,
     SdsFormlyResetModule,
     FontAwesomeModule,
