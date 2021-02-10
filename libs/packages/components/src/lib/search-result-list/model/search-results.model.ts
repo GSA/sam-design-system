cb646bbd16333f915d7aa4272a7e44a2ac06d8a4
yerramshilpa <44271677+yerramshilpa@users.noreply.github.com>
2021-02-10 18:55:40 -0500

    Updated search results to include custom template

--- libs/packages/components/src/lib/search-result-list/model/search-results.model.ts
+++ libs/packages/components/src/lib/search-result-list/model/search-results.model.ts
@@ -1,21 +1,21 @@
-import { EventEmitter } from '@angular/core';
+import { TemplateRef } from '@angular/core';
 
-export class SearchModel{
-    results: any[];
-    metadata: {
-      messages:Message[];
-    }
+export class SearchModel {
+  results: any[];
+  metadata: {
+    messages: Message[] | TemplateRef<any>;
   };
-  export class Message {
-    type: string;
-    title: string;
-    message:string;
-    classes: string;
-    buttons:Button[];
-  }
-  export class Button{
-    id: string;
-    text:string;
-    classes: string;
-    action: any;
-  } 
+}
+export class Message {
+  type: string;
+  title: string;
+  message: string;
+  classes: string;
+  buttons: Button[];
+}
+export class Button {
+  id: string;
+  text: string;
+  classes: string;
+  action: any;
+}
