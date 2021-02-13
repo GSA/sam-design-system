83e02a27fc7aa8e62d8d07cfa87907034628e07d
yerramshilpa <44271677+yerramshilpa@users.noreply.github.com>
2021-02-13 18:03:26 -0500

    demo updated

--- libs/documentation/src/lib/components/download/demos/formlymodal/formly-modal.component.ts
+++ libs/documentation/src/lib/components/download/demos/formlymodal/formly-modal.component.ts
@@ -2,17 +2,26 @@ import { Component } from '@angular/core';
 import { FormlyFieldConfig, FormlyFormOptions } from '@ngx-formly/core';
 import {
   SdsFormlyDialogData,
-  SdsFormlyDialogComponent
+  SdsFormlyDialogComponent,
 } from '@gsa-sam/sam-formly';
-import { SdsDialogService } from '@gsa-sam/components';
+import {
+  SDSAutocompletelConfiguration,
+  SdsDialogService,
+  SDSSelectedItemModel,
+  SelectionMode,
+} from '@gsa-sam/components';
+import { AutocompleteSampleDataService } from './autocomplete-sample.service';
 
 @Component({
   templateUrl: 'formly-modal.component.html',
   selector: `sds-formly-dialog-demo`,
+  providers: [AutocompleteSampleDataService],
 })
 export class FormlyDialog {
   updatedModel: any = {};
   model: any = {};
+  public settings = new SDSAutocompletelConfiguration();
+  public autocompleteModel = new SDSSelectedItemModel();
   options: FormlyFormOptions;
   fields: FormlyFieldConfig[] = [
     {
@@ -22,12 +31,50 @@ export class FormlyDialog {
         label: 'Entity Name',
         placeholder: 'Acme Corporation',
         description: 'Enter the name of your entity.',
-        required: true
-      }
-    }
+        required: true,
+      },
+    },
+    {
+      key: 'filters',
+      wrappers: ['filterwrapper'],
+      templateOptions: { label: 'Keyword' },
+      fieldGroup: [
+        {
+          key: 'firstName',
+          type: 'autocomplete',
+          templateOptions: {
+            label: 'Auto Complete',
+            hideLabel: true,
+            service: this.service,
+            configuration: this.settings,
+            model: this.autocompleteModel,
+            modelChange: this.changes,
+          },
+        },
+      ],
+    },
   ];
+  constructor(
+    public service: AutocompleteSampleDataService,
+    public dialog: SdsDialogService
+  ) {
+    this.setup();
+  }
+  changes(value) {
+    console.log(value);
+  }
 
-  constructor(public dialog: SdsDialogService) { }
+  setup() {
+    this.settings.id = 'autocompleteBasic';
+    this.settings.primaryKeyField = 'id';
+    this.settings.primaryTextField = 'name';
+    this.settings.secondaryTextField = 'subtext';
+    this.settings.labelText = 'Autocomplete 1';
+    this.settings.selectionMode = SelectionMode.SINGLE;
+    this.settings.autocompletePlaceHolderText = 'Enter text';
+    this.settings.debounceTime = 350;
+  }
+
   openDialog() {
     const data: any = {
       fields: this.fields,
@@ -35,15 +82,15 @@ export class FormlyDialog {
       submit: 'Submit',
       title: 'Formly Dialog',
       options: this.options,
-      cancel: 'No thanks'
+      cancel: 'No thanks',
     };
 
     const dialogRef = this.dialog.open(SdsFormlyDialogComponent, {
       width: 'medium',
-      data: data
+      data: data,
     });
 
-    dialogRef.afterClosed().subscribe(result => {
+    dialogRef.afterClosed().subscribe((result) => {
       if (result) {
         this.updatedModel = result;
       }
