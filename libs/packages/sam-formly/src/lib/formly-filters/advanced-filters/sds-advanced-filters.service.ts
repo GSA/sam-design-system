f5cc5a47ac9007cf8e57ad2c9f0cdedd72aae293
yerramshilpa <44271677+yerramshilpa@users.noreply.github.com>
2021-04-22 04:13:06 -0400

    More Filters component displaying filter for empty object

--- libs/packages/sam-formly/src/lib/formly-filters/advanced-filters/sds-advanced-filters.service.ts
+++ libs/packages/sam-formly/src/lib/formly-filters/advanced-filters/sds-advanced-filters.service.ts
@@ -2,31 +2,33 @@ import { Injectable } from '@angular/core';
 import { FormlyFieldConfig } from '@ngx-formly/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SdsAdvancedFiltersService {
-  constructor() { }
+  constructor() {}
 
   convertToCheckboxes(origFields: FormlyFieldConfig[]): FormlyFieldConfig[] {
     const fields: FormlyFieldConfig[] = [];
-    origFields.forEach(origField => {
+    origFields.forEach((origField) => {
       if (origField.fieldGroup && origField.fieldGroup.length > 1) {
         const field = this.createMulticheckbox(origField);
         fields.push(field);
       } else {
-        const field: FormlyFieldConfig = {
-          type: 'checkbox',
-          key: origField.key,
-          defaultValue: !origField.hide,
-          templateOptions: {
-            hideOptional: true
-          }
-        };
+        if (origField.key) {
+          const field: FormlyFieldConfig = {
+            type: 'checkbox',
+            key: origField.key,
+            defaultValue: !origField.hide,
+            templateOptions: {
+              hideOptional: true,
+            },
+          };
 
-        if (origField.templateOptions && origField.templateOptions.label) {
-          field.templateOptions.label = origField.templateOptions.label;
+          if (origField.templateOptions && origField.templateOptions.label) {
+            field.templateOptions.label = origField.templateOptions.label;
+          }
+          fields.push(field);
         }
-        fields.push(field);
       }
     });
     return fields;
@@ -36,7 +38,7 @@ export class SdsAdvancedFiltersService {
   createMulticheckbox(origField: FormlyFieldConfig): FormlyFieldConfig {
     const options = [];
     const defaultValue = [];
-    origField.fieldGroup.forEach(field => {
+    origField.fieldGroup.forEach((field) => {
       const label =
         field.templateOptions && field.templateOptions.label
           ? field.templateOptions.label
@@ -60,8 +62,8 @@ export class SdsAdvancedFiltersService {
         hideOptional: true,
         selectAllOption: true,
         type: 'array',
-        options: options
-      }
+        options: options,
+      },
     };
 
     if (origField.templateOptions && origField.templateOptions.label) {
@@ -87,7 +89,7 @@ export class SdsAdvancedFiltersService {
     });
     return {
       fields: fields,
-      model: model
+      model: model,
     };
   }
 
@@ -98,14 +100,14 @@ export class SdsAdvancedFiltersService {
   ) {
     if (selectedFields && selectedFields.length) {
       parentField.hide = false;
-      parentField.fieldGroup.forEach(field => {
+      parentField.fieldGroup.forEach((field) => {
         const key = field.key;
         const fieldSelected = selectedFields.includes(key);
         this.updateSingleField(field, fieldSelected, model);
       });
     } else {
       parentField.hide = true;
-      parentField.fieldGroup.forEach(field => {
+      parentField.fieldGroup.forEach((field) => {
         this.updateSingleField(field, false, model);
       });
     }
