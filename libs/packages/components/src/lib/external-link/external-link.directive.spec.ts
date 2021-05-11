17a0ce3050bfc837299027cf52c8afeb969bf791
Bibek-Regmi <bibek.regmi@gsa.gov>
2021-05-11 17:19:10 -0400

    Update logic for aria label for external link directive

--- libs/packages/components/src/lib/external-link/external-link.directive.spec.ts
+++ libs/packages/components/src/lib/external-link/external-link.directive.spec.ts
@@ -8,9 +8,11 @@ import { BrowserDynamicTestingModule } from '@angular/platform-browser-dynamic/t
 @Component({
   template: `
     <a id="test" href="google.com">Google </a>
-    <a id="test2" [hideIcon]="true" href="google.com" aria-label="test aria label">Google </a>
+    <a id="test2" [hideIcon]="true" href="google.com" aria-label="test aria label - opens in a new window">Google </a>
     <a id="test3">Not Google </a>
     <a id="test4" href="{{name}}/settings/test123">Google </a>
+    <a id="test5" [hideIcon]="true" href="google.com" aria-label="test aria label with no keywords">Google </a>
+    <a id="test6" [hideIcon]="true" href="google.com">Google <span>test element</span></a>
   `
 })
 class TestComponent {
@@ -54,21 +56,33 @@ describe('Sam External Link Directive', () => {
     expect(icons.length).toEqual(1);
   });
 
-  it ('Showd add aria label attribute to external links if one does not exist', () => {
+  it ('Should add aria label attribute to external links using inner text if one does not exist', () => {
     fixture.detectChanges();
     const testElementWithoutAriaLabel = fixture.debugElement.query(By.css('#test'));
-    expect(testElementWithoutAriaLabel.attributes['aria-label']).toEqual('Open google.com in a new window');
+    expect(testElementWithoutAriaLabel.attributes['aria-label']).toEqual('Open Google in a new window');
   });
 
-  it('Should not add aria label to external link if one is already provided', () => {
+  it('Should not update aria label to external link if valid aria label is set', () => {
     fixture.detectChanges();
     const testElementWithoutAriaLabel = fixture.debugElement.query(By.css('#test2'));
-    expect(testElementWithoutAriaLabel.attributes['aria-label']).toEqual('test aria label');
+    expect(testElementWithoutAriaLabel.attributes['aria-label']).toEqual('test aria label - opens in a new window');
   });
 
   it('Should not add aria label to internal links', () => {
     fixture.detectChanges();
     const testElementWithoutAriaLabel = fixture.debugElement.query(By.css('#test4'));
     expect(testElementWithoutAriaLabel.attributes['aria-label']).toEqual('');
-  })
+  });
+
+  it('Should update aria label to external link if existing aria label does not indicate opening in new window', () => {
+    fixture.detectChanges();
+    const testElementWithoutAriaLabel = fixture.debugElement.query(By.css('#test5'));
+    expect(testElementWithoutAriaLabel.nativeNode['ariaLabel']).toEqual('test aria label with no keywords - opens in a new window');
+  });
+
+  it ('Should add aria label attribute to external links using href if one does not exist', () => {
+    fixture.detectChanges();
+    const testElementWithoutAriaLabel = fixture.debugElement.query(By.css('#test6'));
+    expect(testElementWithoutAriaLabel.attributes['aria-label']).toEqual('Open google.com in a new window');
+  });
 });
