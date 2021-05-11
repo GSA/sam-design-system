77bd764b38a431e0207ef31fd187ee2798dcd43f
Cameron Wolf <cameron.wolf@gsa.gov>
2021-05-11 09:57:24 -0400

    Add date-pipe

---
+++ libs/packages/components/src/lib/date/sds-date.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule, DatePipe } from '@angular/common';
+import { SdsDatePipe } from './sds-date.pipe';
+
+
+@NgModule({
+  declarations: [SdsDatePipe],
+  imports: [
+    CommonModule
+  ],
+  exports: [SdsDatePipe],
+  providers: [DatePipe]
+})
+export class SdsDateModule { }
---
+++ libs/packages/components/src/lib/date/sds-date.pipe.spec.ts
@@ -0,0 +1,63 @@
+import { DatePipe } from '@angular/common';
+import { SdsDatePipe } from './sds-date.pipe';
+
+fdescribe('SdsDatePipe', () => {
+  it('create an instance', () => {
+    const pipe = new SdsDatePipe(new DatePipe('en-us'));
+    expect(pipe).toBeTruthy();
+  });
+  it('should display the time when date displayed is today (milliseconds)', () => {
+    const pipe = new SdsDatePipe(new DatePipe('en-us'));
+    const today = Date.now();
+    const todayDate = new Date(today);
+    const hours = todayDate.getHours();
+    const min = todayDate.getMinutes();
+    expect(pipe.transform(today)).toBe(`${hours > 12 ? hours - 12 : hours}:${min} ${hours > 12 ? 'PM': 'AM'}`);
+  });
+  it('should display the time when date displayed is today (date string)', () => {
+    const pipe = new SdsDatePipe(new DatePipe('en-us'));
+    const todayDate = new Date();
+    const today = todayDate.toISOString()
+    const hours = todayDate.getHours();
+    const min = todayDate.getMinutes();
+    expect(pipe.transform(today)).toBe(`${hours > 12 ? hours - 12 : hours}:${min} ${hours > 12 ? 'PM': 'AM'}`);
+  });
+
+  it('should display the date without the year when date displayed is this calendar year (milliseconds)', () => {
+    const pipe = new SdsDatePipe(new DatePipe('en-us'));
+    const todayDate = new Date(Date.now());
+    if(todayDate.getDate()=== 1){
+      todayDate.setDate(todayDate.getDate() + 1);
+    } else {
+      todayDate.setDate(todayDate.getDate() - 1);
+    }
+    const today = todayDate.getTime()
+    expect(pipe.transform(today)).toBe(new Intl.DateTimeFormat('en-us', {month: 'short', day: 'numeric'}).format(todayDate));
+  });
+  it('should display the date without the year when date displayed is this calendar year (date time)', () => {
+    const pipe = new SdsDatePipe(new DatePipe('en-us'));
+    const todayDate = new Date(Date.now());
+    if(todayDate.getDate()=== 1){
+      todayDate.setDate(todayDate.getDate() + 1);
+    } else {
+      todayDate.setDate(todayDate.getDate() - 1);
+    }
+    const today = todayDate.toISOString()
+    expect(pipe.transform(today)).toBe(new Intl.DateTimeFormat('en-us', {month: 'short', day: 'numeric'}).format(todayDate));
+  });
+
+  it('should display the date with the year when date displayed is not this calendar year (milliseconds)', () => {
+    const pipe = new SdsDatePipe(new DatePipe('en-us'));
+    const todayDate = new Date(Date.now());
+    todayDate.setFullYear(todayDate.getFullYear() - 1);
+    const today = todayDate.getTime()
+    expect(pipe.transform(today)).toBe(new Intl.DateTimeFormat('en-us', {month: 'short', day: 'numeric', year: 'numeric'}).format(todayDate));
+  });
+  it('should display the date without the year when date displayed is this calendar year (date time)', () => {
+    const pipe = new SdsDatePipe(new DatePipe('en-us'));
+    const todayDate = new Date(Date.now());
+    todayDate.setFullYear(todayDate.getFullYear() - 1);
+    const today = todayDate.toISOString()
+    expect(pipe.transform(today)).toBe(new Intl.DateTimeFormat('en-us', {month: 'short', day: 'numeric', year: 'numeric'}).format(todayDate));
+  });
+});
