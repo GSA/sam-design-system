4c88caa01de41f70bb79abb236bc7508557a21d6
Cameron Wolf <cameron.wolf@gsa.gov>
2021-05-12 14:35:26 -0400

    First pass at theme switching

--- apps/sam-design-system-site/src/app/app.component.ts
+++ apps/sam-design-system-site/src/app/app.component.ts
@@ -5,4 +5,29 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent { }
+export class AppComponent {
+  readonly displayThemeKey = 'displayTheme';
+  public readonly sdsThemeString = 'sds';
+  public readonly uswdsThemeString = 'uswds';
+  currentDisplayed;
+  styleSheetLink: Element;
+  constructor(){
+    this. styleSheetLink = document.querySelector(
+      `link[href="sds.css"],[href="uswds.css"]`
+    );
+    this.currentDisplayed = localStorage.getItem(this.displayThemeKey);
+    if(this.currentDisplayed === null){
+      localStorage.setItem(this.displayThemeKey, this.sdsThemeString);
+      this.currentDisplayed = this.sdsThemeString;
+    };
+    if(this.styleSheetLink.getAttribute('href') !== `${this.currentDisplayed}.css`){
+      this.swapTheme(this.currentDisplayed)
+    }
+
+  }
+  swapTheme(themeString){
+    this.styleSheetLink.setAttribute('href', `${themeString}.css`);
+    this.currentDisplayed = themeString;
+    localStorage.setItem(this.displayThemeKey, themeString);
+  }
+}
