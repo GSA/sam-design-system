6a97e3626d7a61a1d6f00b45a58dc2164a800344
Thomas Tighe <thomas.tighe@gsa.gov>
2021-10-11 09:18:35 -0400

    minor cleanup

--- libs/documentation/src/lib/components/subheader/demos/request/sds-subheader-wrapper-request.module.ts
+++ libs/documentation/src/lib/components/subheader/demos/request/sds-subheader-wrapper-request.module.ts
@@ -3,16 +3,10 @@ import { SdsSubheaderModule } from '@gsa-sam/layouts';
 import { CommonModule } from '@angular/common';
 import { SdsSubheaderWrapperRequestComponent } from './sds-subheader-wrapper-request.component';
 
-// import { NgxBootstrapIconsModule, chevronLeft } from 'ngx-bootstrap-icons';
-// import { IconModule } from '@gsa-sam/ngx-uswds-icons';
-
-
-
 @NgModule({
   imports: [
     CommonModule,
     SdsSubheaderModule,
-  //  NgxBootstrapIconsModule.pick({chevronLeft})
   ],
   declarations: [SdsSubheaderWrapperRequestComponent],
   exports: [SdsSubheaderWrapperRequestComponent],
