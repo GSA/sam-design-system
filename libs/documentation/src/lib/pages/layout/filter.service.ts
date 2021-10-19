e6dcb86bbabf06fbe58af4ed57a6d99f2dd05967
yerramshilpa <44271677+yerramshilpa@users.noreply.github.com>
2021-10-18 23:05:22 -0400

    Updated filters issue

--- libs/documentation/src/lib/pages/layout/filter.service.ts
+++ libs/documentation/src/lib/pages/layout/filter.service.ts
@@ -14,7 +14,7 @@ import { Subject } from 'rxjs';
 export class FilterService {
   public settings = new SDSAutocompletelConfiguration();
   public autocompleteModel = new SDSSelectedItemModel();
-  
+
   public model = {};
   public form = new FormGroup({});
   options: FormlyFormOptions = {};
@@ -189,6 +189,14 @@ export class FilterService {
             value: 'Intragovernmental Transactions',
           },
         ],
+      },
+    },
+    {
+      key: 'startDate',
+      type: 'datepicker',
+      templateOptions: {
+        hideOptional: true,
+        label: 'Date',
       },
     },
     {
