b1afdc089fb6a9844626c27f07f5b16f687a0b92
yerramshilpa <44271677+yerramshilpa@users.noreply.github.com>
2021-10-21 15:59:55 -0400

    updated demos

--- libs/packages/components/src/lib/editor/editor.component.ts
+++ libs/packages/components/src/lib/editor/editor.component.ts
@@ -19,7 +19,10 @@ import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
       [attr.id]="id"
       class="minh-15 border-gray-70 border-1px margin-top-1 padding-05"
       contenteditable="true"
-      (input)="valueChange($event.target.innerHTML)"
+      (keydown)="onKeydown($event)"
+      (keypress)="onKeyPressed()"
+      (input)="!validateOnBlur && onValueChange($event.target.innerHTML)"
+      (blur)="validateOnBlur && onBlur($event.target.innerHTML)"
     >
     </div>
   `,
@@ -30,6 +33,12 @@ import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
             multi: true,
         },
     ],
+    styles: [`
+    ::ng-deep mark {
+        background-color: red;
+        color: white;
+    }`
+    ],
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SdsEditorComponent implements ControlValueAccessor {
@@ -37,8 +46,11 @@ export class SdsEditorComponent implements ControlValueAccessor {
 
     @Input() id = 'searchEditor';
     @Input() regex = '';
+    @Input() validateOnBlur = false;
 
     model = '';
+    pos: number = undefined;
+    length: number = 0;
 
     private _onChange = (_: any) => { };
     private _onTouched = () => { };
@@ -46,17 +58,12 @@ export class SdsEditorComponent implements ControlValueAccessor {
     constructor(private cd: ChangeDetectorRef, @Inject(DOCUMENT) private _document: any) {
     }
 
-    // Helper method to programatically add a value to the existing items array
-    valueChange(value) {
+    // Helper method to programatically update a value of the model on blur
+    onBlur(value) {
         this.model = value;
         if (this.regex) {
             this.validateRegex(this.model);
         }
-        this.updateModel();
-    }
-
-    // Method that is fired when the child component event notifies us that the items array has been modified within the child component
-    updateItems($event) {
         this.updateModel();
     }
 
@@ -66,10 +73,11 @@ export class SdsEditorComponent implements ControlValueAccessor {
         this._onChange(model);
     }
 
-    // Helper method to return a new instance of an array that contains our items
+    // Helper method to return a new instance of an value
     getModel() {
         return this.model;
     }
+
     // Validate regex and highlight first charecter of the failure
     validateRegex(value) {
         const rawValue = value.replaceAll('<mark>', '').replaceAll('</mark>', '').replaceAll(/<\/?span[^>]*>/g, '');
@@ -81,15 +89,10 @@ export class SdsEditorComponent implements ControlValueAccessor {
             res = rawValue.substring(0, index) +
                 '<mark>' + rawValue.substring(index, index + 1) +
                 '</mark>' + rawValue.substring(index + 1, index + rawValue.length);
+            this.searchInput.nativeElement.innerHTML = res;
         } else {
-            res = rawValue;
+            this.searchInput.nativeElement.innerHTML = rawValue;
         }
-        this.searchInput.nativeElement.innerHTML = res;
-        // To enable to SelectAll to remove the text
-        this._document.execCommand('selectAll', false, null);
-
-        // moving the cursor to the end when the validation fails
-        this._document.getSelection().collapseToEnd();
     }
 
     // ControlValueAccessor (and Formly) is trying to update the value of the FormControl (our custom component) programatically
@@ -108,7 +111,7 @@ export class SdsEditorComponent implements ControlValueAccessor {
         }
     }
 
-    // ControlValueAccessor hook that lets us call this._onChange(var) to let the form know our variable has changed (in this case model)
+    // ControlValueAccessor hook that lets us call this._onChange(let) to let the form know our variable has changed (in this case model)
     registerOnChange(fn: any): void {
         this._onChange = fn;
     }
@@ -116,5 +119,95 @@ export class SdsEditorComponent implements ControlValueAccessor {
     // ControlValueAccessor hook (not used)
     registerOnTouched(fn: any) {
         this._onTouched = fn;
+    }
+
+    onKeydown(event) {
+        if (event.key === 'Backspace' || event.key === 'Delete') {
+            this.length = this.length - 1;
+        }
+    }
+
+    onKeyPressed() {
+        this.length = this.length + 1;
+    }
+
+    // Get the cursor current position and ma
+    getCaretCharacterOffsetWithin(element) {
+        let caretOffset = 0;
+        if (typeof window.getSelection != 'undefined') {
+            let range = window.getSelection().getRangeAt(0);
+            let preCaretRange = range.cloneRange();
+            preCaretRange.selectNodeContents(element);
+            preCaretRange.setEnd(range.endContainer, range.endOffset);
+            caretOffset = preCaretRange.toString().length;
+        } else if (
+            typeof this._document['selection'] != 'undefined' &&
+            this._document['selection'].type != 'Control'
+        ) {
+            const body: any = this._document.body as any;
+            let textRange = this._document['selection'].createRange();
+            let preCaretTextRange = body.createTextRange();
+            preCaretTextRange.moveToElementText(element);
+            preCaretTextRange.setEndPoint('EndToEnd', textRange);
+            caretOffset = preCaretTextRange.text.length;
+        }
+        return caretOffset;
+    }
+
+    onValueChange(value) {
+
+        const rawValue = value
+            .replaceAll('<mark>', '')
+            .replaceAll('</mark>', '')
+            .replaceAll(/<\/?span[^>]*>/g, '')
+            .replaceAll(/<\/?font[^>]*>/g, '');
+
+        this.pos = this.getCaretCharacterOffsetWithin(
+            this._document.getElementById(this.id)
+        );
+        const regex = new RegExp(this.regex, 'g');
+        let res = '';
+        let result = regex.exec(rawValue);
+        if (result) {
+            let index = result.index;
+            res =
+                rawValue.substring(0, index) +
+                '<mark>' +
+                rawValue.substring(index, index + 1) +
+                '</mark>' +
+                rawValue.substring(index + 1, index + rawValue.length);
+        } else {
+            res = rawValue;
+        }
+        this.searchInput.nativeElement.innerHTML = res;
+        this.model = rawValue;
+        this.updateModel();
+        let node = this._document.getElementById(this.id);
+
+        if (this.pos < this.length) {
+            let firstNodeLength = node.childNodes[0].textContent.length;
+            let childNodeIndex;
+            if (this.pos < firstNodeLength) {
+                childNodeIndex = 0;
+            } else if (this.pos > firstNodeLength) {
+                childNodeIndex = 2;
+            }
+            let startPosition =
+                childNodeIndex === 0 ? this.pos : this.pos - firstNodeLength - 1;
+            let range = this._document.createRange();
+            let sel = window.getSelection();
+            range.setStart(
+                this._document.getElementById(this.id).childNodes[childNodeIndex],
+                startPosition
+            );
+            range.collapse(true);
+            sel.removeAllRanges();
+            sel.addRange(range);
+            this._document.getElementById(this.id).focus();
+        } else {
+            this._document.execCommand('selectAll', false, null);
+            this._document.getSelection().collapseToEnd();
+        }
+
     }
 }
