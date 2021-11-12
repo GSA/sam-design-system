35db685922ddfcf841c22bd5efd7ad06a6c5a61b
Bibek-Regmi <bibek.regmi@gsa.gov>
2021-11-12 10:38:35 -0500

    Work in progress

--- libs/documentation/src/lib/components/tree-table/demos/basic/tree-table-basic.component.ts
+++ libs/documentation/src/lib/components/tree-table/demos/basic/tree-table-basic.component.ts
@@ -17,42 +17,50 @@ export class TreeTableBasicComponent {
       description: 'Statement adopted by the Continental Congress declaring independence from the British Empire.',
       title: 'Declaration of Independence',
       year: 1776,
+      id: 'row1',
       children: [
         {
           title: 'Bill of Rights',
           description: 'The first ten amendments of the U.S. Constitution guaranteeing rights and freedoms.',
-          year: 1791
+          year: 1791,
+          id: 'row2'
         },
         {
           title: 'Declaration of Sentiments',
           description: 'A document written during the Seneca Falls Convention outlining the rights that American women should be entitled to as citizens.',
           year: 1848,
+          id: 'row3',
           totalChildren: 42,
           children: [
             {
               title: 'Declaration of Sentiments',
               description: 'A document written during the Seneca Falls Convention outlining the rights that American women should be entitled to as citizens.',
-              year: 1848
+              year: 1848,
+              id: 'row4',
             },
             {
               title: 'Declaration of Sentiments',
               description: 'A document written during the Seneca Falls Convention outlining the rights that American women should be entitled to as citizens.',
-              year: 1848
+              year: 1848,
+              id: 'row5',
             },
             {
               title: 'Declaration of Sentiments',
               description: 'A document written during the Seneca Falls Convention outlining the rights that American women should be entitled to as citizens.',
-              year: 1848
+              year: 1848,
+              id: 'row6',
             },
             {
               title: 'Declaration of Sentiments',
               description: 'A document written during the Seneca Falls Convention outlining the rights that American women should be entitled to as citizens.',
-              year: 1848
+              year: 1848,
+              id: 'row7',
             },
             {
-              title: 'Declaration of Sentiments',
+              title: 'Declaration of Sentiments2',
               description: 'A document written during the Seneca Falls Convention outlining the rights that American women should be entitled to as citizens.',
-              year: 1848
+              year: 1848,
+              id: 'row8',
             },
           ]
         },
@@ -60,11 +68,46 @@ export class TreeTableBasicComponent {
           title: 'Emancipation Proclamation',
           description: 'An executive order granting freedom to slaves in designated southern states.',
           year: 1863,
+          id: 'row9',
           children: [
             {
               title: 'Declaration of Sentiments',
               description: 'A document written during the Seneca Falls Convention outlining the rights that American women should be entitled to as citizens.',
-              year: 1848
+              year: 1848,
+              id: 'row10',
+              children: [
+                {
+                  title: 'Emancipation Proclamation',
+                  description: 'An executive order granting freedom to slaves in designated southern states.',
+                  year: 1863,
+                  id: 'row9',
+                  children: [
+                    {
+                      title: 'Declaration of Sentiments',
+                      description: 'A document written during the Seneca Falls Convention outlining the rights that American women should be entitled to as citizens.',
+                      year: 1848,
+                      id: 'row10',
+                      children: [
+                        {
+                          title: 'Emancipation Proclamation',
+                          description: 'An executive order granting freedom to slaves in designated southern states.',
+                          year: 1863,
+                          id: 'row9',
+                          children: [
+                            {
+                              title: 'Declaration of Sentiments',
+                              description: 'A document written during the Seneca Falls Convention outlining the rights that American women should be entitled to as citizens.',
+                              year: 1848,
+                              id: 'row10',
+                            },
+                          ]
+                        },
+                      ]
+                    },
+                  ]
+                },
+              ]
+              
             },
           ]
         },
@@ -78,5 +121,29 @@ export class TreeTableBasicComponent {
 
   collapseAll() {
     this.treeTable.collapseAll();
+  }
+
+  expandChild() {
+    this.treeTable.expandRow('row8');
+  }
+
+  collapseRow() {
+    this.treeTable.collapseRow('row1');
+  }
+
+  onViewAll($event) {
+    const billOfRights = this.displayedData[0].children[0];
+
+    const childrenLength = $event.children.length;
+    const totalChildren = $event.totalChildren;
+    const childrenToAdd = totalChildren - childrenLength;
+
+    const additions = [];
+
+    for (let i = 0; i < childrenToAdd; i++) {
+      additions.push({...billOfRights});
+    }
+
+    $event.children = [...$event.children, ...additions];
   }
 }
--- libs/packages/components/src/lib/tree-table/tree-table.component.ts
+++ libs/packages/components/src/lib/tree-table/tree-table.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from "@angular/core";
+import { ChangeDetectorRef, Component, ElementRef, EventEmitter, Input, Output } from "@angular/core";
 
 
 @Component({
@@ -9,6 +9,16 @@ import { Component, Input } from "@angular/core";
 export class SdsTreeTableComponent {
   @Input() dataSource: any[];
   @Input() displayColumns: string[];
+
+
+  @Output() viewAll = new EventEmitter<any>();
+
+  _selectedRow: any;
+
+  constructor(
+    private elementRef: ElementRef,
+    private cdr: ChangeDetectorRef,
+  ) {}
 
 
   /**
@@ -25,6 +35,27 @@ export class SdsTreeTableComponent {
     this.toggleAllHelper(this.dataSource, true);
   }
 
+  public expandRow(rowId: string) {
+    this.expandRowHelper(this.dataSource, rowId);
+    this.cdr.detectChanges();
+
+    const tableRow: HTMLTableRowElement = this.elementRef.nativeElement.querySelector(`#${rowId}`);
+    tableRow.scrollIntoView({behavior: 'smooth', block: 'center'});
+  }
+
+  public collapseRow(rowId: string) {
+    const row = this.findRow(this.dataSource, rowId);
+    if (!row) {
+      return;
+    }
+
+    this.collapseRowHelper(row);
+  }
+
+  viewAllClicked(row: any) {
+    this.viewAll.emit(row);
+  }
+
   private toggleAllHelper(data: any[], expanded: boolean) {
     data.forEach(data => {
       if (data.children) {
@@ -32,6 +63,44 @@ export class SdsTreeTableComponent {
         data.expanded = expanded;
       }
     });
+  }
+
+  private expandRowHelper(allRows: any[], id: string) {
+    for (let i = 0; i < allRows.length; i++) {
+      const row = allRows[i];
+      if (row.id === id) {
+        if (this._selectedRow) this._selectedRow.selected = false;
+        this._selectedRow = row;
+        return true;
+      } else if (row.children) {
+        const isChildExpanded = this.expandRowHelper(row.children, id);
+        if (isChildExpanded) {
+          row.expanded = true;
+          return true;
+        }
+      }
+    }
+
+    return false;
+  }
+
+  private findRow(allRows: any[], id: string) {
+    for (let i = 0; i < allRows.length; i++) {
+      const row = allRows[i];
+      if (row.id === id) {
+        return row;
+      } else if (row.children) {
+        return this.findRow(allRows, id);
+      }
+    }
+    return null;
+  }
+
+  private collapseRowHelper(row: any) {
+    row.expanded = false;
+    if (row.children) {
+      row.children.forEach(child => this.collapseRowHelper(child));
+    }
   }
 
   getTemplateContext(parent: any, row: any, index: number, level: number) {
@@ -43,5 +112,14 @@ export class SdsTreeTableComponent {
       index: posinset,
       size: parent.children ? parent.children.length : 1,
     }
+  }
+
+  onRowClicked(row: any) {
+    row.selected = true;
+    if (row.children) {
+      row.expanded = !row.expanded;
+    }
+
+    this._selectedRow = row;
   }
 }
