7d9bcc730f87897ff142b7978dc84200ad5de781
Bibek Regmi <bibekregmi@Bibek-Regmi-MBP.fios-router.home>
2021-12-07 13:06:38 -0500

    display expansion on totalChildren value

--- libs/documentation/src/lib/components/tree-table/demos/mixed-api/mixed-api.component.ts
+++ libs/documentation/src/lib/components/tree-table/demos/mixed-api/mixed-api.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from "@angular/core";
-import { SdsTreeTableData } from "libs/packages/components/src/lib/public-api";
+import { AfterViewInit, Component, OnInit, ViewChild } from "@angular/core";
+import { SdsTreeTableComponent, SdsTreeTableData } from "libs/packages/components/src/lib/public-api";
 import tableDataFull from "../../../table/demos/full/data";
 
 
@@ -7,11 +7,12 @@ import tableDataFull from "../../../table/demos/full/data";
   selector: `tree-table-mixed-api`,
   templateUrl: './mixed-api.component.html'
 })
-export class TreeTableMixedApiComponent implements OnInit {
+export class TreeTableMixedApiComponent implements OnInit, AfterViewInit {
+  @ViewChild(SdsTreeTableComponent) treeTable: SdsTreeTableComponent;
 
   columns = ['Name', 'Description'];
 
-  // Data from backend
+  // Data from backend - Made search on uuid #45
   apiData = {
     name: 'Data',
     description: 'This is the data user searched for',
@@ -20,36 +21,43 @@ export class TreeTableMixedApiComponent implements OnInit {
         name: 'parent 1',
         description: 'This is top level parent',
         hierarchyLevel: 1,
-        totalImmediateChildren: 1
+        totalImmediateChildren: 1,
+        uuid: 38,
       },
       {
         name: 'parent 2',
         description: 'This is second level parent',
         hierarchyLevel: 2,
         totalImmediateChildren: 1,
+        uuid: 40,
       },
       {
         name: 'parent 3',
         description: 'This is third level parent',
         hierarchyLevel: 3,
-        totalImmediateChildren: 1
+        totalImmediateChildren: 1,
+        uuid: 42,
       }
     ],
+    uuid: 45,
     hierarchyLevel: 4,
     totalImmediateChildren: 3,
     children: [
       {
         name: 'Child 1',
         hierarchyLevel: 5,
+        uuid: 50,
         totalImmediateChildren: 2
       },
       {
         name: 'Child 2',
         hierarchyLevel: 5,
+        uuid: 52,
         totalImmediateChildren: 0
       },
       {
         name: 'Child 3',
+        uuid: 53,
         hierarchyLevel: 5,
         totalImmediateChildren: 0
       }
@@ -64,6 +72,37 @@ export class TreeTableMixedApiComponent implements OnInit {
     this.mapApiPropertyToTreeProperty(this.treeTableData);
   }
 
+  ngAfterViewInit() {
+    // After view has been initialized, I'd like to auto expand the tree to the uuid user searched for
+    // I know I had done api search on uuid number 45
+    this.treeTable.expandRow('45');
+  }
+
+  onViewAll($event) {
+    console.log($event);
+  }
+
+  onRowExpansion($event) {
+    console.log($event);
+    if ($event.totalChildren > 0 && !$event.children) {
+      // Mock Api call
+      setTimeout(() => {
+        // Assume this is api response after querying for expanded heirarchy
+        const responseData = {
+            parent: [], 
+            children: [
+              {name:'Grand Child 1', description: 'Loaded GrandChild', totalImmediateChildren: 2}, 
+              {name: 'GrandChild2', description: 'Loaded Grandchild', totalImmediateChildren: 0},
+          ]
+        }
+
+        this.mapApiPropertyToTreeProperty(responseData.children);
+        
+        $event.children = responseData.children;
+        this.treeTable.cdr.detectChanges();
+      })
+    }
+  }
 
   /**
    * Sets up parents to recursive json model rather than a flat array.
@@ -74,24 +113,24 @@ export class TreeTableMixedApiComponent implements OnInit {
     const parentData = data.parent[0];
     let currData = parentData;
 
-    for (let i = 0; i < data.parent.length; i++) {
-      currData.chilldren = [data[i]];
+    for (let i = 1; i < data.parent.length; i++) {
+      currData.children = [data.parent[i]];
       currData = currData.children[0];
     }
 
-    currData.push(data);
+    currData.children = [data];
 
     return [parentData]
   }
 
   /** Updates fields to match tree table data. In this case, we added totalChildren property, which is defined by totalImmediateChildren */
-  private mapApiPropertyToTreeProperty(treeTableData: SdsTreeTableData[]) {
+  private mapApiPropertyToTreeProperty(treeTableData: any[]) {
     treeTableData.forEach(item => {
-      item.totalChildren = item.totalImmediateChildren;
+      item.totalChildren = item.totalImmediateChildren; // map totalChildren as totalImmediateChildren
+      item.id = `${item.uuid}`; // Map id as uuid
       if (item.children) {
         this.mapApiPropertyToTreeProperty(item.children);
       }
     })
   }
 }
-
