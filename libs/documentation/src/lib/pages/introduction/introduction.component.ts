045e4a897da460394eb7c151d5ec944213a5e5bc
yerramshilpa <44271677+yerramshilpa@users.noreply.github.com>
2022-01-20 15:45:09 -0500

    Updated component

--- libs/documentation/src/lib/pages/introduction/introduction.component.ts
+++ libs/documentation/src/lib/pages/introduction/introduction.component.ts
@@ -9,7 +9,6 @@ export class IntroductionComponent implements OnInit {
   constructor() {
     this.packages.components = require('./../../../../../packages/components/package.json');
     this.packages.formly = require('./../../../../../packages/sam-formly/package.json');
-    // this.packages.layouts = require('./../../../../../packages/layouts/package.json');
     this.packages.material = require('./../../../../../packages/sam-material-extensions/package.json');
   }
 
