03c7792f098c49d5616c366d22e7013ebaccda31
Bibek Regmi <bibekregmi@Bibek-Regmi-MBP.fios-router.home>
2022-01-26 14:20:20 -0500

    Fix issue with sidenav demo

--- libs/documentation/src/lib/components/sidenavigation/demos/links-side-navigation/links-side-navigation.component.ts
+++ libs/documentation/src/lib/components/sidenavigation/demos/links-side-navigation/links-side-navigation.component.ts
@@ -1,17 +1,28 @@
-import { Component } from '@angular/core';
-import { SdsDialogConfig, SideNavigationModel } from '@gsa-sam/components';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { SdsDialogConfig, SideNavigationModel, SdsSideNavigationComponent } from '@gsa-sam/components';
 import { navigationConfig } from '../filter-side-navigation/navigation.config';
 
 @Component({
   selector: 'links-side-navigation',
   templateUrl: './links-side-navigation.component.html'
 })
-export class LinksSideNavigationComponent {
+export class LinksSideNavigationComponent implements AfterViewInit {
+
+  @ViewChild('sideNavigation') sideNavigation: SdsSideNavigationComponent;
 
   responsiveDialogOptions: SdsDialogConfig = {
     ariaLabel: 'Navigation Links',
   };
 
   public navigationModel: SideNavigationModel = navigationConfig;
+
+  ngAfterViewInit() {
+    // Pre-select first sidenav item
+    this.sideNavigation.select(navigationConfig.navigationLinks[0].id);
+  }
+
+  onSideNavItemClicked($event) {
+    this.sideNavigation.select($event.id);
+  }
 
 }
