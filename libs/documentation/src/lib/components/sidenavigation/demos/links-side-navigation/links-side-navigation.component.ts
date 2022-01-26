ad1a0a0f6f4845a90bb4bf2c116f671ce3036ee3
Bibek Regmi <bibekregmi@Bibek-Regmi-MBP.fios-router.home>
2022-01-26 14:41:58 -0500

    Update comments, add example for mobile scenario

--- libs/documentation/src/lib/components/sidenavigation/demos/links-side-navigation/links-side-navigation.component.ts
+++ libs/documentation/src/lib/components/sidenavigation/demos/links-side-navigation/links-side-navigation.component.ts
@@ -1,5 +1,5 @@
-import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
-import { SdsDialogConfig, SideNavigationModel, SdsSideNavigationComponent } from '@gsa-sam/components';
+import { AfterViewInit, Component, TemplateRef, ViewChild } from '@angular/core';
+import { SdsDialogConfig, SideNavigationModel, SdsSideNavigationComponent, SdsDialogRef } from '@gsa-sam/components';
 import { navigationConfig } from '../filter-side-navigation/navigation.config';
 
 @Component({
@@ -16,13 +16,39 @@ export class LinksSideNavigationComponent implements AfterViewInit {
 
   public navigationModel: SideNavigationModel = navigationConfig;
 
+  /**  
+   * Pre-select first sidenav item if sidenav is defined during initialization phase.
+   * We do AfterViewInit rather than OnInit because non-static view child references
+   * are not defined until AfterViewInit
+   * On mobile view, due to sds-side-toolbar, the sidenav does not exist until user
+   * opens the sidenav through modal. See onSidenavDialogOpen for listening for the
+   * modal to popup and pre-selecting a sidenav link
+  */
   ngAfterViewInit() {
-    // Pre-select first sidenav item
+    if (!this.sideNavigation) return;
     this.sideNavigation.select(navigationConfig.navigationLinks[0].id);
   }
 
   onSideNavItemClicked($event) {
     this.sideNavigation.select($event.id);
+  }
+
+  /**
+   * Execute event after user clicks to open sidenav dialog in mobile view
+   * On mobile view, sidenav will only be defined after user chooses to open the dialog.
+   *  We listen for that dialog open event,
+   * @param $event 
+   */
+  onSidenavDialogOpen($event: SdsDialogRef<TemplateRef<any>>) {
+    /**
+     * We need to wait for dialog to finish opening before we can reference components
+     * inside the dialog
+     * */    
+    $event.afterOpened().toPromise().then(() => {
+      // Once the sidenav is completely initialized, select the first item
+      this.sideNavigation.select(navigationConfig.navigationLinks[0].id);
+    });
+
   }
 
 }
