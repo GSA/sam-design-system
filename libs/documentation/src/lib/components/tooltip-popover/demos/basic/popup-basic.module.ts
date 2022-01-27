314e5cfd39d81270cae7b15c13cbb479a79b55a1
yerramshilpa <44271677+yerramshilpa@users.noreply.github.com>
2022-01-27 13:57:54 -0500

    Updated stack icon and module added

--- libs/documentation/src/lib/components/tooltip-popover/demos/basic/popup-basic.module.ts
+++ libs/documentation/src/lib/components/tooltip-popover/demos/basic/popup-basic.module.ts
@@ -3,19 +3,29 @@ import { CommonModule } from '@angular/common';
 import { PopupBasic } from './popup-basic.component';
 
 import { SdsPopoverModule, SdsTooltipModule } from '@gsa-sam/components';
-import { NgxBootstrapIconsModule, caretDownSquare, squareFill, handThumbsDown } from 'ngx-bootstrap-icons';
-import { IconModule } from '@gsa-sam/ngx-uswds-icons';
+import {
+  NgxBootstrapIconsModule,
+  caretDownSquare,
+  squareFill,
+  handThumbsDown,
+} from 'ngx-bootstrap-icons';
+import { IconModule, StackedIconModule } from '@gsa-sam/ngx-uswds-icons';
 
 @NgModule({
   declarations: [PopupBasic],
   imports: [
     CommonModule,
     IconModule,
+    StackedIconModule,
     SdsTooltipModule,
     SdsPopoverModule,
-    NgxBootstrapIconsModule.pick({caretDownSquare, squareFill, handThumbsDown})
+    NgxBootstrapIconsModule.pick({
+      caretDownSquare,
+      squareFill,
+      handThumbsDown,
+    }),
   ],
   exports: [PopupBasic],
-  bootstrap: [PopupBasic]
+  bootstrap: [PopupBasic],
 })
 export class PopupBasicModule {}
