ec7c67743ddf9bf22c08f5e9fe5b687b8378460f
Cameron Wolf <cameron.wolf@gsa.gov>
2022-03-08 08:25:24 -0500

    Remove icon references

--- libs/documentation/src/lib/components/formly-radio/demos/horizontal/radio-horizontal.module.ts
+++ libs/documentation/src/lib/components/formly-radio/demos/horizontal/radio-horizontal.module.ts
@@ -1,16 +1,13 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
-import { IconModule, uswdsAttachMoney, uswdsIdentification } from '@gsa-sam/ngx-uswds-icons';
 import { SdsFormlyModule } from "@gsa-sam/sam-formly";
 import { FormlyModule } from "@ngx-formly/core";
-import { NgxBootstrapIconsModule } from 'ngx-bootstrap-icons';
 import { RadioHorizontalComponent } from "./radio-horizontal.component";
-import { award } from 'ngx-bootstrap-icons'
 
 
 @NgModule({
-  imports: [CommonModule, ReactiveFormsModule, SdsFormlyModule, FormsModule, FormlyModule.forRoot(), IconModule, NgxBootstrapIconsModule.pick({uswdsAttachMoney, uswdsIdentification, award})],
+  imports: [CommonModule, ReactiveFormsModule, SdsFormlyModule, FormsModule, FormlyModule.forRoot(),],
   declarations: [RadioHorizontalComponent],
   exports: [RadioHorizontalComponent],
   bootstrap: [RadioHorizontalComponent]
