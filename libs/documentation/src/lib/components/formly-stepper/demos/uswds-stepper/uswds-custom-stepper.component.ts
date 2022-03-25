061e3add9ecf670837dab39af76df99e3c65808d
Cameron Wolf <cameron.wolf@gsa.gov>
2022-03-25 07:47:30 -0400

    Add comments and remove extra casting

--- libs/documentation/src/lib/components/formly-stepper/demos/uswds-stepper/uswds-custom-stepper.component.ts
+++ libs/documentation/src/lib/components/formly-stepper/demos/uswds-stepper/uswds-custom-stepper.component.ts
@@ -27,10 +27,13 @@ export class USWDSCustomStepperComponent extends SdsStepper implements AfterCont
   async changeStep(stepId: string, incrementor?: 1 | -1) {
     await super.changeStep(stepId, incrementor);
 
+    // Need to know what major step to highlight.
+    // Even if substep, major step needs to be highlighted and partially filled
     let mainStepIndex = (this.stepTemplates.toArray() as Array<any>).findIndex(step => step === this.selectedStep);
+    // If sub-step
     if(mainStepIndex === -1){
       let childIndex = -1;
-      (this.stepTemplates.toArray() as Array<any>).forEach((mainStep, index) => {
+      this.stepTemplates.toArray().forEach((mainStep, index) => {
         const tempIndex = mainStep.children.toArray().findIndex(child => {
           return child === this.selectedStep;
         });
@@ -41,12 +44,21 @@ export class USWDSCustomStepperComponent extends SdsStepper implements AfterCont
         }
       } );
 
+      /**
+       * With index of first child being 0, need to increment both the child's
+       * index and total length to ensure getting a valid percent.
+       *
+       * For example, with one sub-step step indicator should show 50% completion
+       * Without incrementing input values you'd get 0 / 1 = 0%
+       * With incrementing you'd get 1 / 2 = 50%
+       */
       this.stepLabels[mainStepIndex].completionPercent = Math.round(
         (
           (childIndex + 1) /
           (this.stepTemplates.get(mainStepIndex).children.length + 1)
         ) * 100
       );
+    // If main step
     } else {
       this.stepLabels[mainStepIndex].completionPercent = 0;
     }
