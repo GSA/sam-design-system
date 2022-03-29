7ea43ece7507e0b438f0a63feea71a6b6b19113e
Cameron Wolf <cameron.wolf@gsa.gov>
2022-03-29 06:23:42 -0400

    Add demo section for new features

---
+++ libs/documentation/src/lib/components/table/demos/highlight-and-click/highlight.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SdsTableModule } from '@gsa-sam/sam-material-extensions';
+
+import { IconModule } from '@gsa-sam/ngx-uswds-icons';
+import { TableHighlightComponent } from './highlight.component';
+
+
+@NgModule({
+  declarations: [TableHighlightComponent],
+  imports: [
+    CommonModule,
+    SdsTableModule,
+    IconModule
+  ],
+  exports: [TableHighlightComponent],
+  bootstrap: [TableHighlightComponent]
+})
+
+export class TableHighlightModule { }
