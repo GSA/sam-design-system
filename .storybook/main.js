60f3485ee95e5c4ed3f2fad983e5651a52652fdd
Cameron Wolf <cameron.wolf@gsa.gov>
2022-05-16 11:11:54 -0400

    Fix react errors

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,11 +2,14 @@ module.exports = {
   stories: [
     '../stories/**/*.stories.mdx',
     '../stories/**/*.stories.@(js|jsx|ts|tsx)',
+    '../libs/documentation/**/*.stories.ts',
   ],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     // '@storybook/addon-interactions',
+    '@storybook/addon-actions',
+    'storybook-addon-preview/register',
   ],
   framework: '@storybook/angular',
   core: {
