6eca629d6201917557db79028e6d2eefeb0f6a67
yerramshilpa <44271677+yerramshilpa@users.noreply.github.com>
2022-06-01 00:33:01 -0400

    git push

--- libs/documentation/src/lib/components/table/table.module.ts
+++ libs/documentation/src/lib/components/table/table.module.ts
@@ -65,15 +65,6 @@ const DEMOS = {
     markup: require('!!raw-loader!./demos/sticky/sticky.component.html'), // Template Tab Content
     path: 'libs/documentation/src/lib/components/table/demos/sticky', // Path to demo for the Github link
   },
-
-  highlight: {
-    title: 'Highlighting and click event', // Title
-    type: TableHighlightComponent, // Component
-    code: require('!!raw-loader!./demos/highlight-and-click/highlight.component'), // Source Tab Content
-    module: require('!!raw-loader!./demos/highlight-and-click/highlight.module'), // Module Tab Content
-    markup: require('!!raw-loader!./demos/highlight-and-click/highlight.component.html'), // Template Tab Content
-    path: 'libs/documentation/src/lib/components/table/demos/highlight-and-click', // Path to demo for the Github link
-  },
   full: {
     title: 'Kitchen Sink', // Title
     type: TableFullComponent, // Component
@@ -81,6 +72,14 @@ const DEMOS = {
     module: require('!!raw-loader!./demos/full/full.module'), // Module Tab Content
     markup: require('!!raw-loader!./demos/full/full.component.html'), // Template Tab Content
     path: 'libs/documentation/src/lib/components/table/demos/full', // Path to demo for the Github link
+  },
+  highlight: {
+    title: 'Highlighting and click event', // Title
+    type: TableHighlightComponent, // Component
+    code: require('!!raw-loader!./demos/highlight-and-click/highlight.component'), // Source Tab Content
+    module: require('!!raw-loader!./demos/highlight-and-click/highlight.module'), // Module Tab Content
+    markup: require('!!raw-loader!./demos/highlight-and-click/highlight.component.html'), // Template Tab Content
+    path: 'libs/documentation/src/lib/components/table/demos/highlight-and-click', // Path to demo for the Github link
   },
 };
 
