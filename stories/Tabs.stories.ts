a08f54303174dc8118f3b368f68ad131de2ba63d
Cameron Wolf <cameron.wolf@gsa.gov>
2022-08-05 09:45:13 -0400

    Add intercept demo to SB

--- stories/Tabs.stories.ts
+++ stories/Tabs.stories.ts
@@ -3,26 +3,41 @@ import { CommonModule } from '@angular/common';
 // also exported from '@storybook/angular' if you can deal with breaking changes in 6.1
 import { Story, Meta } from '@storybook/angular/types-6-0';
 
-import { TabsAutoActivateComponent } from '../libs/documentation/src/lib/components/tabs/demos/auto-activate/tabs-auto-activate.component'
-import { TabsAutoActivateModule } from '../libs/documentation/src/lib/components/tabs/demos/auto-activate/tabs-auto-activate.module'
-import { TabsBasicComponent } from '../libs/documentation/src/lib/components/tabs/demos/basic/tabs-basic.component'
-import { TabsBasicModule } from '../libs/documentation/src/lib/components/tabs/demos/basic/tabs-basic.module'
-import { TabsDisabledComponent } from '../libs/documentation/src/lib/components/tabs/demos/disabled/tabs-disabled.component'
-import { TabsDisabledModule  } from '../libs/documentation/src/lib/components/tabs/demos/disabled/tabs-disabled.module'
-import { DynamicTabsComponet } from '../libs/documentation/src/lib/components/tabs/demos/dynamic-tabs/dynamic-tabs.component'
-import { DynamicTabsModule } from '../libs/documentation/src/lib/components/tabs/demos/dynamic-tabs/dynamic-tabs.module'
-import { TabsStylingComponent } from '../libs/documentation/src/lib/components/tabs/demos/styling/tabs-styling.component'
-import { TabsStylingModule } from '../libs/documentation/src/lib/components/tabs/demos/styling/tabs-styling.module'
-import { TabsTemplateHeaderComponent } from '../libs/documentation/src/lib/components/tabs/demos/template-header/tabs-template-header.component'
-import { TabsTemplateHeaderModule } from '../libs/documentation/src/lib/components/tabs/demos/template-header/tabs-template-header.module'
+import { TabsAutoActivateComponent } from '../libs/documentation/src/lib/components/tabs/demos/auto-activate/tabs-auto-activate.component';
+import { TabsAutoActivateModule } from '../libs/documentation/src/lib/components/tabs/demos/auto-activate/tabs-auto-activate.module';
+import { TabsBasicComponent } from '../libs/documentation/src/lib/components/tabs/demos/basic/tabs-basic.component';
+import { TabsBasicModule } from '../libs/documentation/src/lib/components/tabs/demos/basic/tabs-basic.module';
+import { TabsDisabledComponent } from '../libs/documentation/src/lib/components/tabs/demos/disabled/tabs-disabled.component';
+import { TabsDisabledModule } from '../libs/documentation/src/lib/components/tabs/demos/disabled/tabs-disabled.module';
+import { DynamicTabsComponet } from '../libs/documentation/src/lib/components/tabs/demos/dynamic-tabs/dynamic-tabs.component';
+import { DynamicTabsModule } from '../libs/documentation/src/lib/components/tabs/demos/dynamic-tabs/dynamic-tabs.module';
+import { TabsStylingComponent } from '../libs/documentation/src/lib/components/tabs/demos/styling/tabs-styling.component';
+import { TabsStylingModule } from '../libs/documentation/src/lib/components/tabs/demos/styling/tabs-styling.module';
+import { TabsTemplateHeaderComponent } from '../libs/documentation/src/lib/components/tabs/demos/template-header/tabs-template-header.component';
+import { TabsTemplateHeaderModule } from '../libs/documentation/src/lib/components/tabs/demos/template-header/tabs-template-header.module';
+import { generateConfig } from 'libs/documentation/src/sandbox/sandbox-utils';
+import { TabsInterceptModule } from 'libs/documentation/src/lib/components/tabs/demos/tabs-intercept/tabs-intercept.module';
 
 export default {
   title: 'Example/Tabs',
-  component: TabsBasicComponent, TabsAutoActivateComponent, TabsDisabledComponent, DynamicTabsComponet, TabsTemplateHeaderComponent,
+  component: TabsBasicComponent,
+  TabsAutoActivateComponent,
+  TabsDisabledComponent,
+  DynamicTabsComponet,
+  TabsTemplateHeaderComponent,
   decorators: [
     moduleMetadata({
       declarations: [],
-      imports: [CommonModule, TabsAutoActivateModule, TabsBasicModule, TabsDisabledModule, DynamicTabsModule, TabsStylingModule, TabsTemplateHeaderModule],
+      imports: [
+        CommonModule,
+        TabsAutoActivateModule,
+        TabsBasicModule,
+        TabsDisabledModule,
+        DynamicTabsModule,
+        TabsStylingModule,
+        TabsTemplateHeaderModule,
+        TabsInterceptModule,
+      ],
     }),
   ],
 } as Meta;
@@ -33,55 +48,60 @@ const TabsBasicTemplate: Story<TabsBasicComponent> = (args: TabsBasicComponent) 
 });
 
 const TabsAutoActivateTemplate: Story<TabsAutoActivateComponent> = (args: TabsAutoActivateComponent) => ({
-    component: TabsAutoActivateComponent,
-    props: args,
+  component: TabsAutoActivateComponent,
+  props: args,
 });
 
 const TabsDisabledTemplate: Story<TabsDisabledComponent> = (args: TabsDisabledComponent) => ({
-    component: TabsDisabledComponent,
-    props: args,
+  component: TabsDisabledComponent,
+  props: args,
 });
 
 const TabsDynamicTemplate: Story<DynamicTabsComponet> = (args: DynamicTabsComponet) => ({
-    component: DynamicTabsComponet,
-    props: args,
+  component: DynamicTabsComponet,
+  props: args,
 });
 
 const TabsStylingTemplate: Story<TabsStylingComponent> = (args: TabsStylingComponent) => ({
-    component: TabsStylingComponent,
-    props: args,
+  component: TabsStylingComponent,
+  props: args,
 });
 
 const TabsTemplateHeaderTemplate: Story<TabsTemplateHeaderComponent> = (args: TabsTemplateHeaderComponent) => ({
-    component: TabsTemplateHeaderComponent,
-    props: args,
+  component: TabsTemplateHeaderComponent,
+  props: args,
 });
-
-
 
 // Template
 
 export const Basic = TabsBasicTemplate.bind({});
-Basic.args = {
-};
+Basic.args = {};
 
 export const AutomaticActivate = TabsAutoActivateTemplate.bind({});
-AutomaticActivate.args = {
-};
+AutomaticActivate.args = {};
 
 export const Disabled = TabsDisabledTemplate.bind({});
-Disabled.args = {
-};
+Disabled.args = {};
 
 export const DynamicallyGenerate = TabsDynamicTemplate.bind({});
-DynamicallyGenerate.args = {
-};
+DynamicallyGenerate.args = {};
 
 export const CustomStyling = TabsStylingTemplate.bind({});
-CustomStyling.args = {
-};
+CustomStyling.args = {};
 
 export const CustomHeader = TabsTemplateHeaderTemplate.bind({});
-CustomHeader.args = {
-};
+CustomHeader.args = {};
 
+export const InterceptTabChange: Story = (args) => ({
+  template: `<sds-tabs-intercept-demo></sds-tabs-intercept-demo>`,
+  props: {
+    ...args,
+  },
+});
+InterceptTabChange.parameters = {
+  controls: {
+    disabled: true,
+    hideNoControlsWarning: true,
+  },
+  actions: { disabled: true },
+  preview: generateConfig('components/tabs/demos/tabs-intercept', 'TabsInterceptModule', 'sds-tabs-intercept-demo')
