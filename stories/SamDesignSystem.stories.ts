448e1488be03e675ca085e2676fca67c6a402c4b
julieduong46 <julieduong.work@gmail.com>
2022-09-09 10:43:03 -0400

    add sds story

---
+++ stories/SamDesignSystem.stories.ts
@@ -0,0 +1,15 @@
+// also exported from '@storybook/angular' if you can deal with breaking changes in 6.1
+import { Story, Meta } from '@storybook/angular/types-6-0';
+
+export default {
+  title: 'Resource/SamDesignSystem',
+} as Meta;
+
+const Template: Story<any> = (args: any) => ({
+    template: ``,
+    props: args,
+  });
+
+
+
+
