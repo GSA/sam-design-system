234ae14cb9d52ef0e62695b83d020d48ce4a7558
Cameron Wolf <cameron.wolf@gsa.gov>
2023-01-13 11:06:28 -0500

    Revert "Merge branch 'master' into storybook"
    This reverts commit f5a9d100c9514c068c80fc7a2374632dd42ab637, reversing
    changes made to ec98e3fce3eb787fb8e3f56523694dbb088f8ade.

---
+++ libs/documentation/src/lib/storybook/external-link/external-link-hide-icon/external-link-hide-icon.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: './external-link-hide-icon.component.html',
+  selector: `sds-external-link-hide-icon`,
+})
+export class ExternalLinkHideIconComponent {}
