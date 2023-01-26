88475415a0bddfafbb25d5668e5c8a732e38800b
Cameron Wolf <cameron.wolf@gsa.gov>
2023-01-26 08:04:42 -0500

    Add collapse demos to SB

---
+++ libs/documentation/src/lib/storybook/collapse/collapse-basic/collapse-basic.component.ts
@@ -0,0 +1,9 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'sds-collapse-basic',
+  templateUrl: './collapse-basic.component.html',
+})
+export class CollapseBasicComponent {
+  public isCollapsedContent = true;
+}
---
+++ libs/documentation/src/lib/storybook/collapse/collapse-introduction/collapse-introduction.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sds-collapse-introduction',
+  templateUrl: './collapse-introduction.component.html',
+})
+export class CollapseIntroductionComponent {
+  constructor() {}
+}
