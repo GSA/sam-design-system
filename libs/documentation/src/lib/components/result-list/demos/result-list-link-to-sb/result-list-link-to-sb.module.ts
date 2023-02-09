40df276ba87e8a09f775123bf3b62e013e61959e
Cameron Wolf <cameron.wolf@gsa.gov>
2023-02-09 16:59:59 -0500

    Add link from SDS docs to SB

---
+++ libs/documentation/src/lib/components/result-list/demos/result-list-link-to-sb/result-list-link-to-sb.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ResultListLinkToSbComponent } from './result-list-link-to-sb.component';
+
+@NgModule({
+  imports: [CommonModule],
+  declarations: [ResultListLinkToSbComponent],
+  exports: [ResultListLinkToSbComponent],
+  bootstrap: [ResultListLinkToSbComponent],
+})
+export class ResultListLinkToSbModule {}
