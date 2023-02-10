5f2b4067b62dd7476b50356a588aa1803febb152
Cameron Wolf <cameron.wolf@gsa.gov>
2023-02-10 12:04:38 -0500

    Update messages to preserve the states regularly used

--- libs/documentation/src/lib/storybook/result-list/result-list-messages/result-list-messages.module.ts
+++ libs/documentation/src/lib/storybook/result-list/result-list-messages/result-list-messages.module.ts
@@ -2,9 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ResultListMessagesComponent } from './result-list-messages.component';
 import { SdsSearchResultListModule, SdsPageModule } from '@gsa-sam/components';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { SdsFormlyModule } from '@gsa-sam/sam-formly';
+import { FormlyModule } from '@ngx-formly/core';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  imports: [CommonModule, SdsSearchResultListModule, SdsPageModule],
+  imports: [CommonModule, SdsSearchResultListModule, SdsPageModule, ReactiveFormsModule, SdsFormlyModule, FormsModule, FormlyModule.forRoot(), NoopAnimationsModule,],
   declarations: [ResultListMessagesComponent],
   bootstrap: [ResultListMessagesComponent],
   exports: [ResultListMessagesComponent],
