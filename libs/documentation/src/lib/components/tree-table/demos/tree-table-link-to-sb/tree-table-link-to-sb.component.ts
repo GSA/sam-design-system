6ce070e2ce75d0007f8af906754fefeb84b596d2
Cameron Wolf <cameron.wolf@gsa.gov>
2023-02-23 08:09:09 -0500

    Add link from SDS to SB

---
+++ libs/documentation/src/lib/components/tree-table/demos/tree-table-link-to-sb/tree-table-link-to-sb.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sds-tree-table-link-to-sb',
+  templateUrl: './tree-table-link-to-sb.component.html',
+})
+export class TreeTableLinkToSbComponent {
+
+  constructor() { }
+
+}
