476e32ced6e6f3b8e3bfce14da6bff508e3399de
Cameron Wolf <cameron.wolf@gsa.gov>
2023-04-07 08:52:38 -0400

    Add link from old demos to SB

---
+++ libs/documentation/src/lib/components/selection-panel/demos/selection-panel-link-to-sb/selection-panel-link-to-sb.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sds-selection-panel-link-to-sb',
+  templateUrl: './selection-panel-link-to-sb.component.html',
+})
+export class SelectionPanelLinkToSbComponent {
+
+  constructor() { }
+
+}
---
+++ libs/documentation/src/lib/components/selection-panel/demos/selection-panel-link-to-sb/selection-panel-link-to-sb.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SelectionPanelLinkToSbComponent } from './selection-panel-link-to-sb.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [SelectionPanelLinkToSbComponent],
+  exports: [SelectionPanelLinkToSbComponent],
+  bootstrap: [SelectionPanelLinkToSbComponent],
+})
+export class SelectionPanelLinkToSbModule { }
