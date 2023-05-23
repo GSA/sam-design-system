739819b6f9872e07bdb6831a6960fb909c2efc1b
Shayan Roshan <shayan.roshan@gsa.gov>
2023-05-22 19:33:29 -0500

    sb formly editor
    sb formly editor component

---
+++ libs/documentation/src/lib/storybook/formly/formly-editor/formly-editor-basic/formly-editor-basic.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { FormlyFormOptions, FormlyFieldConfig } from '@ngx-formly/core';
+
+@Component({
+  selector: 'sds-formly-editor-basic',
+  templateUrl: './formly-editor-basic.component.html',
+})
+export class FormlyEditorBasicComponent {
+    textModel = 'test';
+    form = new FormGroup({});
+    model: any = {};
+    options: FormlyFormOptions = {};
+    fields: FormlyFieldConfig[] = [
+      {
+        key: 'searchkey',
+        type: 'editor',
+        templateOptions: {
+          label: 'Search',
+          placeholder: 'eg: type here',
+          regex: /hello/gm,
+        },
+      },
+    ];
+}
---
+++ libs/documentation/src/lib/storybook/formly/formly-editor/formly-editor-regex/formly-editor-regex.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { FormlyFormOptions, FormlyFieldConfig } from '@ngx-formly/core';
+
+@Component({
+  selector: 'sds-formly-editor-regex',
+  templateUrl: './formly-editor-regex.component.html',
+})
+export class FormlyEditorRegexComponent {
+    textModel = 'test';
+    form = new FormGroup({});
+    model: any = {};
+    options: FormlyFormOptions = {};
+    fields: FormlyFieldConfig[] = [
+      {
+        key: 'searchkey',
+        type: 'editor',
+        templateOptions: {
+          regex: /hello/gm,
+        },
+      },
+    ];
+}
