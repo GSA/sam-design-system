c8ce80305c4714dbbaa317cb5577940734efecec
yerramshilpa <44271677+yerramshilpa@users.noreply.github.com>
2023-05-23 00:07:14 -0400

    Date picker changes

---
+++ libs/documentation/src/lib/storybook/formly/formly-datepicker/formly-datepicker-introduction/formly-datepicker-introduction.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sds-formly-datepicker-introduction',
+  templateUrl: './formly-datepicker-introduction.component.html',
+})
+export class FormlyDatepickerIntroductionComponent {}
---
+++ libs/documentation/src/lib/storybook/formly/formly-datepicker/formly-datepicker-range/formly-datepicker-range.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { FormlyFormOptions, FormlyFieldConfig } from '@ngx-formly/core';
+
+@Component({
+  selector: 'sds-formly-datepicker-range',
+  templateUrl: './formly-datepicker-range.component.html',
+})
+export class FormlyDatepickerRangeComponent {
+   form = new FormGroup({});
+  model: any = {};
+  options: FormlyFormOptions = {};
+
+  fields: FormlyFieldConfig[] = [
+    {
+      key: 'expirationDateRangeEx',
+      type: 'daterangepickerv2',
+      templateOptions: {
+        label: 'Expiration Date Range',
+        minDate: new Date(2019, 9, 5),
+        maxDate: new Date(2020, 11, 15),
+      },
+    },
+  ];
+}
