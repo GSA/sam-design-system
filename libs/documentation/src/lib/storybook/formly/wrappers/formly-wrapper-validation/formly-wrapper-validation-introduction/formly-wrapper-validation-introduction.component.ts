27309ab7309507658ef7abdb61d18231b1ad3d77
yerramshilpa <44271677+yerramshilpa@users.noreply.github.com>
2023-05-24 00:22:39 -0400

    validation wrapper

---
+++ libs/documentation/src/lib/storybook/formly/wrappers/formly-wrapper-validation/formly-wrapper-validation-introduction/formly-wrapper-validation-introduction.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sds-formly-wrapper-validation-introduction',
+  templateUrl: './formly-wrapper-validation-introduction.component.html',
+})
+export class FormlyWrapperValidationIntroductionComponent {}
