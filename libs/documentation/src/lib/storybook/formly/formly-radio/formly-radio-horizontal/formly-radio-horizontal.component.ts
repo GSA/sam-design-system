f77b1987793f2d58f94c123ca7b94ba8ae4c1e06
Shayan Roshan <shayan.roshan@gsa.gov>
2023-05-24 20:59:04 -0500

    sb formly radio
    sb formly radio demo

---
+++ libs/documentation/src/lib/storybook/formly/formly-radio/formly-radio-horizontal/formly-radio-horizontal.component.ts
@@ -0,0 +1,50 @@
+import { Component } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { FormlyFieldConfig, FormlyFormOptions } from '@ngx-formly/core';
+
+@Component({
+  selector: 'sds-formly-radio-horizontal',
+  templateUrl: './formly-radio-horizontal.component.html',
+})
+export class FormlyRadioHorizontalComponent {
+  form = new FormGroup({});
+  model: any = {};
+  options: FormlyFormOptions = {};
+  fields: FormlyFieldConfig[] = [
+    {
+      key: 'entity.taxFilingStatus',
+      type: 'radio',
+
+      templateOptions: {
+        label: 'Are you registering an entity?',
+        description: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem',
+        required: true,
+        options: [
+          {
+            key: 'yes',
+            value: 'Yes',
+          },
+          {
+            key: 'no',
+            value: 'No',
+          },
+        ],
+        horizontal: true,
+      },
+      modelOptions: {
+        updateOn: 'blur',
+      },
+      lifecycle: {
+        onChanges: function (form, field) {
+          field.formControl.valueChanges.subscribe((v) => {
+            console.log(form['controls']['entity']);
+          });
+        },
+      },
+    },
+  ];
+
+  onModelChange($event) {
+    console.log($event);
+  }
+}
---
+++ libs/documentation/src/lib/storybook/formly/formly-radio/formly-radio-introduction/formly-radio-introduction.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sds-formly-radio-introduction',
+  templateUrl: './formly-radio-introduction.component.html',
+})
+export class FormlyRadioIntroductionComponent {
+  constructor() {}
+}
