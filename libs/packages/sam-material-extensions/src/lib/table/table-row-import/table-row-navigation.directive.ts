6d02fb6908908516e9d85bb3c74422e602e6836e
Cameron Wolf <cameron.wolf@gsa.gov>
2023-08-03 10:23:42 -0400

    Add warnings if not highlighting or using old input

--- libs/packages/sam-material-extensions/src/lib/table/table-row-import/table-row-navigation.directive.ts
+++ libs/packages/sam-material-extensions/src/lib/table/table-row-import/table-row-navigation.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, HostBinding, HostListener, Input, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, EventEmitter, HostListener, Input, Output, Renderer2 } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Directive({
@@ -8,19 +8,40 @@ export class TableRowNavigationDirective {
   private readonly hoverClass = 'sds-table__row--hovered';
 
   @Input()
+  emitOnRowClick = false;
+
+  @Input()
   highlightOnHover = false;
+
+  @Output('rowClicked')
+  rowClicked = new EventEmitter<null>();
 
   constructor(private el: ElementRef, private renderer: Renderer2, private router: Router) {}
 
-  @HostListener('mouseenter') onMouseEnter() {
+  ngAfterViewInit(): void {
     if (this.highlightOnHover) {
+      console.warn('Row Input highlightOnHover is depricated. Please update input to emitOnRowClick.');
+    }
+  }
+
+  @HostListener('mouseenter') onMouseEnter() {
+    if (this.highlightOnHover || this.emitOnRowClick) {
       this.addHoverClass();
     }
   }
 
   @HostListener('mouseleave') onMouseLeave() {
-    if (this.highlightOnHover) {
+    if (this.highlightOnHover || this.emitOnRowClick) {
       this.removeHoverClass();
+    }
+  }
+  @HostListener('click')
+  handleRowClicked() {
+    if (!this.emitOnRowClick && !this.highlightOnHover) {
+      console.warn(
+        `Row click event is being emitted without highlighting enable. This feature is depricated and will be removed in the future. To continue receiving rowClicked events please set emitOnRowClick=true on sds-row.`
+      );
+      this.rowClicked.emit();
     }
   }
 
