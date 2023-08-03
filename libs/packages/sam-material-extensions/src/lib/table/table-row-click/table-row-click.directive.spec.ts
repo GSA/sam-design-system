113d30c5576953bb700dda3257b49963564ff9d7
Cameron Wolf <cameron.wolf@gsa.gov>
2023-08-03 10:48:08 -0400

    Fix test error

--- libs/packages/sam-material-extensions/src/lib/table/table-row-click/table-row-click.directive.spec.ts
+++ libs/packages/sam-material-extensions/src/lib/table/table-row-click/table-row-click.directive.spec.ts
@@ -1,7 +1,7 @@
 import { Component, CUSTOM_ELEMENTS_SCHEMA, DebugElement, ElementRef, Renderer2 } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
-import { TableRowNavigationDirective } from './table-row-click.directive';
+import { TableRowClickDirective } from './table-row-click.directive';
 import { RouterTestingModule } from '@angular/router/testing';
 
 @Component({
@@ -9,14 +9,14 @@ import { RouterTestingModule } from '@angular/router/testing';
 })
 class TestHoverFocusComponent {}
 
-describe('TableRowNavigationDirective', () => {
+describe('TableRowClickDirective', () => {
   let component: TestHoverFocusComponent;
   let fixture: ComponentFixture<TestHoverFocusComponent>;
   let trEl: DebugElement;
 
   beforeEach(() => {
     fixture = TestBed.configureTestingModule({
-      declarations: [TestHoverFocusComponent, TableRowNavigationDirective],
+      declarations: [TestHoverFocusComponent, TableRowClickDirective],
       imports: [RouterTestingModule],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
     }).createComponent(TestHoverFocusComponent);
