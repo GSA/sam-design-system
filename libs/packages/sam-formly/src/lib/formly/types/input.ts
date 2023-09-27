894c915c3cfd26fae74b1608a19d6e57ca9cd52a
yerramshilpa <44271677+yerramshilpa@users.noreply.github.com>
2023-09-27 14:08:21 -0400

    updated input to have a clear icon

--- libs/packages/sam-formly/src/lib/formly/types/input.ts
+++ libs/packages/sam-formly/src/lib/formly/types/input.ts
@@ -3,19 +3,22 @@ import { FieldType } from '@ngx-formly/core';
 
 @Component({
   selector: 'sds-formly-field-input',
-  template: `
-    <input
-      [ngClass]="{
-        'usa-input--disabled': to.disabled,
-        'usa-input--error': showError
-      }"
-      class="usa-input"
-      [placeholder]="to.placeholder"
-      name="input-success"
-      [formlyAttributes]="field"
-      [type]="to.inputType ? to.inputType : 'text'"
-      [formControl]="formControl"
-    />
-  `,
+ template: `
+<div class="maxw-mobile-lg position-relative">
+    <input [ngClass]="{
+          'usa-input--disabled': to.disabled,
+          'usa-input--error': showError
+        }" class="usa-input padding-right-3" [placeholder]="to.placeholder" name="input-success"
+      [formlyAttributes]="field" [type]="to.inputType ? to.inputType : 'text'" [formControl]="formControl" />
+  <ng-container *ngIf="field.formControl.value">
+    <span class="position-absolute right-105 top-1 cursor-pointer bg-white">
+      <span role="button" aria-label="Clear input" (click)="field.formControl.reset()" (keyup.enter)="field.formControl.reset()" tabindex="0"
+        class="icon-container">
+        <usa-icon [icon]="'x'" size="lg" class="font-sans-xs"></usa-icon>
+      </span>
+
+    </span>
+  </ng-container>
+</div>`
 })
 export class FormlyFieldInputComponent extends FieldType {}
