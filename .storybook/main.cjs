b74bce5987c15ef05567014ac9abe3e1ceff2da4
Cameron Wolf <cameron.wolf@gsa.gov>
2023-10-17 12:59:34 -0400

    storybook v7 migrations

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -2,15 +2,22 @@ module.exports = {
   stories: [
     '../libs/documentation/**/*.stories.ts',
   ],
+
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-actions',
-    'storybook-addon-preview/register',
+    // 'storybook-addon-preview/register',
   ],
+
   staticDirs: ['../node_modules/accessible-html5-video-player/js'],
-  framework: '@storybook/angular',
-  core: {
-    builder: 'webpack5',
+
+  framework: {
+    name: '@storybook/angular',
+    options: {}
+  },
+
+  docs: {
+    autodocs: true
   }
 };
