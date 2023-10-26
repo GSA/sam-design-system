4fa85953d9d2e8256dc78e5de2503a13115ddeb0
Cameron Wolf <cameron.wolf@gsa.gov>
2023-10-26 07:29:44 -0400

    Remove autodocs and add static dirs to serve images, fonts, and stackblitz files

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -7,10 +7,15 @@ module.exports = {
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-actions',
-    // 'storybook-addon-preview/register',
   ],
 
-  staticDirs: ['../node_modules/accessible-html5-video-player/js'],
+  staticDirs: [
+    '../node_modules/accessible-html5-video-player/js',
+    { from: '../apps/sam-design-system-site/src/assets/', to: 'assets' },
+    { from: '../node_modules/@uswds/uswds/dist/fonts/', to: '/assets/uswds/fonts/'},
+    { from: '../node_modules/@uswds/uswds/dist/img/', to: '/assets/uswds/img/'},
+    { from: '../node_modules/@gsa-sam/sam-styles/sam-styles/packages/images/', to: '/assets/uswds/img/'},
+  ],
 
   framework: {
     name: '@storybook/angular',
@@ -18,6 +23,6 @@ module.exports = {
   },
 
   docs: {
-    autodocs: true
+    autodocs: false
   }
 };
