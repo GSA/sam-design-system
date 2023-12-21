8049c45802ef1f5068ad0f3e53f9b0b8a003ccaa
Shayan Roshan <shayan.roshan@gsa.gov>
2023-12-21 11:35:54 -0600

    add styles

--- .storybook/source-code-addon/manager.js
+++ .storybook/source-code-addon/manager.js
@@ -4,6 +4,12 @@ import { useParameter, addons, types } from '@storybook/api';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { themes } from '@storybook/theming';
 import { oneLight } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
+import './source-code.css';
+
+const customOneLight = { ...oneLight };
+customOneLight['pre[class*="language-"]'].background = 'transparent';
+customOneLight['code[class*="language-"]'].background = 'transparent';
 
 addons.register('my/panel', () => {
   addons.add('my-panel-addon/panel', {
@@ -22,19 +28,24 @@ addons.register('my/panel', () => {
           {preview && preview.length > 0 ? (
             <div>
               {preview.map((item, index) => (
-                <button key={index} onClick={() => handleTabClick(item.tab)}>
+                <button
+                  key={index}
+                  onClick={() => handleTabClick(item.tab)}
+                  className={item.tab === selectedTab ? "activeTabButton" : "tabButton"}
+                >
                   {item.tab}
                 </button>
               ))}
               {preview.map((item, index) => {
                 if (item.tab === selectedTab && item.template) {
                   return (
-                    <div key={index}>
-                      <div>
-                        <SyntaxHighlighter language="typescript" style={oneLight}>
-                          {item.template.default}
-                        </SyntaxHighlighter>
-                      </div>
+                    <div key={index} className="codeContainer">
+                      <CopyToClipboard text={item.template.default}>
+                        <button className="tabButton copyButton">Copy Code</button>
+                      </CopyToClipboard>
+                      <SyntaxHighlighter language="typescript" style={customOneLight}>
+                        {item.template.default}
+                      </SyntaxHighlighter>
                     </div>
                   );
                 }
@@ -53,4 +64,3 @@ addons.register('my/panel', () => {
 addons.setConfig({
   theme: themes.light,
 });
-
