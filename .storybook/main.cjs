eb54147b82f482fb9e763f41f4eea4ec43fac096
Shayan Roshan <shayan.roshan@gsa.gov>
2024-01-03 11:17:48 -0600

    initial

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -11,7 +11,7 @@ module.exports = {
 
   staticDirs: [
     '../node_modules/accessible-html5-video-player/js',
-    { from: '../apps/sam-design-system-site/src/assets/', to: 'assets' },
+    { from: '../apps/sam-design-system-site/src/assets/', to: '/assets' },
     { from: '../node_modules/@uswds/uswds/dist/fonts/', to: '/assets/uswds/fonts/'},
     { from: '../node_modules/@uswds/uswds/dist/img/', to: '/assets/uswds/img/'},
     { from: '../node_modules/@gsa-sam/sam-styles/sam-styles/packages/images/', to: '/assets/uswds/img/'},
