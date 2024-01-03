9ab64a2bb68f463ea01d9d7613159474003d8404
cwolf10 <72805180+cwolf10@users.noreply.github.com>
2024-01-03 14:06:54 -0500

    revert Update main.cjs

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -11,10 +11,10 @@ module.exports = {
 
   staticDirs: [
     '../node_modules/accessible-html5-video-player/js',
-    { from: '../apps/sam-design-system-site/src/assets/', to: './assets' },
-    { from: '../node_modules/@uswds/uswds/dist/fonts/', to: './assets/uswds/fonts/'},
-    { from: '../node_modules/@uswds/uswds/dist/img/', to: './assets/uswds/img/'},
-    { from: '../node_modules/@gsa-sam/sam-styles/sam-styles/packages/images/', to: './assets/uswds/img/'},
+    { from: '../apps/sam-design-system-site/src/assets/', to: '/assets' },
+    { from: '../node_modules/@uswds/uswds/dist/fonts/', to: '/assets/uswds/fonts/'},
+    { from: '../node_modules/@uswds/uswds/dist/img/', to: '/assets/uswds/img/'},
+    { from: '../node_modules/@gsa-sam/sam-styles/sam-styles/packages/images/', to: '/assets/uswds/img/'},
   ],
 
   framework: {
