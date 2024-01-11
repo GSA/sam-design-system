b4265f7d017be2a8877b32a25ebce4c98a11ef83
yerramshilpa <44271677+yerramshilpa@users.noreply.github.com>
2024-01-11 15:14:35 -0500

    updated changes for Icon

--- libs/documentation/src/lib/storybook/formly/formly-input/formly-input-affix/formly-input-affix.module.ts
+++ libs/documentation/src/lib/storybook/formly/formly-input/formly-input-affix/formly-input-affix.module.ts
@@ -7,11 +7,16 @@ import { FormlyInputAffixComponent } from './formly-input-affix.component';
 import { arrowRepeat, NgxBootstrapIconsModule } from 'ngx-bootstrap-icons';
 
 @NgModule({
-  imports: [CommonModule, ReactiveFormsModule, SdsFormlyModule, FormsModule, FormlyModule.forRoot(),
- NgxBootstrapIconsModule.pick({
-      arrowRepeat
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    SdsFormlyModule,
+    FormsModule,
+    FormlyModule.forRoot(),
+    NgxBootstrapIconsModule.pick({
+      arrowRepeat,
     }),
-],
+  ],
   declarations: [FormlyInputAffixComponent],
   exports: [FormlyInputAffixComponent],
   bootstrap: [FormlyInputAffixComponent],
