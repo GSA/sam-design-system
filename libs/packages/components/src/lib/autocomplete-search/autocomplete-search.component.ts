477d6df4ec46e70d36cfe97351cac81a110a6e14
yerramshilpa <44271677+yerramshilpa@users.noreply.github.com>
2024-03-11 12:28:01 -0400

    lint fixes

--- libs/packages/components/src/lib/autocomplete-search/autocomplete-search.component.ts
+++ libs/packages/components/src/lib/autocomplete-search/autocomplete-search.component.ts
@@ -220,19 +220,14 @@ export class SDSAutocompleteSearchComponent implements ControlValueAccessor {
           SDSSelectedItemModelHelper.clearItems(this.model.items);
           this.propogateChange(this.model);
         }
-        if(this.inputValue === ''){
-
-        } else if(this.model.items.length === 0 && this.inputValue !== ''){
+        if (this.inputValue === '') {
+        } else if (this.model.items.length === 0 && this.inputValue !== '') {
           const customItem = this.createFreeTextItem();
           this.selectItem(customItem);
-        }else if (
-          this.model.items.length > 0 &&
-          this.model.items[0]['name'] !== this.inputValue
-        ){
+        } else if (this.model.items.length > 0 && this.model.items[0]['name'] !== this.inputValue) {
           const customItem = this.createFreeTextItem();
           this.selectItem(customItem);
         }
-
       } else if (this.model.items.length > 0) {
         this.inputValue = this.getObjectValue(this.model.items[0], this.configuration.primaryTextField);
       } else {
