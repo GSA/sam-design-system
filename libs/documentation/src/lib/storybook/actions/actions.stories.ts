4e614c1f8a4e9c201a54307a6fe759914eb4c0e7
Cameron Wolf <cameron.wolf@gsa.gov>
2024-06-12 08:23:07 -0400

    Fix lint

--- libs/documentation/src/lib/storybook/actions/actions.stories.ts
+++ libs/documentation/src/lib/storybook/actions/actions.stories.ts
@@ -24,7 +24,7 @@ export default {
         ActionsSizeModule,
         ActionsLabelModule,
         ActionsModelTriggerModule,
-        ActionsIntroductionModule
+        ActionsIntroductionModule,
       ],
     }),
     applicationConfig({
