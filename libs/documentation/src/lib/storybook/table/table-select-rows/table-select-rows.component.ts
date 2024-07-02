1246160c753496e52d9455be96e36a898c8b637d
Cameron Wolf <cameron.wolf@gsa.gov>
2024-07-02 11:07:34 -0400

    Push current changes

---
+++ libs/documentation/src/lib/storybook/table/table-select-rows/table-select-rows.component.ts
@@ -0,0 +1,97 @@
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { PageEvent } from '@angular/material/paginator';
+import { SdsTableComponent } from '@gsa-sam/sam-material-extensions';
+import tableDataFull from '../table-pagination/data';
+import { Options } from '@popperjs/core';
+import { filter } from 'rxjs';
+
+@Component({
+  selector: 'sds-table-select-rows',
+  templateUrl: './table-select-rows.component.html',
+  styleUrls: ['./table-select-rows.component.scss']
+})
+export class TableSelectRowsComponent implements AfterViewInit {
+
+  @ViewChild(SdsTableComponent) table: SdsTableComponent;
+
+  data = tableDataFull.map((data => {
+    data['status'] = this.getRandomStatus()
+    return data;
+  }));
+  displayedColumns: string[] = ['selectBox', 'status', 'id', 'firstName', 'lastName', 'email', 'requests', 'date'];
+
+  numberOfRows = 25;
+  currentPageIndex = 0;
+  selectedRows = [];
+  selectAllChecked = false;
+
+  popperOptions = (options: Partial<Options>) => {
+    options.modifiers!.push({
+      name: 'offset',
+      options: {
+        offset: [-8, 10],
+      },
+    });
+
+    return options;
+  };
+
+  get deleteButtonEnabled(): boolean{
+    return this.selectedRows.length === 0;
+  }
+
+  getRandomStatus(): string{
+    const possibleStatuses = ['Draft', 'Reopened', 'Published'];
+    const statusIndex = Math.floor(Math.random() * 3);
+    return possibleStatuses[statusIndex];
+  }
+
+  ngAfterViewInit(): void {
+    this.table.matPaginator.page.subscribe((pageEvent: PageEvent) => {
+      this.numberOfRows = pageEvent.pageSize;
+      this.currentPageIndex = pageEvent.pageIndex;
+    })
+  }
+
+  onSingleRowSelect(element: any){
+    const elementIndex = this.selectedRows.indexOf(element)
+    if(elementIndex !== -1){
+      this.selectedRows.splice(elementIndex,1);
+    }else {
+      this.selectedRows.push(element)
+    }
+
+    console.log('this.selectedRows', this.selectedRows)
+  }
+
+  onSelectAllRows(){
+    if(!this.selectAllChecked){
+      const startIndex = this.currentPageIndex * this.numberOfRows;
+      const endIndex = startIndex + this.numberOfRows;
+      const dataToSelect = this.data.slice(startIndex, endIndex);
+      this.selectedRows = dataToSelect;
+      this.selectedRows.forEach(row => row.checked = true);
+      this.selectAllChecked = true;
+    } else {
+      this.selectedRows.forEach(row => row.checked = false);
+      this.selectedRows = [];
+      this.selectAllChecked = false;
+    }
+    console.log('this.selectedRows', this.selectedRows)
+  }
+
+  onSelectAllStatus(statusToSelect: string){
+    const startIndex = this.currentPageIndex * this.numberOfRows;
+    const endIndex = startIndex + this.numberOfRows;
+    const dataToSelect = this.data.slice(startIndex, endIndex);
+    const filteredData = dataToSelect.filter(data => data['status']===statusToSelect)
+    this.selectedRows = filteredData;
+    this.selectedRows.forEach(row => row.checked = true);
+    this.selectAllChecked = true;
+  }
+
+  deleteFunction(){
+    console.log('Do the deleting action with this data', this.selectedRows)
+  }
+
+}
---
+++ libs/documentation/src/lib/storybook/table/table-select-rows/table-select-rows.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SdsTableModule } from '@gsa-sam/sam-material-extensions';
+import { TableSelectRowsComponent } from './table-select-rows.component';
+import { FormsModule } from '@angular/forms';
+import { IconModule } from '@gsa-sam/ngx-uswds-icons';
+import { NgxBootstrapIconsModule, caretDownFill } from 'ngx-bootstrap-icons';
+import { SdsPopoverModule } from '@gsa-sam/components';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    SdsTableModule,
+    FormsModule,
+    IconModule,
+    NgxBootstrapIconsModule.pick({caretDownFill}),
+    SdsPopoverModule
+  ],
+  declarations: [TableSelectRowsComponent],
+  exports: [TableSelectRowsComponent],
+  bootstrap: [TableSelectRowsComponent],
+})
+export class TableSelectRowsModule {}
