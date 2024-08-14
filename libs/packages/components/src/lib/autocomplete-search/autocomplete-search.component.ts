de1c8225ead85c76adcb8fbc0b704e3a19161ede
yerramshilpa <44271677+yerramshilpa@users.noreply.github.com>
2024-08-14 00:17:55 -0400

    autocomple tag mode text clear

--- libs/packages/components/src/lib/autocomplete-search/autocomplete-search.component.ts
+++ libs/packages/components/src/lib/autocomplete-search/autocomplete-search.component.ts
@@ -369,7 +369,9 @@ export class SDSAutocompleteSearchComponent implements ControlValueAccessor {
       this.setHighlightedItem(flat[this.highlightedIndex]);
     } else {
       this.showResults = false;
+      this.inputValue = '';
       this.input.nativeElement.focus();
+      
     }
   }
 
