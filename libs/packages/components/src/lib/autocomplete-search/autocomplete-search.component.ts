33f44591399bf38f00e30883239ff693675e7435
yerramshilpa <44271677+yerramshilpa@users.noreply.github.com>
2024-09-09 11:10:07 -0400

    lint fix

--- libs/packages/components/src/lib/autocomplete-search/autocomplete-search.component.ts
+++ libs/packages/components/src/lib/autocomplete-search/autocomplete-search.component.ts
@@ -369,8 +369,8 @@ export class SDSAutocompleteSearchComponent implements ControlValueAccessor {
       this.setHighlightedItem(flat[this.highlightedIndex]);
     } else {
       this.showResults = false;
-      if(this.configuration.selectionMode === SelectionMode.MULTIPLE) {
-      this.inputValue = '';
+      if (this.configuration.selectionMode === SelectionMode.MULTIPLE) {
+        this.inputValue = '';
       }
       this.input.nativeElement.focus();
     }
