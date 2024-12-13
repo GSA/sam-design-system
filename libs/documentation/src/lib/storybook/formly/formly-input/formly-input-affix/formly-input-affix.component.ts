e1462111e462d9500002017dddc61a848311e838
yerramshilpa <44271677+yerramshilpa@users.noreply.github.com>
2024-12-13 11:52:36 -0500

    input updated

--- libs/documentation/src/lib/storybook/formly/formly-input/formly-input-affix/formly-input-affix.component.ts
+++ libs/documentation/src/lib/storybook/formly/formly-input/formly-input-affix/formly-input-affix.component.ts
@@ -10,7 +10,7 @@ export class FormlyInputAffixComponent {
   form = new FormGroup({});
   model: any = {};
   options: FormlyFormOptions = {};
-  fields1: FormlyFieldConfig[] = [
+  fields: FormlyFieldConfig[] = [
     {
       key: 'suffix',
       type: 'input',
@@ -40,66 +40,4 @@ export class FormlyInputAffixComponent {
       },
     },
   ];
-
-  fields: FormlyFieldConfig[] = [
-    {
-      key: 'name',
-      type: 'input',
-      props: {
-        label: 'Name (required)',
-        required: true,
-      },
-    },
-    {
-      key: 'age',
-      type: 'input',
-      props: {
-        label: 'Age (min= 18, max= 40)',
-        type: 'number',
-        min: 18,
-        max: 40,
-        required: true,
-      },
-    },
-    {
-      key: 'password',
-      type: 'input',
-      props: {
-        label: 'Password (minLength = 6)',
-        type: 'password',
-        required: true,
-        minLength: 6,
-      },
-    },
-    {
-      key: 'comment',
-      type: 'textarea',
-      props: {
-        label: 'Comment (maxLength = 100)',
-        required: true,
-        maxLength: 100,
-        rows: 5,
-      },
-    },
-    {
-      key: 'ip',
-      type: 'input',
-      props: {
-        label: 'IP Address (pattern = /(d{1,3}.){3}d{1,3}/)',
-        pattern: /(\d{1,3}\.){3}\d{1,3}/,
-        required: true,
-      },
-      validation: {
-        messages: {
-          pattern: (error: any, field: FormlyFieldConfig) => `"${field.formControl.value}" is not a valid IP Address`,
-        },
-      },
-    },
-  ];
-
-  submit() {
-    if (this.form.valid) {
-      alert(JSON.stringify(this.model));
-    }
-  }
 }
