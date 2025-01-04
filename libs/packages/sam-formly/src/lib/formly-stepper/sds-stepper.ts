6310d22de84772ad13ebe6409575014016888a92
yerramshilpa <44271677+yerramshilpa@users.noreply.github.com>
2025-01-03 21:11:44 -0500

    working demo

--- libs/packages/sam-formly/src/lib/formly-stepper/sds-stepper.ts
+++ libs/packages/sam-formly/src/lib/formly-stepper/sds-stepper.ts
@@ -147,13 +147,30 @@ export class SdsStepComponent {
     @Inject(DOCUMENT) private _document
   ) {}
 
+hasAllEmptyValues(obj) {
+  if (!obj || typeof obj !== 'object') {
+    return true; // Consider non-object types as empty
+  }
+
+  return Object.values(obj).every((value) => {
+    if (Array.isArray(value)) {
+      return value.length === 0; // Empty arrays
+    }
+    if (typeof value === 'object' && value !== null) {
+      return this.hasAllEmptyValues(value); // Recursively check nested objects
+    }
+    return value === '' || value === null || value === undefined || value === false || value === 0;
+  });
+}
   ngOnInit() {
     if (this.validateOnBlur) {
       this.form = new FormGroup({}, { updateOn:'change'  });
-    }
+   
+    
     const keyid = this.fieldConfig?.key ? this.fieldConfig?.key.toString() : '';
-    if (keyid && this._stepper.model[keyid]) {
-      this.form.markAllAsTouched();
+    if (keyid && this._stepper.model[keyid] && !this.hasAllEmptyValues(this._stepper.model[keyid])) {
+
+      setTimeout(() => this.form.markAllAsTouched());
       console.log('model', this._stepper.model[keyid]);
 
       this.options = {
@@ -161,6 +178,8 @@ export class SdsStepComponent {
           submitted: false,
         },
       };
+    }
+
     }
     this.form.statusChanges.subscribe((status) => {
       this._stepper.updateValidation(this._stepper.selectedStep);
