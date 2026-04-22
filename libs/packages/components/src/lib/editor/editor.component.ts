46b458ccdeed4310af1e2fcc89c2a0228a03f50b
Michael Getz <michael.getz@gsa.gov>
2026-04-22 09:26:55 -0600

    swap to non-capturing groups, this is used in a replaceall

--- libs/packages/components/src/lib/editor/editor.component.ts
+++ libs/packages/components/src/lib/editor/editor.component.ts
@@ -79,7 +79,7 @@ export class SdsEditorComponent implements ControlValueAccessor {
   // Validate regex and highlight first charecter of the failure
   validateRegex(value: string) {
     const rawValue = value
-      .replaceAll(/(<\/?mark[^>]*>)|(<\/?span[^>]*>)|(<\/?font[^>]*>)/g, '');
+      .replaceAll(/(?:<\/?mark[^>]*>)|(?:<\/?span[^>]*>)|(?:<\/?font[^>]*>)/g, '');
     const regex = new RegExp(this.regex, 'g');
     let res = rawValue;
     let result = regex.exec(rawValue);
